16fead2702ac5d63fa98679b392d2acd75e2cdac
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-18 01:40:50 +0200

    feat: Include category channel to api namespace

--- lib/api.dart
+++ lib/api.dart
@@ -1,8 +1,10 @@
 library api;
 
+import 'package:mineral/src/api/guild.dart';
 import 'package:mineral/src/constants.dart';
 
 part 'src/api/channels/channel.dart';
 part 'src/api/channels/voice_channel.dart';
 part 'src/api/channels/text_based_channel.dart';
 part 'src/api/channels/text_channel.dart';
+part 'src/api/channels/category_channel.dart';
