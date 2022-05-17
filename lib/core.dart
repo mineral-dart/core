82f9768937f4e75b889a15440cbd865ae68e5ea2
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-17 17:06:22 +0200

    feat: Change imports

--- lib/core.dart
+++ lib/core.dart
@@ -1,1 +1,6 @@
-library core;
+library mineral;
+
+export 'package:mineral/src/ioc.dart' show ioc;
+export 'package:mineral/src/http.dart';
+export 'package:mineral/src/websockets/websocket_manager.dart';
+export 'package:mineral/src/event_emitter.dart';
