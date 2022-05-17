6bc630fe11a0cb8105b0e9b8c143f4755159d207
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-17 17:23:33 +0200

    feat: Improve guild create

--- lib/src/websockets/packets/guild_create.dart
+++ lib/src/websockets/packets/guild_create.dart
@@ -1,3 +1,4 @@
+import 'package:mineral/src/api/user.dart';
 import 'package:mineral/src/constants.dart';
 import 'package:mineral/src/websockets/websocket_packet.dart';
 import 'package:mineral/src/websockets/websocket_response.dart';
@@ -8,6 +9,9 @@ class GuildCreate implements WebsocketPacket {
 
   @override
   Future<void> handle(WebsocketResponse websocketResponse) async {
-    print(websocketResponse.payload);
+    for(dynamic member in websocketResponse.payload['members']) {
+      print(User.from(member['user']));
+      print(User.from(member['user']).username);
+    }
   }
 }
