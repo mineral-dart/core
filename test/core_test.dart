8334c4783d94117f397a0566fba80db554061bce
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-17 17:06:37 +0200

    feat: Write tests

--- test/core_test.dart
+++ test/core_test.dart
@@ -1,16 +1,30 @@
-import 'package:core/core.dart';
-import 'package:test/test.dart';
+import 'package:mineral/core.dart';
+import 'package:test/scaffolding.dart';
 
 void main() {
-  group('A group of tests', () {
-    final awesome = Awesome();
+  test('can get Http from ioc', () {
+    String namespace = 'Mineral/Core/Http';
+    String discordEndpoint = 'https://discord.com/api';
+    ioc.bind(namespace: namespace, service: Http(baseUrl: discordEndpoint));
 
-    setUp(() {
-      // Additional setup goes here.
+    assert(ioc.singleton(namespace) is Http);
+  });
+
+  test('can emit with event emitter', () async {
+    EventEmitter emitter = EventEmitter<Object?>();
+    String key = 'foo';
+    emitter.on(key, (params) {
+      assert(params['bar'] == 'bar');
     });
 
-    test('First Test', () {
-      expect(awesome.isAwesome, isTrue);
-    });
+    emitter.emit(key, { 'bar': 'bar' });
+  });
+
+  test('can create websocket connection', () async {
+    String discordEndpoint = 'https://discord.com/api';
+
+    Http http = Http(baseUrl: discordEndpoint);
+    WebsocketManager manager = WebsocketManager(http);
+    await manager.connect(token: 'Nzg1ODgxOTk1NDc2ODYwOTc5.Gihx50.TKgmVB5cBsw-QV_W3kzAJdRP_Hk9CKZXUxbxnk');
   });
 }
