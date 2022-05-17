7425e9978dd6fc05a92aa2afc73ad5a690b643a8
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-17 17:23:12 +0200

    feat: Implement User

---
+++ lib/src/api/user.dart
@@ -0,0 +1,30 @@
+import 'package:mineral/src/constants.dart';
+
+class User {
+  Snowflake id;
+  String username;
+  String discriminator;
+  bool bot = false;
+  int publicFlags;
+  String? avatar;
+
+  User({
+    required this.id,
+    required this.username,
+    required this.discriminator,
+    required this.bot,
+    required this.publicFlags,
+    required this.avatar,
+  });
+
+  factory User.from(dynamic payload) {
+    return User(
+      id: payload['id'],
+      username: payload['username'],
+      discriminator: payload['discriminator'],
+      bot: payload['bot'] == true,
+      publicFlags: payload['public_flags'] ?? 0,
+      avatar: payload['avatar'],
+    );
+  }
+}
