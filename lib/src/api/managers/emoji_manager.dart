76c35ebadb9399ec05a14a796294bb3c7213544e
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-18 11:38:06 +0200

    feat: Add sync() implementation

--- lib/src/api/managers/emoji_manager.dart
+++ lib/src/api/managers/emoji_manager.dart
@@ -1,4 +1,9 @@
+import 'dart:convert';
+
+import 'package:http/http.dart';
 import 'package:mineral/api.dart';
+import 'package:mineral/core.dart';
+import 'package:mineral/src/api/guild.dart';
 import 'package:mineral/src/api/managers/cache_manager.dart';
 import 'package:mineral/src/constants.dart';
 import 'package:mineral/src/collection.dart';
@@ -6,4 +11,33 @@ import 'package:mineral/src/collection.dart';
 class EmojiManager implements CacheManager<Emoji> {
   @override
   Collection<Snowflake, Emoji> cache = Collection();
+
+  @override
+  Snowflake guildId;
+
+  @override
+  late Guild guild;
+
+  EmojiManager({ required this.guildId });
+
+  @override
+  Future<Collection<Snowflake, Emoji>> sync () async {
+    Http http = ioc.singleton('Mineral/Core/Http');
+    cache.clear();
+
+    Response response = await http.get("/guilds/$guildId/emojis");
+    dynamic payload = jsonDecode(response.body);
+
+    for(dynamic element in payload) {
+      Emoji emoji = Emoji.from(
+        memberManager: guild.members,
+        roleManager: guild.roles,
+        payload: element
+      );
+
+      cache.putIfAbsent(emoji.id, () => emoji);
+    }
+
+    return cache;
+  }
 }
