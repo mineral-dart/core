4265bfa19a94ff51d0c0967a8b31a13f23fcbb2a
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-18 11:38:47 +0200

    feat: Implement update()

--- lib/src/api/channels/category_channel.dart
+++ lib/src/api/channels/category_channel.dart
@@ -19,6 +19,36 @@ class CategoryChannel extends Channel {
     flags: flags
   );
 
+  Future<T?> update<T> ({ String? label, int? position }) async {
+    Http http = ioc.singleton('Mineral/Core/Http');
+
+    Response response = await http.patch("/channels/$id", {
+      'name': label,
+      'position': position,
+      'permission_overwrites': [],
+    });
+
+    dynamic payload = jsonDecode(response.body);
+    CategoryChannel channel = CategoryChannel.from(payload);
+
+    // Define deep properties
+    channel.guildId = guildId;
+    channel.guild = guild;
+    channel.parent = channel.parentId != null ? guild?.channels.cache.get<CategoryChannel>(channel.parentId) : null;
+
+    guild?.channels.cache.set(channel.id, channel);
+    return channel as T;
+  }
+
+  Future<bool> delete () async {
+    Http http = ioc.singleton('Mineral/Core/Http');
+    Response response = await http.destroy("/channels/$id");
+
+    guild?.channels.cache.remove(id);
+
+    return response.statusCode == 200;
+  }
+
   factory CategoryChannel.from(dynamic payload) {
     return CategoryChannel(
       id: payload['id'],
--- lib/src/api/channels/text_channel.dart
+++ lib/src/api/channels/text_channel.dart
@@ -27,6 +27,30 @@ class TextChannel extends TextBasedChannel {
     lastPinTimestamp: lastPinTimestamp,
   );
 
+  Future<T?> update<T> ({ String? label, String? description, int? delay, int? position, CategoryChannel? categoryChannel, bool? nsfw }) async {
+    Http http = ioc.singleton('Mineral/Core/Http');
+
+    Response response = await http.patch("/channels/$id", {
+      'name': label ?? this.label,
+      'topic': description,
+      'parent_id': categoryChannel?.id,
+      'nsfw': nsfw ?? false,
+      'rate_limit_per_user': delay ?? 0,
+      'permission_overwrites': [],
+    });
+
+    dynamic payload = jsonDecode(response.body);
+    TextChannel channel = TextChannel.from(payload);
+
+    // Define deep properties
+    channel.guildId = guildId;
+    channel.guild = guild;
+    channel.parent = channel.parentId != null ? guild?.channels.cache.get<CategoryChannel>(channel.parentId) : null;
+
+    guild?.channels.cache.set(channel.id, channel);
+    return channel as T;
+  }
+
   factory TextChannel.from(dynamic payload) {
     return TextChannel(
       id: payload['id'],
