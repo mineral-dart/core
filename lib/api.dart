298b7fac75d15316e2eda06071b514bc9baaa56f
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-18 14:15:59 +0200

    feat: Add emojis to api namespace

--- lib/api.dart
+++ lib/api.dart
@@ -1,6 +1,15 @@
 library api;
 
+import 'dart:convert';
+
+import 'package:http/http.dart';
+import 'package:mineral/core.dart';
 import 'package:mineral/src/api/guild.dart';
+import 'package:mineral/src/api/guild_member.dart';
+import 'package:mineral/src/api/managers/member_manager.dart';
+import 'package:mineral/src/api/managers/role_manager.dart';
+import 'package:mineral/src/api/role.dart';
+import 'package:mineral/src/collection.dart';
 import 'package:mineral/src/constants.dart';
 
 part 'src/api/channels/channel.dart';
@@ -8,3 +17,5 @@ part 'src/api/channels/voice_channel.dart';
 part 'src/api/channels/text_based_channel.dart';
 part 'src/api/channels/text_channel.dart';
 part 'src/api/channels/category_channel.dart';
+
+part 'src/api/emoji.dart';
