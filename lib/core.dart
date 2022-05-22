71664fc4eebc71d1466bf1ccbeabfc95c3b6e773
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-22 17:21:51 +0200

    feat: Improve core namespace

--- lib/core.dart
+++ lib/core.dart
@@ -1,15 +1,25 @@
 library core;
+import 'dart:io';
 
 import 'dart:collection';
 import 'dart:convert';
 
+import 'package:mineral/exception.dart';
+import 'package:mineral/src/internal/entities/event_manager.dart';
+import 'package:mineral/src/internal/environment.dart';
 import 'package:mineral/src/ioc/Container.dart';
 import 'package:http/http.dart' as http;
+import 'package:path/path.dart' as path;
+
+import 'core.dart';
 
 export 'package:mineral/src/websockets/websocket_manager.dart';
 export 'package:mineral/src/event_emitter.dart';
+
+part 'src/internal/kernel.dart';
 
 part 'package:mineral/src/ioc/ioc.dart';
 part 'src/constants.dart';
 part 'src/collection.dart';
 part 'package:mineral/src/http.dart';
+part 'package:mineral/src/entities/event.dart';
