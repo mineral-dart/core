8e83e2c346f979364c205d9bcf54b85fef814b31
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-05-22 17:22:15 +0200

    feat: Add Services

--- lib/src/ioc/ioc.dart
+++ lib/src/ioc/ioc.dart
@@ -3,6 +3,8 @@ part of core;
 class Service {
   static Symbol http = Symbol('Mineral/Core/Http');
   static Symbol client = Symbol('Mineral/Core/Client');
+  static Symbol environment = Symbol('Mineral/Core/Environment');
+  static Symbol event = Symbol('Mineral/Core/Event');
 }
 
 IocContainer ioc = IocContainer.init();
