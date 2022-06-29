02bfda24672d01c70b957e53f663b30aa0f03ca8
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-06-29 21:06:00 +0200

    feat: Move id & reply method

--- lib/src/api/interactions/command_interaction.dart
+++ lib/src/api/interactions/command_interaction.dart
@@ -1,24 +1,21 @@
 import 'dart:core';
 
-import 'package:http/http.dart';
 import 'package:mineral/api.dart';
-import 'package:mineral/core.dart';
 import 'package:mineral/src/api/interactions/interaction.dart';
 
 class CommandInteraction extends Interaction {
-  Snowflake id;
   String identifier;
   Map<String, dynamic> data = {};
 
   CommandInteraction({
     required this.identifier,
-    required this.id,
     required InteractionType type,
     required Snowflake applicationId,
+    required Snowflake id,
     required int version,
     required String token,
     required User user
-  }) : super(version: version, token: token, type: type, user: user, applicationId: applicationId);
+  }) : super(id: id, version: version, token: token, type: type, user: user, applicationId: applicationId);
 
   T? getChannel<T extends Channel> (String optionName) {
     return guild?.channels.cache.get(data[optionName]['value']);
@@ -50,37 +47,6 @@ class CommandInteraction extends Interaction {
 
   dynamic getMentionable (String optionName) {
     return data[optionName]['value'];
-  }
-
-  Future<void> reply ({ String? content, List<MessageEmbed>? embeds, List<Row>? components, bool? tts, bool? private }) async {
-    Http http = ioc.singleton(ioc.services.http);
-
-    List<dynamic> embedList = [];
-    if (embeds != null) {
-      for (MessageEmbed element in embeds) {
-        embedList.add(element.toJson());
-      }
-    }
-
-    List<dynamic> componentList = [];
-    if (components != null) {
-      for (Row element in components) {
-        componentList.add(element.toJson());
-      }
-    }
-
-    Response response = await http.post(url: "/interactions/$id/$token/callback", payload: {
-      'type': InteractionCallbackType.channelMessageWithSource.value,
-      'data': {
-        'tts': tts ?? false,
-        'content': content,
-        'embeds': embeds != null ? embedList : [],
-        'components': components != null ? componentList : [],
-        'flags': private != null && private == true ? 1 << 6 : null,
-      }
-    });
-
-    print(response.body);
   }
 
   factory CommandInteraction.from({ required User user, required dynamic payload }) {
--- lib/src/api/interactions/interaction.dart
+++ lib/src/api/interactions/interaction.dart
@@ -1,4 +1,5 @@
 import 'package:mineral/api.dart';
+import 'package:mineral/core.dart';
 
 enum InteractionCallbackType {
   pong(1),
@@ -14,6 +15,7 @@ enum InteractionCallbackType {
 }
 
 class Interaction {
+  Snowflake id;
   Snowflake applicationId;
   int version;
   InteractionType type;
@@ -21,5 +23,35 @@ class Interaction {
   User user;
   Guild? guild;
 
-  Interaction({ required this.applicationId, required this.version, required this.type, required this.token, required this.user });
+
+  Interaction({ required this.id, required this.applicationId, required this.version, required this.type, required this.token, required this.user });
+
+  Future<void> reply ({ String? content, List<MessageEmbed>? embeds, List<Row>? components, bool? tts, bool? private }) async {
+    Http http = ioc.singleton(ioc.services.http);
+
+    List<dynamic> embedList = [];
+    if (embeds != null) {
+      for (MessageEmbed element in embeds) {
+        embedList.add(element.toJson());
+      }
+    }
+
+    List<dynamic> componentList = [];
+    if (components != null) {
+      for (Row element in components) {
+        componentList.add(element.toJson());
+      }
+    }
+
+    await http.post(url: "/interactions/$id/$token/callback", payload: {
+      'type': InteractionCallbackType.channelMessageWithSource.value,
+      'data': {
+        'tts': tts ?? false,
+        'content': content,
+        'embeds': embeds != null ? embedList : [],
+        'components': components != null ? componentList : [],
+        'flags': private != null && private == true ? 1 << 6 : null,
+      }
+    });
+  }
 }
