585195a853b66bdb05d5ba9fcdc5e55d3973b588
Vic256 <contact.vic256@gmail.com>
2022-07-11 01:04:11 +0200

    feat: add get user method

--- lib/src/api/interactions/command_interaction.dart
+++ lib/src/api/interactions/command_interaction.dart
@@ -1,6 +1,7 @@
 import 'dart:core';
 
 import 'package:mineral/api.dart';
+import 'package:mineral/core.dart';
 import 'package:mineral/src/api/interactions/interaction.dart';
 
 class CommandInteraction extends Interaction {
@@ -18,35 +19,40 @@ class CommandInteraction extends Interaction {
   }) : super(id: id, version: version, token: token, type: type, user: user, applicationId: applicationId);
 
   T? getChannel<T extends Channel> (String optionName) {
-    return guild?.channels.cache.get(data[optionName]['value']);
+    return guild?.channels.cache.get(data[optionName]?['value']);
   }
 
   int? getInteger (String optionName) {
-    return data[optionName]['value'];
+    return data[optionName]?['value'];
   }
 
   String? getString (String optionName) {
-    return data[optionName]['value'];
+    return data[optionName]?['value'];
   }
 
   GuildMember? getMember (String optionName) {
-    return guild?.members.cache.get(data[optionName]['value']);
+    return guild?.members.cache.get(data[optionName]?['value']);
+  }
+
+  User? getUser (String optionName) {
+    final MineralClient client = ioc.singleton(ioc.services.client);
+    return client.users.cache.get(data[optionName]?['value']);
   }
 
   bool? getBoolean (String optionName) {
-    return data[optionName]['value'];
+    return data[optionName]?['value'];
   }
 
   Role? getRole (String optionName) {
-    return guild?.roles.cache.get(data[optionName]['value']);
+    return guild?.roles.cache.get(data[optionName]?['value']);
   }
 
   T? getChoice<T> (String optionName) {
-    return data[optionName]['value'];
+    return data[optionName]?['value'];
   }
 
   dynamic getMentionable (String optionName) {
-    return data[optionName]['value'];
+    return data[optionName]?['value'];
   }
 
   factory CommandInteraction.from({ required User user, required dynamic payload }) {
