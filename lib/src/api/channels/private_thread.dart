bc5efa4b8b2010bb204cd17b005f7f09b3111608
NathaelB <kirua48000@gmail.com>
2022-07-17 01:04:46 +0200

    :sparkles: create PrivateThread

---
+++ lib/src/api/channels/private_thread.dart
@@ -0,0 +1,53 @@
+
+import 'package:mineral/api.dart';
+import 'package:mineral/core.dart';
+import 'package:mineral/src/api/managers/webhook_manager.dart';
+
+class PrivateThread extends Channel {
+
+  String? name;
+  Snowflake? lastMessageId;
+  DateTime? lastPinTimeStamp;
+
+  PrivateThread({
+    required Snowflake id,
+    required Snowflake? guildId,
+    required int? position,
+    required String label,
+    required Snowflake? applicationId,
+    required Snowflake? parentId,
+    required int? flags,
+    required WebhookManager webhooks,
+  }) : super(
+    id: id,
+    type: ChannelType.guildPublicThread,
+    guildId: guildId,
+    position: position,
+    label: label,
+    applicationId: applicationId,
+    parentId: parentId,
+    flags: flags,
+    webhooks: webhooks,
+  );
+
+  Future<void> create (Snowflake message, String name) async {
+    Http http = ioc.singleton(ioc.services.http);
+    await http.post(url: '/channels/$id/messages/$message/threads', payload: {
+      'name': name,
+
+    });
+  }
+
+  factory PrivateThread.from({ required dynamic payload }) {
+    return PrivateThread(
+      id: payload['id'],
+      guildId: payload['guild_id'],
+      position: payload['position'],
+      label: payload['name'],
+      applicationId: payload['application_id'],
+      parentId: payload['parent_id'],
+      flags: payload['flags'],
+      webhooks: payload['webhooks'],
+    );
+  }
+}
