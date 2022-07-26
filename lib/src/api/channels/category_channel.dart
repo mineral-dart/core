dc95c7e4b8c67320726c9aa30db35f91c629832b
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-07-26 11:57:28 +0200

    refactor: CategoryChannel

--- lib/src/api/channels/category_channel.dart
+++ lib/src/api/channels/category_channel.dart
@@ -3,30 +3,19 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:mineral/api.dart';
 import 'package:mineral/core.dart';
-import 'package:mineral/src/api/channels/permission_overwrite.dart';
 import 'package:mineral/src/api/managers/permission_overwrite_manager.dart';
-import 'package:mineral/src/api/managers/webhook_manager.dart';
 
 class CategoryChannel extends Channel {
-  CategoryChannel({
-    required Snowflake id,
-    required Snowflake? guildId,
-    required int? position,
-    required String label,
-    required Snowflake? applicationId,
-    required int? flags,
-    required PermissionOverwriteManager permissionOverwrites
-  }) : super(
-    id: id,
-    guildId: guildId,
-    position: position,
-    label: label,
-    applicationId: applicationId,
-    parentId: null,
-    type: ChannelType.guildCategory,
-    flags: flags,
-    webhooks: WebhookManager(guildId: guildId, channelId: id),
-    permissionOverwrites: permissionOverwrites
+  CategoryChannel(
+    super._id,
+    super._type,
+    super.position,
+    super.label,
+    super.applicationId,
+    super.flags,
+    super._webhooks,
+    super.permissionOverwrites,
+    super._guild
   );
 
   Future<CategoryChannel?> update ({ String? label, int? position, List<PermissionOverwrite>? permissionOverwrites }) async {
@@ -42,9 +31,9 @@ class CategoryChannel extends Channel {
     CategoryChannel channel = CategoryChannel.from(payload);
 
     // Define deep properties
-    channel.guildId = guildId;
-    channel.guild = guild;
-    channel.parent = channel.parentId != null ? guild?.channels.cache.get<CategoryChannel>(channel.parentId) : null;
+    channel.parent = channel.parent != null
+      ? guild?.channels.cache.get<CategoryChannel>(payload['parent_id'])
+      : null;
 
     guild?.channels.cache.set(channel.id, channel);
     return channel;
@@ -60,23 +49,28 @@ class CategoryChannel extends Channel {
   }
 
   factory CategoryChannel.from(dynamic payload) {
-    final PermissionOverwriteManager permissionOverwriteManager = PermissionOverwriteManager(
-        guildId: payload['guild_id'],
-        channelId: payload['id']
-    );
+    MineralClient client = ioc.singleton(ioc.services.client);
+    final permissionOverwriteManager = PermissionOverwriteManager();
+
     for(dynamic element in payload['permission_overwrites']) {
       final PermissionOverwrite overwrite = PermissionOverwrite.from(payload: element);
       permissionOverwriteManager.cache.putIfAbsent(overwrite.id, () => overwrite);
     }
 
-    return CategoryChannel(
-      id: payload['id'],
-      guildId: payload['guild_id'],
-      position: payload['position'],
-      label: payload['name'],
-      applicationId: payload['application_id'],
-      flags: payload['flags'],
-      permissionOverwrites: permissionOverwriteManager
+    final category = CategoryChannel(
+      payload['id'],
+      ChannelType.guildCategory,
+       payload['position'],
+      payload['name'],
+      payload['application_id'],
+      payload['flags'],
+      null,
+      permissionOverwriteManager,
+      client.guilds.cache.get(payload['guild_id'])
     );
+
+    category.permissionOverwrites?.channel = category;
+
+    return category;
   }
 }
