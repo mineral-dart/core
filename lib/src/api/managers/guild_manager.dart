d8e73c8c73556f5efcf066f036ae7a9a5eaeddab
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-07-26 11:54:14 +0200

    refactor: GuildManager

--- lib/src/api/managers/guild_manager.dart
+++ lib/src/api/managers/guild_manager.dart
@@ -1,12 +1,5 @@
 import 'package:mineral/api.dart';
 import 'package:mineral/src/api/managers/cache_manager.dart';
 
-class GuildManager implements CacheManager<Guild> {
-  @override
-  Map<Snowflake, Guild> cache = {};
-
-  @override
-  Future<Map<Snowflake, Guild>> sync() {
-    throw UnimplementedError();
-  }
+class GuildManager extends CacheManager<Guild> {
 }
