80bda19a8dd6a899d9c02a03590969aa3cfafa79
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-07-26 11:57:05 +0200

    refactor: UserManager

--- lib/src/api/managers/user_manager.dart
+++ lib/src/api/managers/user_manager.dart
@@ -1,7 +1,7 @@
 import 'package:mineral/api.dart';
 import 'package:mineral/src/api/managers/cache_manager.dart';
 
-class UserManager implements CacheManager<User> {
+class UserManager extends CacheManager<User> {
   @override
   Map<Snowflake, User> cache = {};
 
