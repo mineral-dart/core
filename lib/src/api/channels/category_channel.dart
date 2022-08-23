7b2577b0fcbb4ec12690dc2f4e55b2ed34ad94ce
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-08-23 17:26:40 +0200

    feat: WIP - Implement CategoryChannel

--- lib/src/api/channels/category_channel.dart
+++ lib/src/api/channels/category_channel.dart
@@ -1,5 +1,8 @@
 import 'package:mineral/api.dart';
 
 class CategoryChannel extends GuildChannel {
-  CategoryChannel(super.id);
+  CategoryChannel(super.guildId, super.parentId, super.label, super.type, super.position, super.flags, super.permissions, super.id);
+
+  /// Get children of this
+  Map<Snowflake, GuildChannel> get children => guild.channels.cache.where((element) => element.parent?.id == id);
 }
