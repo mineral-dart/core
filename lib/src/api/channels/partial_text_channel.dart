360417e30d39aa07435b4fbfdfcb3ef6ead17739
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-08-23 17:31:14 +0200

    feat: WIP - Implement partialTextChannel

--- lib/src/api/channels/partial_text_channel.dart
+++ lib/src/api/channels/partial_text_channel.dart
@@ -1,5 +1,15 @@
 import 'package:mineral/api.dart';
+import 'package:mineral/src/api/managers/message_manager.dart';
 
 class PartialTextChannel extends GuildChannel {
-  PartialTextChannel(super.id);
+  final MessageManager _messages;
+  final Snowflake? _lastMessageId;
+
+  PartialTextChannel(this._messages, this._lastMessageId, super.guildId, super.parentId, super.label, super.type, super.position, super.flags, super.permissions, super.id);
+
+  /// Get [MessageManager]
+  MessageManager get messages => _messages;
+
+  /// Get last [Message] of this
+  Message? get lastMessage => _messages.cache.get(_lastMessageId);
 }
