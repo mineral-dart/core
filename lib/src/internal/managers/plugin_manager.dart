c394b9dfbd54d65d7070b9207700213df76e8d77
Freeze455 <baptiste.parmantier.pro@gmail.com>
2022-09-19 17:50:03 +0200

    feat: Inject ioc into plugins entrypoint

--- lib/src/internal/managers/plugin_manager.dart
+++ lib/src/internal/managers/plugin_manager.dart
@@ -1,11 +1,20 @@
 import 'dart:io';
+import 'dart:mirrors';
 
 import 'package:mineral_ioc/ioc.dart';
 
 class PluginManager {
+  final List _plugins = [];
+
   void use (List plugins) {
-    for (final plugin in plugins) {
-      ioc.bind(namespace: 'Mineral/Plugins/${plugin.label}', service: plugin);
+    _plugins.addAll(plugins);
+  }
+
+  Future<void> load () async {
+    for (final plugin in _plugins) {
+      final namespace = reflect(plugin).type.getField(Symbol('namespace'));
+      ioc.bind(namespace: namespace.reflectee, service: plugin);
+
       plugin..root = Directory.current
         ..init();
     }
