5a00bc0706f6d8e5ab3849f9df676b51c07cf413
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2022-10-24 02:00:32 +0200

    feat: Implement generic event

---
+++ lib/event.dart
@@ -0,0 +1,6 @@
+library events;
+
+export './src/internal/entities/event.dart';
+
+export './src/internal/websockets/events/ready.dart';
+export './src/internal/websockets/events/guild_create.dart';
