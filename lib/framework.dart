2e6e41b74c28966b101b65bd2d34d5ee64a8e6d4
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2022-12-27 22:14:06 +0100

    feat: Create compile:exe command

--- lib/framework.dart
+++ lib/framework.dart
@@ -1,7 +1,6 @@
 /// Contains the main services of the mineral framework as well as helper extensions
 library framework;
 
-export '../src/console.dart';
 export '../src/internal/entities/command.dart' show MineralCommand, CommandBuilder, Option, OptionType, OptionChoice, SubCommandBuilder, CommandGroupBuilder, Scope;
 export '../src/internal/entities/context_menu.dart' show MineralContextMenu;
 export '../src/internal/entities/event.dart' show MineralEvent, Event;
@@ -9,3 +8,5 @@ export '../src/internal/entities/module.dart' show MineralModule;
 export '../src/internal/entities/state.dart' show MineralState;
 export '../src/internal/extensions/collection.dart';
 export '../src/internal/extensions/string.dart';
+
+export '../src/internal/mixins/console.dart';
