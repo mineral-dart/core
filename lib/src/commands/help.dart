92df6a49b1d1bf1b78fe170fe492d65d4d833e6c
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2022-12-27 22:15:24 +0100

    feat: Create help command

--- lib/src/commands/help.dart
+++ lib/src/commands/help.dart
@@ -1,42 +1,41 @@
 import 'package:collection/collection.dart';
-import 'package:mineral/framework.dart';
-import 'package:mineral/src/console.dart';
-import 'package:mineral/src/internal/managers/cli_manager.dart';
-import 'package:mineral_ioc/ioc.dart';
+import 'package:mineral_cli/mineral_cli.dart';
 
-class Help extends MineralCliCommand {
-  @override
-  String name = 'help';
-
-  @override
-  String description = 'Displays the list of commands';
+class Help extends CliCommand {
+  final MineralCliContract _cli;
+  Help(Console console, this._cli): super(console, 'help', 'Displays the list of commands', []);
 
   @override
   Future<void> handle(args) async {
-    Map<String, List> commands = {};
-    for (final command in parser.commands.entries) {
-      final String key = command.key.contains(':')
-          ? command.key.split(':').first
-          : 'Available commands';
+    Map<String, List<CliCommand>> commands = {};
 
-      MineralCliCommand? mineralCommand = ioc.use<CliManager>().commands.get(command.key);
+    for (final command in _cli.commands.values) {
+      final String key = command.name.contains(':')
+        ? command.name.split(':').first
+        : 'Available commands';
 
       if (commands.containsKey(key)) {
-        commands.get(key)?.add({ 'name': command.key, 'args': mineralCommand?.description });
+        commands[key]?.add(command);
       } else {
-        commands.putIfAbsent(key, () => [{ 'name': command.key, 'args': mineralCommand?.description }]);
+        commands.putIfAbsent(key, () => [command]);
       }
     }
 
     String display = '\n';
-    for (final group in commands.entries.sorted((a, b) => b.key.length.compareTo(a.key.length))) {
-      display += ColorList.blue(group.key) + '\n';
-      for (final command in group.value) {
-        int length = command['name']!.length;
-        String commandName = ColorList.green(command['name']);
-        String args = ColorList.dim(command['args']);
+    _cli.commands.values.toList().sort((a, b) => a.arguments.length + b.arguments.length);
+    int maxArgumentLength = _cli.commands.values.last.arguments.map((argument) => '<$argument>').join('').length;
 
-        display += '  $commandName${List.filled(20 - length, ' ').join()}$args${ColorList.reset()}\n';
+    for (final group in commands.entries.sorted((a, b) => b.key.length.compareTo(a.key.length))) {
+      display += '${group.key.blue()}\n';
+
+      for (final command in group.value) {
+        int length = command.name.length;
+        String commandName = command.name.green();
+        String description = command.description.grey();
+        String arguments = command.arguments.map((argument) => '<$argument>').join(' ').green();
+        final indent = (20 + maxArgumentLength) - ((arguments.length - maxArgumentLength) + length);
+
+        display += '  $commandName $arguments ${' ' * indent}$description\n';
       }
 
       display += '\n';
