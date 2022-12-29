8840b6723d9d07ca3d0c85d7ce6b72801306f1ce
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2022-12-29 23:24:21 +0100

    feat: Add getter to simplify access

--- lib/src/internal/websockets/events/button_create_event.dart
+++ lib/src/internal/websockets/events/button_create_event.dart
@@ -8,4 +8,5 @@ class ButtonCreateEvent extends Event {
 
   ButtonInteraction get interaction => _interaction;
   GuildMember get sender => _interaction.member!;
+  TextBasedChannel get channel => _interaction.channel;
 }
