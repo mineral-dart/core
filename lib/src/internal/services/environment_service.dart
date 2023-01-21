04bfc24f0242b6065038fa6aeec71e48d0e0facc
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-01-22 00:57:51 +0100

    feat: Add Docker environment injector

--- lib/src/internal/services/environment_service.dart
+++ lib/src/internal/services/environment_service.dart
@@ -12,6 +12,14 @@ class EnvironmentService extends MineralService with Console {
   EnvironmentService(): super(inject: true);
 
   Future<MineralEnvironment> load () async {
+    if (hasValidEnvironment()) {
+      environment.data.putIfAbsent('APP_TOKEN', () => Platform.environment.getOrFail('APP_TOKEN'));
+      environment.data.putIfAbsent('LOG_LEVEL', () => Platform.environment.getOrFail('LOG_LEVEL'));
+      environment.data.putIfAbsent('DEBUGGER', () => Platform.environment.getOrFail('DEBUGGER'));
+
+      return environment;
+    }
+
     File file = File(join(Directory.current.path, '.env'));
     if (!await file.exists()) {
       await createEnvironmentFile();
@@ -43,5 +51,12 @@ class EnvironmentService extends MineralService with Console {
 
     await sink.flush();
     await sink.close();
+  }
+
+  bool hasValidEnvironment () {
+    final requiredEnvironmentKeys = ['APP_TOKEN', 'APP_NAME', 'LOG_LEVEL', 'DEBUGGER'];
+    final requiredKeysChecked = requiredEnvironmentKeys.fold([], (previousValue, element) => [...previousValue, Platform.environment.get(element) != null]);
+
+    return !requiredKeysChecked.contains(false);
   }
 }
