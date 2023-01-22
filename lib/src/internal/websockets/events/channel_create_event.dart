f91fa1b646f503d8e867fb4f10cc35ef719d6797
Vic256 <contact.vic256@gmail.com>
2023-01-22 22:25:59 +0100

    fix: Accept DMChannel on ChannelCreate event

--- lib/src/internal/websockets/events/channel_create_event.dart
+++ lib/src/internal/websockets/events/channel_create_event.dart
@@ -2,9 +2,9 @@ import 'package:mineral/core/api.dart';
 import 'package:mineral/framework.dart';
 
 class ChannelCreateEvent extends Event {
-  final GuildChannel _channel;
+  final PartialChannel _channel;
 
   ChannelCreateEvent(this._channel);
 
-  GuildChannel get channel => _channel;
+  PartialChannel get channel => _channel;
 }
