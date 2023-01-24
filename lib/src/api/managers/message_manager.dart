9386f991b42d922bc02b0649f47f6fe3f5828894
Vic256 <contact.vic256@gmail.com>
2023-01-24 08:21:48 +0100

    feat: Add get method on message manager

--- lib/src/api/managers/message_manager.dart
+++ lib/src/api/managers/message_manager.dart
@@ -3,9 +3,12 @@ import 'dart:convert';
 import 'package:http/http.dart';
 import 'package:mineral/core.dart';
 import 'package:mineral/core/api.dart';
+import 'package:mineral/exception.dart';
 import 'package:mineral/framework.dart';
 import 'package:mineral/src/api/managers/cache_manager.dart';
+import 'package:mineral/src/api/messages/dm_message.dart';
 import 'package:mineral/src/api/messages/partial_message.dart';
+import 'package:mineral_cli/mineral_cli.dart';
 import 'package:mineral_ioc/ioc.dart';
 
 class MessageManager<T extends PartialMessage> extends CacheManager<T>  {
@@ -38,5 +41,40 @@ class MessageManager<T extends PartialMessage> extends CacheManager<T>  {
     }
 
     return cache;
+  }
+
+  Future<T> get (Snowflake id) async {
+    if(cache.containsKey(id)) {
+      return cache.getOrFail(id);
+    }
+
+    final Response response = await ioc.use<DiscordApiHttpService>()
+        .get(url: '/channels/${_channelId}/messages/$id')
+        .build();
+
+    if(response.statusCode == 200) {
+      dynamic payload = jsonDecode(response.body);
+
+      if (_guildId != null) {
+        final Guild guild = ioc.use<MineralClient>().guilds.cache.getOrFail(_guildId);
+        Message message = Message.from(
+            channel: guild.channels.cache.getOrFail(_channelId),
+            payload: payload
+        );
+
+        cache.putIfAbsent(message.id, () => message as T);
+        return message as T;
+      } else {
+        DmMessage message = DmMessage.from(
+            channel: ioc.use<MineralClient>().dmChannels.cache.getOrFail(_channelId),
+            payload: payload
+        );
+
+        cache.putIfAbsent(message.id, () => message as T);
+        return message as T;
+      }
+    }
+
+    throw ApiException('Unable to fetch message!');
   }
 }
