d8adfe93644b2017340d31ad8325121aee0aa3a5
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-03-24 20:29:27 +0100

    feat: Implement channel select menu interaction

--- lib/src/api/builders/menus/channel_select_menu_builder.dart
+++ lib/src/api/builders/menus/channel_select_menu_builder.dart
@@ -3,12 +3,16 @@ import 'package:mineral/src/api/builders/component_wrapper.dart';
 import 'package:mineral/src/api/builders/menus/select_menu_builder.dart';
 
 class ChannelSelectMenuBuilder extends SelectMenuBuilder {
-  List<ChannelType>? channels;
-  ChannelSelectMenuBuilder(String customId, { this.channels }) : super (customId, ComponentType.channelSelect);
+  List<ChannelType> channels = [];
+  ChannelSelectMenuBuilder(String customId) : super (customId, ComponentType.channelSelect);
+
+  void setChannelTypes (List<ChannelType> types) {
+    channels.addAll(types);
+  }
 
   @override
-  Map<String, dynamic>  toJson () => {
+  Map<String, dynamic> toJson () => {
     ...super.toJson(),
-    'channel_types': channels?.map((element) => element.value).toList(),
+    'channel_types': channels.map((element) => element.value).toList(),
   };
 }
