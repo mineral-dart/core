9eec223dce8e09e2acf7be756cb2ecfa40f2d8a0
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-03-24 13:47:01 +0100

    feat: Implement role select menu

---
+++ lib/src/api/builders/menus/role_select_menu_builder.dart
@@ -0,0 +1,6 @@
+import 'package:mineral/src/api/builders/component_wrapper.dart';
+import 'package:mineral/src/api/builders/menus/select_menu_builder.dart';
+
+class RoleSelectMenuBuilder extends SelectMenuBuilder {
+  RoleSelectMenuBuilder(String customId) : super(customId, ComponentType.roleSelect);
+}
