6891a64b33b20770a58775d09be20b313c544e11
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-03-27 23:27:55 +0200

    feat: Implement event service

--- lib/src/internal/entities/event.dart
+++ lib/src/internal/entities/event.dart
@@ -1,7 +1,10 @@
+import 'package:mineral_contract/mineral_contract.dart';
+
 abstract class Event {}
 
-abstract class MineralEvent<Event> {
+abstract class MineralEvent<Event> extends MineralEventContract {
   final Type listener = Event;
+
   String? customId;
 
   Future<void> handle (Event event);
