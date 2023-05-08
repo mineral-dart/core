3f5b56b773bbc4c56c4cac0be573eb08df06d91f
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-08 22:53:06 +0200

    docs: Improve GuildMemberPresence documentation

--- lib/src/api/guilds/guild_member_presence.dart
+++ lib/src/api/guilds/guild_member_presence.dart
@@ -2,6 +2,7 @@ import 'package:mineral/core/api.dart';
 import 'package:mineral/src/api/guilds/activities/guild_member_activity.dart';
 import 'package:mineral/src/api/guilds/client_status_bucket.dart';
 
+/// Represents a presence of a [GuildMember].
 class GuildMemberPresence {
   final Snowflake _guildId;
   final String _status;
@@ -11,10 +12,17 @@ class GuildMemberPresence {
 
   GuildMemberPresence(this._guildId, this._status, this._premiumSince, this._clientStatus, this._activities);
 
+  /// Get the presence status.
   StatusType get status => StatusType.values.firstWhere((element) => element.value == _status);
+
+  /// Get the premium since.
   DateTime? get premiumSince => _premiumSince != null
     ? DateTime.parse(_premiumSince!)
     : null;
+
+  /// Get the client status.
   ClientStatusBucket get clientStatus => _clientStatus;
+
+  /// Get the activities.
   List<GuildMemberActivity> get activities => _activities;
 }
