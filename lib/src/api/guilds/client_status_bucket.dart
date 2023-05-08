3478632ac8ecac5c81d9defbbbaf4b6612fc20dd
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-08 21:20:37 +0200

    docs: Write ClientStatusBucket documentations

--- lib/src/api/guilds/client_status_bucket.dart
+++ lib/src/api/guilds/client_status_bucket.dart
@@ -1,3 +1,4 @@
+/// Represents the client status bucket of a [User]
 class ClientStatusBucket {
   final String? _desktop;
   final String? _web;
@@ -5,7 +6,12 @@ class ClientStatusBucket {
 
   ClientStatusBucket(this._desktop, this._web, this._mobile);
 
+  /// Returns the desktop status of the [User]
   String? get desktop => _desktop;
+
+  /// Returns the web status of the [User]
   String? get web => _web;
+
+  /// Returns the mobile status of the [User]
   String? get mobile => _mobile;
 }
