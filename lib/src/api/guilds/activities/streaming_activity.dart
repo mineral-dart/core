1668454941d4dc5d907a31af1f1ccc5636b51dbf
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-08 23:28:56 +0200

    docs: Improve StreamingActivity documentation

--- lib/src/api/guilds/activities/streaming_activity.dart
+++ lib/src/api/guilds/activities/streaming_activity.dart
@@ -1,6 +1,7 @@
 import 'package:mineral/core/api.dart';
 import 'package:mineral/src/api/guilds/activities/guild_member_activity.dart';
 
+/// Represents a streaming [Activity].
 class StreamingActivity extends GuildMemberActivity {
   final String? _state;
   final String? _details;
@@ -13,8 +14,13 @@ class StreamingActivity extends GuildMemberActivity {
     this._url,
   ): super(ActivityType.streaming, name);
 
+  /// The state of this.
   String? get state => _state;
+
+  /// The details of this.
   String? get details => _details;
+
+  /// The url of this.
   String get url => _url;
 
   factory StreamingActivity.from(Snowflake guildId, dynamic payload) => StreamingActivity(
