8d8a5185524acf66ca9755eebb637dc3d6333c07
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-09 00:30:25 +0200

    docs: Write EmbedThumbnail documentation

---
+++ lib/src/api/builders/embed/embed_thumbnail.dart
@@ -0,0 +1,23 @@
+class EmbedThumbnail {
+  /// The source url of the thumbnail
+  String url;
+
+  /// A proxy url of the thumbnail
+  String? proxyUrl;
+
+  /// The height of the thumbnail
+  int? height;
+
+  /// The width of the thumbnail
+  int? width;
+
+  EmbedThumbnail({ required this.url, this.proxyUrl, this.height, this.width });
+
+  /// Converts the [EmbedThumbnail] to a [Map] that can be serialized to JSON.
+  Object toJson () => {
+    'url': url,
+    'proxy_url': proxyUrl,
+    'width': width,
+    'height': height,
+  };
+}
