0400532eee8b7a59c515bff78fb2cd0b6446d09b
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-09 00:03:09 +0200

    docs: Write many SelectMenu documentation

--- lib/src/api/builders/menus/select_menu_builder.dart
+++ lib/src/api/builders/menus/select_menu_builder.dart
@@ -1,5 +1,6 @@
 import 'package:mineral/src/api/builders/component_wrapper.dart';
 
+/// A builder for select menus component.
 class SelectMenuBuilder extends ComponentWrapper {
   final String _customId;
   String? _placeholder;
@@ -9,14 +10,19 @@ class SelectMenuBuilder extends ComponentWrapper {
 
   SelectMenuBuilder(this._customId, ComponentType componentType) : super(type: componentType);
 
+  /// Sets the placeholder text shown on the menu.
   void setPlaceholder (String value) => _placeholder = value;
 
+  /// Sets the disabled state of the menu.
   void setDisabled (bool value) => _disabled = value;
 
+  /// Sets the minimum number of items that must be chosen; default 1, min 0, max 25.
   void setMinValues (int value) => _minValues = value;
 
+  /// Sets the maximum number of items that can be chosen; default 1, max 25.
   void setMaxValues (int value) => _maxValues = value;
 
+  /// Serialize this component to a JSON object.
   @override
   Map<String, dynamic> toJson () {
     return {
