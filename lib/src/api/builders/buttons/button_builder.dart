4532b10d08aee612fe1119040240d1414fddb6e6
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-08 23:54:23 +0200

    docs: Write button documentations

--- lib/src/api/builders/buttons/button_builder.dart
+++ lib/src/api/builders/buttons/button_builder.dart
@@ -7,8 +7,12 @@ import 'package:mineral/src/api/builders/component_wrapper.dart';
 
 import 'contracts/link_contract.dart';
 
+/// ButtonBuilder component
 class ButtonBuilder extends ComponentWrapper implements ButtonContract {
+  /// Create a basic button with dedicated properties
   static ClickableButtonContract button (String customId) => ClickableButton(customId, null, ButtonStyle.primary);
+
+  /// Create a link button with dedicated properties
   static LinkContract link (String url) => LinkButton(null, url, ButtonStyle.link);
 
   final String? _customId;
@@ -20,26 +24,37 @@ class ButtonBuilder extends ComponentWrapper implements ButtonContract {
 
   ButtonBuilder(this._customId, this._url, this._style): super(type: ComponentType.button);
 
+  /// Gets the custom id of this
   String get customId => _customId!;
 
+  /// Gets the label of this
   String? get label => _label;
 
+  /// Gets the [ButtonStyle] of this
   ButtonStyle get style => _style;
 
+  /// Gets the [EmojiBuilder] of this
   EmojiBuilder? get emoji => _emoji;
 
+  /// Gets the disabled state of this
   bool get disabled => _disabled;
 
+  /// Gets the url of this
   String get url => _url!;
 
+  /// Sets the label of this
   void setLabel (String value) => _label = value;
 
+  /// Sets the disabled state of this
   void setDisabled (bool value) => _disabled = value;
 
+  /// Sets the emoji of this
   void setEmoji (EmojiBuilder? value) => _emoji = value;
 
+  /// Sets the style of this
   void setStyle (ButtonStyle value) => _style = value;
 
+  /// Sets the url of this
   void setUrl (String value) => _url = value;
 
   @override
--- lib/src/api/builders/buttons/button_style.dart
+++ lib/src/api/builders/buttons/button_style.dart
@@ -1,13 +1,22 @@
+import 'package:mineral/core/builders.dart';
+
+/// Style of [ButtonBuilder]
 enum ButtonStyle {
+  /// Primary style
   primary(1),
+
+  /// Secondary style
   secondary(2),
+
+  /// Success style
   success(3),
+
+  /// Danger style
   danger(4),
+
+  /// Link style
   link(5);
 
   final int value;
   const ButtonStyle(this.value);
-
-  @override
-  String toString () => value.toString();
 }
