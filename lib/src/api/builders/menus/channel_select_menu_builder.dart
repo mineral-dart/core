3b427ace6b3a147719c5123bab15f7a37b5a6c13
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-08 23:59:48 +0200

    docs: Write ChannelSelectMenuBuilder documentation

--- lib/src/api/builders/menus/channel_select_menu_builder.dart
+++ lib/src/api/builders/menus/channel_select_menu_builder.dart
@@ -2,14 +2,21 @@ import 'package:mineral/core/api.dart';
 import 'package:mineral/src/api/builders/component_wrapper.dart';
 import 'package:mineral/src/api/builders/menus/select_menu_builder.dart';
 
+/// A builder for select menus component.
 class ChannelSelectMenuBuilder extends SelectMenuBuilder {
-  List<ChannelType> channels = [];
+  final List<ChannelType> _channels = [];
+
   ChannelSelectMenuBuilder(String customId) : super (customId, ComponentType.channelSelect);
 
+  /// The list of [ChannelType] that can be selected.
+  List<ChannelType> get channels => _channels;
+
+  /// Sets the list of [ChannelType] that can be selected.
   void setChannelTypes (List<ChannelType> types) {
     channels.addAll(types);
   }
 
+  /// Serialize this component to a JSON object.
   @override
   Map<String, dynamic> toJson () => {
     ...super.toJson(),
