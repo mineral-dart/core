4224ea4d24adbab663ba2dd9e1aaef176d342759
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-08 23:12:41 +0200

    docs: Improve AssetsActivity documentation

--- lib/src/api/guilds/activities/assets_activity.dart
+++ lib/src/api/guilds/activities/assets_activity.dart
@@ -1,5 +1,6 @@
 import 'package:mineral/core/api.dart';
 
+/// Represents an [Activity] assets.
 class AssetsActivity {
   final ImageFormater? _smallImage;
   final String? _smallText;
@@ -8,8 +9,15 @@ class AssetsActivity {
 
   AssetsActivity(this._smallImage, this._smallText, this._largeImage, this._largeText);
 
+  /// Returns the small image.
   ImageFormater? get smallImage => _smallImage;
+
+  /// Returns the small text.
   String? get smallText => _smallText;
+
+  /// Returns the large image.
   ImageFormater? get largeImage => _largeImage;
+
+  /// Returns the large text.
   String? get largeText => _largeText;
 }
