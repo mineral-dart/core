3745f43a667c28bd9915f755e4e3998653ec3d4e
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-08 23:57:42 +0200

    docs: Write TimestampStyle documentation

--- lib/src/api/builders/markdown/timestamp_style.dart
+++ lib/src/api/builders/markdown/timestamp_style.dart
@@ -1,10 +1,24 @@
+/// Defines the style of the timestamp.
 enum TimestampStyle {
+  /// Short time style
   shortTime('t'),
+
+  /// Long time style
   longTime('T'),
+
+  /// Short date style
   shortDate('d'),
+
+  /// Long date style
   longDate('D'),
+
+  /// Short date time style
   shortDateTime('f*'),
+
+  /// Long date time style
   longDateTime('F'),
+
+  /// Relative time style
   relativeTime('R');
 
   final String value;
