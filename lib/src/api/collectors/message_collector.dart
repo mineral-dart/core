1d039a892c309619da73254f1e8bff41e60ebae1
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-08 19:36:59 +0200

    docs: Write messageCollector documentation

--- lib/src/api/collectors/message_collector.dart
+++ lib/src/api/collectors/message_collector.dart
@@ -6,9 +6,11 @@ import 'package:mineral/src/api/collectors/collector.dart';
 import 'package:mineral/src/internal/services/collector_service.dart';
 import 'package:mineral_ioc/ioc.dart';
 
+/// The MessageCollector class is part of the Mineral framework and is used to collect messages that match certain criteria.
+/// It can be used to implement functionality like message polls or message surveys.
 class MessageCollector extends Collector  {
   final Map<Snowflake, Message> _messages = {};
-  StreamSubscription? subscription;
+  StreamSubscription? _subscription;
 
   final bool Function(Message message) _filter;
   final int? _max;
@@ -18,12 +20,19 @@ class MessageCollector extends Collector  {
     ioc.use<CollectorService>().subscribe(this);
   }
 
+  /// Represents the maximum number of messages to collect.
+  int? get max => _max;
+
+  /// /// Represents the maximum amount of time to collect messages for.
   Duration? get time => _time;
 
+  /// It starts listening to the [MessageCreateEvent] and collects messages that match the filter criteria.
+  ///
+  /// If max messages have been collected, it cancels the subscription, unsubscribes the collector from the CollectorService, and returns a future containing the collected messages.
   @override
   Future<dynamic> collect () async {
     final completer = Completer();
-    subscription = controller.stream.listen((message) async {
+    _subscription = controller.stream.listen((message) async {
       if (_filter(message)) {
         _messages.putIfAbsent(message.id, () => message);
       }
@@ -37,8 +46,9 @@ class MessageCollector extends Collector  {
     return completer.future;
   }
 
+  /// Unsubscribes the collector from the [MessageCollector] and cancels the subscription.
   Future<void> _unsubscribe () async {
     ioc.use<CollectorService>().unsubscribe(this);
-    await subscription?.cancel();
+    await _subscription?.cancel();
   }
 }
