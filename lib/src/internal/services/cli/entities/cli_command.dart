8fb74747ca67e9a995741faf00d894c2bf9d2d3f
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-09 09:38:50 +0200

    docs: Write CliCommand documentation

--- lib/src/internal/services/cli/entities/cli_command.dart
+++ lib/src/internal/services/cli/entities/cli_command.dart
@@ -1,8 +1,10 @@
 import 'dart:io';
 
 import 'package:mineral/src/internal/services/console/console_service.dart';
+import 'package:mineral_contract/mineral_contract.dart';
 import 'package:path/path.dart';
 
+/// Base class for all [CliCommandContract] commands
 abstract class CliCommand {
   final ConsoleService _console;
   final String _name;
@@ -11,13 +13,22 @@ abstract class CliCommand {
 
   CliCommand(this._console, this._name, this._description, this._args);
 
+  /// The [ConsoleService] used to display messages in this
   ConsoleService get console => _console;
+
+  /// The name of the command
   String get name => _name;
+
+  /// The description of the command
   String get description => _description;
+
+  /// The arguments of the command
   List<String> get arguments => _args;
 
+  /// Handle the command line arguments to execute the given entry command.
   Future<void> handle (Map args);
 
+  /// Get all directories from the given location
   Future<List<Directory>> getDirectories (String location) async {
     List<Directory> directories = [];
     directories.add(Directory(join(Directory.current.path, location)));
