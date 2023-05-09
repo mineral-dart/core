38c4913735d248f058a3a7c1d79c35f840f9b43b
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-09 09:23:30 +0200

    docs: Write RowBuilder documentation

--- lib/src/api/builders/row_builder.dart
+++ lib/src/api/builders/row_builder.dart
@@ -1,15 +1,16 @@
 import 'package:mineral/src/api/builders/component_wrapper.dart';
 
+/// Split [ComponentBuilder] into multiple rows.
 class RowBuilder extends ComponentWrapper {
+  /// The [ComponentBuilder] of this as [List].
   List components;
 
   RowBuilder(this.components) : super(type: ComponentType.actionRow);
 
+  /// Serialize this to json.
   @override
-  Object toJson () {
-    return {
-      'type': type?.value,
-      'components': components.map((dynamic component) => component.toJson()).toList()
-    };
-  }
+  Object toJson () => {
+    'type': type?.value,
+    'components': components.map((dynamic component) => component.toJson()).toList()
+  };
 }
