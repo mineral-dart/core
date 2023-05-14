9eca7dbd24e2e42e493ebf6261a85657fa9bad5e
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-05-14 16:27:00 +0200

    feat: Implement createInvite method

--- lib/src/api/channels/text_based_channel.dart
+++ lib/src/api/channels/text_based_channel.dart
@@ -1,6 +1,11 @@
+import 'dart:convert';
+
+import 'package:mineral/core.dart';
 import 'package:mineral/core/api.dart';
 import 'package:mineral/src/api/builders/channel_builder.dart';
+import 'package:mineral/src/api/invites/invite.dart';
 import 'package:mineral/src/api/managers/webhook_manager.dart';
+import 'package:mineral_ioc/ioc.dart';
 
 class TextBasedChannel extends PartialTextChannel {
   final bool _nsfw;
@@ -30,5 +35,19 @@ class TextBasedChannel extends PartialTextChannel {
   /// Allow or disallow nsfw of this
   Future<void> setNsfw(bool value) async {
     await update(ChannelBuilder({ 'nsfw': value}));
+  }
+
+  /// Create an [Invite] for this
+  /// ```dart
+  /// final Invite invite = await channel.createInvite();
+  /// print(invite.url);
+  /// ```
+  /// Channel should be an instance of [TextBasedChannel]
+  Future<Invite> createInvite() async {
+    final result = await ioc.use<DiscordApiHttpService>()
+      .post(url: '/channels/$id/invites')
+      .build();
+
+    return Invite.from(guild.id, jsonDecode(result.body));
   }
 }
