3621f4304653ce27aa806fd4659ee693955e57c9
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-05-18 21:36:25 +0200

    Added MineralSubCommand to exports

--- lib/framework.dart
+++ lib/framework.dart
@@ -1,7 +1,7 @@
 /// Contains the main services of the mineral framework as well as helper extensions
 library framework;
 
-export '../src/internal/entities/command.dart' show MineralCommand, CommandBuilder, Option, OptionType, OptionChoice, SubCommandBuilder, CommandGroupBuilder, Scope;
+export '../src/internal/entities/command.dart' show MineralCommand, MineralSubCommand, CommandBuilder, Option, OptionType, OptionChoice, SubCommandBuilder, CommandGroupBuilder, Scope;
 export '../src/internal/entities/context_menu.dart' show MineralContextMenu;
 export '../src/internal/entities/event.dart' show MineralEvent, Event;
 export '../src/internal/entities/state.dart' show MineralState;
