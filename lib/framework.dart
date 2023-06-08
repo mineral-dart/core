20d9bfbad570557c32b89d7f525e02b92d013da2
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-06-08 08:26:20 +0200

    feat: export some classes

--- lib/framework.dart
+++ lib/framework.dart
@@ -1,7 +1,16 @@
 /// Contains the main services of the mineral framework as well as helper extensions
 library framework;
 
-export '../src/internal/entities/command.dart' show MineralCommand, MineralSubCommand, CommandBuilder, Option, OptionType, OptionChoice, SubCommandBuilder, CommandGroupBuilder, Scope;
+export '../src/internal/entities/commands/mineral_command.dart' show MineralCommand;
+export '../src/internal/entities/commands/mineral_subcommand.dart' show MineralSubcommand;
+export '../src/internal/entities/commands/mineral_command_group.dart' show MineralCommandGroup;
+export '../src/internal/entities/commands/command_option.dart' show CommandOption;
+export '../src/internal/entities/commands/command_scope.dart' show CommandScope;
+export '../src/internal/entities/commands/abstract_command.dart' show AbstractCommand;
+export '../src/internal/entities/commands/command_type.dart' show CommandType;
+export '../src/internal/entities/commands/option_choice.dart' show OptionChoice;
+export '../src/internal/entities/commands/option_type.dart' show OptionType;
+
 export '../src/internal/entities/context_menu.dart' show MineralContextMenu;
 export '../src/internal/entities/event.dart' show MineralEvent, Event;
 export '../src/internal/entities/state.dart' show MineralState;
