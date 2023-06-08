af88026dfd5aafd48d18c7afa655622afbd5a8c5
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-06-08 09:17:37 +0200

    feat: implement option choice translations

--- lib/src/internal/entities/commands/option_choice.dart
+++ lib/src/internal/entities/commands/option_choice.dart
@@ -1,11 +1,15 @@
+import 'package:mineral/framework.dart';
+
 class OptionChoice {
   final String label;
   final String value;
+  final Translate? labelTranslation;
 
-  const OptionChoice({ required this.label, required this.value });
+  const OptionChoice({ required this.label, required this.value, this.labelTranslation });
 
   Map<String, dynamic> get serialize => {
     'name': label,
+    'name_localizations': labelTranslation != null ? labelTranslation!.serialize : null,
     'value': value
   };
 }
