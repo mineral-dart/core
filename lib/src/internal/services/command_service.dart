40b73258c4cf52d13374b7246ca0a3cd6e22cc3e
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-06-11 18:51:00 +0200

    Fix: commands with no subcommand / options error

--- lib/src/internal/services/command_service.dart
+++ lib/src/internal/services/command_service.dart
@@ -6,7 +6,6 @@ import 'package:mineral/core/events.dart';
 import 'package:mineral/framework.dart';
 import 'package:mineral_contract/mineral_contract.dart';
 import 'package:mineral_ioc/ioc.dart';
-import 'package:path/path.dart';
 
 class CommandService extends MineralService implements CommandServiceContract {
   final Map<String, AbstractCommand> _commands = {};
@@ -32,6 +31,7 @@ class CommandService extends MineralService implements CommandServiceContract {
   void register (List<MineralCommandContract> commands) {
     for (final command in List<MineralCommand>.from(commands)) {
       _commands.putIfAbsent(command.label.uid, () => command);
+      _handlers.putIfAbsent(command.label.uid, () => command); // todo that it ?
 
       if (command.subcommands.isNotEmpty) {
         _registerSubCommands(command.label.uid, command.scope ?? CommandScope.guild, command.subcommands, null);
@@ -54,6 +54,14 @@ class CommandService extends MineralService implements CommandServiceContract {
 
   String _buildIdentifier (Map<String, dynamic> payload) {
     if (payload['type'] == CommandType.subcommand.type && payload['id'] == null) {
+      return payload['name'];
+    }
+
+    if (payload['type'] == CommandType.group.type && payload['id'] == null) {
+      return payload['name'] += '.' + _buildIdentifier(payload['options'][0]);
+    }
+
+    if (payload['options'] == null) {
       return payload['name'];
     }
 
