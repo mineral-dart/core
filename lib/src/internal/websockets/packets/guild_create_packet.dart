44d3699c39e601a876a83f225726f0ccf0258966
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-08-31 00:55:56 +0200

    refractor: remove unusualy methods

--- lib/src/internal/websockets/packets/guild_create_packet.dart
+++ lib/src/internal/websockets/packets/guild_create_packet.dart
@@ -1,6 +1,3 @@
-import 'dart:convert';
-
-import 'package:http/http.dart';
 import 'package:mineral/core/api.dart';
 import 'package:mineral/core/events.dart';
 import 'package:mineral/src/internal/mixins/container.dart';
@@ -8,8 +5,6 @@ import 'package:mineral/src/internal/mixins/mineral_client.dart';
 import 'package:mineral/src/internal/services/event_service.dart';
 import 'package:mineral/src/internal/websockets/websocket_packet.dart';
 import 'package:mineral/src/internal/websockets/websocket_response.dart';
-
-import '../../../../core.dart';
 
 class GuildCreatePacket with Container implements WebsocketPacket  {
   @override
@@ -19,25 +14,5 @@ class GuildCreatePacket with Container implements WebsocketPacket  {
     Guild guild = await client.makeGuild(websocketResponse.payload);
 
     eventService.controller.add(GuildCreateEvent(guild));
-  }
-
-  Future<Map<Snowflake, ModerationRule>?> getAutoModerationRules (Guild guild) async {
-    Response response = await container.use<DiscordApiHttpService>()
-        .get(url: "/guilds/${guild.id}/auto-moderation/rules")
-        .build();
-
-    if (response.statusCode == 200) {
-      final payload = jsonDecode(response.body);
-
-      Map<Snowflake, ModerationRule> rules = {};
-      for (final element in payload) {
-        ModerationRule rule = ModerationRule.fromPayload(element);
-        rules.putIfAbsent(rule.id, () => rule);
-      }
-
-      return rules;
-    }
-
-    return null;
   }
 }
