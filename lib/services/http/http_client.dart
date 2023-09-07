11c0d89fa44a7ef3b7932e9a3377c0cf32f50951
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-09-07 16:05:13 +0200

    docs: write http documentation

--- lib/services/http/http_client.dart
+++ lib/services/http/http_client.dart
@@ -1,7 +1,14 @@
 import 'package:mineral/services/http/header_bucket.dart';
 
+/// HTTP Client used to make requests to a server
+/// ```dart
+/// final HttpClient client = HttpClient(baseUrl: '/');
+/// ```
 class HttpClient {
+  /// Base URL of this
   final String baseUrl;
+
+  /// Headers of this
   final HeaderBucket headers = HeaderBucket();
 
   HttpClient({ required this.baseUrl, Map<String, String> headers = const {} }) {
