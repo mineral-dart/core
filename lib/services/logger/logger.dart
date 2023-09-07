e318bc35565e208f60177be45f2d8945b79373b4
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-09-08 00:57:58 +0200

    feat: move console to logger

--- lib/services/logger/logger.dart
+++ lib/services/logger/logger.dart
@@ -1,23 +1,30 @@
 import 'dart:io';
 
 import 'package:mineral/services/logger/logger_contract.dart';
+import 'package:tint/tint.dart';
 
 class Logger implements LoggerContract {
   @override
-  void debug(String message) => stdout.writeln(message);
+  void debug(String message) =>
+      stdout.writeln('${'[ debug ]'.grey()} $message');
 
   @override
-  void error(String message) => stdout.writeln(message);
+  void error(String message) =>
+      stdout.writeln('${'[ error ]'.red()} $message');
 
   @override
-  void info(String message) => stdout.writeln(message);
+  void info(String message) =>
+      stdout.writeln('${'[ info ]'.cyan()} $message');
 
   @override
-  void log(message) => stdout.writeln(message);
+  void log(message) =>
+      stdout.writeln('[log] $message');
 
   @override
-  void success(String message) => stdout.writeln(message);
+  void success(String message) =>
+      stdout.writeln('${'[ success ]'.green()} $message');
 
   @override
-  void warning(String message) => stdout.writeln(message);
+  void warning(String message) =>
+      stdout.writeln('${'[ warning ]'.yellow()} $message');
 }
