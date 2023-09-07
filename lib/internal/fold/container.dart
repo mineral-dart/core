70073c20c4c739f34d0705e826aab5f9b34ad7b7
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-09-07 15:14:16 +0200

    docs: write docs

--- lib/internal/fold/container.dart
+++ lib/internal/fold/container.dart
@@ -1,8 +1,16 @@
 import 'package:mineral/internal/fold/injectable.dart';
 
+/// The Container package is one of the core components of the Mineral framework, in fact it registers the entire application through its Container.
 final class Container {
   final Map<String, dynamic> _services = {};
 
+  /// Registers a new injectable instance within the [Container]
+  /// ```dart
+  /// final container = Container();
+  ///
+  /// expect(container.bind('foo', (container) => Foo()), isA<Foo>());
+  /// expect(container.length(), 1);
+  /// ```
   T bind<T extends Injectable> (String binding, T Function(Container) injectable) {
     if (_services.containsKey(binding)) {
       throw Exception('Service $binding already exists');
@@ -14,6 +22,10 @@ final class Container {
     return injectableInstance;
   }
 
+  /// Resolve the service instance from its namespace
+  /// ```dart
+  /// final myService = container.use<MyService>('binding');
+  /// ```
   T use<T extends Injectable> (String binding) {
     if (!_services.containsKey(binding)) {
       throw Exception('Service $binding does not exist');
@@ -22,6 +34,10 @@ final class Container {
     return _services[binding];
   }
 
+  /// Deletes a service registered in the
+  /// /// ```dart
+  /// container.remove('binding');
+  /// ```
   void remove (String binding) {
     if (!_services.containsKey(binding)) {
       throw Exception('Service $binding does not exist');
