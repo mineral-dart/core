53b9bae660527601ce3c9e76156177b65f80a949
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-09-07 15:08:19 +0200

    test: write tests

--- test/core_test.dart
+++ test/core_test.dart
@@ -1,5 +1,7 @@
 import 'functionals/either_test.dart' as either_test;
+import 'functionals/container_test.dart' as container_test;
 
 void main () {
   either_test.main();
+  container_test.main();
 }
