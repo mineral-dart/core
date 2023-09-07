d4371e3074c549eb71c72747a710fe7672cb1367
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-09-07 11:45:16 +0200

    feat: enhance Either.future

---
+++ lib/internal/either.dart
@@ -0,0 +1,42 @@
+import 'dart:async';
+
+class Either<V, E> {
+  Either._();
+
+  factory Either.success(V value) = Success<V, E>;
+  factory Either.failure(E error, { StackTrace? stackTrace }) = Failure<V, E>;
+
+  static Future<Either<V, E>> future<V, E> ({ required Future future, Function(Failure<V, E>)? onError }) async {
+    try {
+      final result = await future;
+      return Success(result);
+    } catch (e, s) {
+      final failure = Failure<V, E>(e, stackTrace: s);
+
+      if (onError != null) {
+        return onError(failure);
+      }
+
+      return failure;
+    }
+  }
+
+  factory Either.tryCatch(V Function() run, Function(Failure) onError) {
+    try {
+      return Success(run());
+    } catch (e, s) {
+      return onError(Failure(e, stackTrace: s));
+    }
+  }
+}
+
+final class Success<V, E> extends Either<V, E> {
+  final V value;
+  Success(this.value): super._();
+}
+
+final class Failure<V, E> extends Either<V, E> {
+  final dynamic error;
+  final StackTrace? stackTrace;
+  Failure(this.error, { this.stackTrace }): super._();
+}
