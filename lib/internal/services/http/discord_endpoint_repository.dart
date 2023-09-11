313feea7c42e519d667d65209f8c0ec19cfd73b9
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-09-12 00:31:52 +0200

    feat: implement auto-moderation repository

--- lib/internal/services/http/discord_endpoint_repository.dart
+++ lib/internal/services/http/discord_endpoint_repository.dart
@@ -1,7 +1,14 @@
+import 'package:mineral/internal/services/http/repositories/auto_moderation_repository.dart';
 import 'package:mineral/internal/services/http/repositories/channel_repository.dart';
 import 'package:mineral/internal/services/http/repositories/emoji_repository.dart';
 
 final class DiscordEndpointRepository {
+  /// Get [Channel] endpoints
   final ChannelRepository channels = ChannelRepository();
+
+  /// Get [Emoji] endpoints
   final EmojiRepository emojis = EmojiRepository();
+
+  /// Get [AutoModeration] endpoints
+  final AutoModerationRepository moderation = AutoModerationRepository();
 }
---
+++ lib/internal/services/http/repositories/auto_moderation_repository.dart
@@ -0,0 +1,42 @@
+/// Repository for the [AutoModeration]
+///
+/// Related [official documentation](https://discord.com/developers/docs/resources/auto-moderation)
+final class AutoModerationRepository {
+  /// Returns a list of [AutoModeration] objects for the given [Guild].
+  /// Related to the official Discord API documentation: https://discord.com/developers/docs/resources/auto-moderation#list-auto-moderation-rules-for-guild
+  /// ```dart
+  /// final moderation = await client.moderation.list(guildId: '1234567890');
+  /// ```
+  String list(String guildId) => Uri(pathSegments: ['guilds', guildId, 'auto-moderation', 'rules']).path;
+
+  /// Returns an [AutoModeration] object for the given [Guild] and rule IDs.
+  /// Related to the official Discord API documentation: https://discord.com/developers/docs/resources/auto-moderation#get-auto-moderation-rule
+  /// ```dart
+  /// final moderation = await client.moderation.get(guildId: '1234567890', ruleId: '1234567890');
+  /// ```
+  String get(String guildId, String ruleId) =>
+      Uri(pathSegments: ['guilds', guildId, 'auto-moderation', 'rules', ruleId]).path;
+
+  /// Create a new [AutoModeration] for the given [Guild].
+  /// Related to the official Discord API documentation: https://discord.com/developers/docs/resources/auto-moderation#create-auto-moderation-rule
+  /// ```dart
+  /// final moderation = await client.moderation.create(guildId: '1234567890', ruleId: '1234567890');
+  /// ```
+  String create(String guildId) => Uri(pathSegments: ['guilds', guildId, 'auto-moderation', 'rules']).path;
+
+  /// Modify the given [AutoModeration].
+  /// Related to the official Discord API documentation: https://discord.com/developers/docs/resources/auto-moderation#modify-auto-moderation-rule
+  /// ```dart
+  /// final moderation = await client.moderation.update(guildId: '1234567890', ruleId: '1234567890');
+  /// ```
+  String update(String guildId, String ruleId) =>
+      Uri(pathSegments: ['guilds', guildId, 'auto-moderation', 'rules', ruleId]).path;
+
+  /// Delete the given [AutoModeration].
+  /// Related to the official Discord API documentation: https://discord.com/developers/docs/resources/auto-moderation#delete-auto-moderation-rule
+  /// ```dart
+  /// final moderation = await client.moderation.delete(guildId: '1234567890', ruleId: '1234567890');
+  /// ```
+  String delete(String guildId, String ruleId) =>
+      Uri(pathSegments: ['guilds', guildId, 'auto-moderation', 'rules', ruleId]).path;
+}
