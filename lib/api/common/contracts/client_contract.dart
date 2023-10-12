1d5cd4fa09d015f84f7951d2f7fe5811442efc05
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-10-13 00:57:18 +0200

    feat(api): implement client

--- lib/api/common/contracts/client_contract.dart
+++ lib/api/common/contracts/client_contract.dart
@@ -1,3 +1,4 @@
+import 'package:mineral/api/common/contracts/application_contract.dart';
 import 'package:mineral/api/common/contracts/user_contract.dart';
 import 'package:mineral/api/private/caches/private_channel_cache.dart';
 import 'package:mineral/api/server/caches/guild_cache.dart';
@@ -5,6 +6,10 @@ import 'package:mineral/api/server/caches/guild_cache.dart';
 abstract interface class ClientContract {
   abstract final GuildCache guilds;
   abstract final UserContract user;
-
+  abstract final int version;
+  abstract final String sessionType;
+  abstract final String sessionId;
+  abstract final String resumeGatewayUrl;
+  abstract final ApplicationContract application;
   abstract final PrivateChannelCache privateChannels;
 }
