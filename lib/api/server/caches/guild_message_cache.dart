0b0eb06ad2c76ba1dc433091681d5e70c5d33aaf
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-10-12 23:45:54 +0200

    feat(api): implement guild message cache

---
+++ lib/api/server/caches/guild_message_cache.dart
@@ -0,0 +1,14 @@
+import 'package:mineral/api/common/collection.dart';
+import 'package:mineral/api/common/contracts/cache_contract.dart';
+import 'package:mineral/api/common/snowflake.dart';
+import 'package:mineral/api/server/contracts/guild_message_contract.dart';
+
+final class GuildMessageCache implements CacheContract<GuildMessageContract> {
+  @override
+  final Collection<Snowflake, GuildMessageContract> cache = Collection();
+
+  @override
+  Future<GuildMessageContract> resolve(Snowflake id) async {
+    return cache.getOrFail(id);
+  }
+}
