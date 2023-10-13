d9d8015443bcafdd03c59596722b28a69207a9d9
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-10-13 21:07:21 +0200

    feat(embedded): enhance embedded production

--- lib/internal/app/embedded_production.dart
+++ lib/internal/app/embedded_production.dart
@@ -1,10 +1,16 @@
 import 'package:mineral/internal/app/contracts/embedded_application_contract.dart';
+import 'package:mineral/internal/wss/entities/websocket_event_dispatcher.dart';
 import 'package:mineral/internal/wss/entities/websocket_response.dart';
+import 'package:mineral/services/logger/logger_contract.dart';
 
 final class EmbeddedProduction implements EmbeddedApplication {
+  final WebsocketEventDispatcher dispatcher = WebsocketEventDispatcher();
+  final LoggerContract _logger;
+
+  EmbeddedProduction(this._logger);
+
   @override
   void dispatch(WebsocketResponse response) {
-    // TODO: implement dispatch
+    dispatcher.dispatch(response);
   }
-
 }
