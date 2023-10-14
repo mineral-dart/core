bd76fd7e9214d077b4a87ad34a79880163ac223f
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-10-14 22:42:56 +0200

    feat: introduce guild text channel contracts

--- lib/api/server/contracts/channels/guild_text_channel_contracts.dart
+++ lib/api/server/contracts/channels/guild_text_channel_contracts.dart
@@ -1,21 +1,19 @@
 import 'package:mineral/api/common/contracts/cache_contract.dart';
 import 'package:mineral/api/common/snowflake.dart';
 import 'package:mineral/api/common/contracts/channel_contract.dart';
+import 'package:mineral/api/server/contracts/channels/guild_channel_contracts.dart';
 import 'package:mineral/api/server/contracts/guild_message_contracts.dart';
 
-abstract interface class GuildTextChannelContract implements ChannelContract {
-  abstract final int guildId;
-  abstract final String topic;
+abstract interface class GuildTextChannelContract implements GuildChannelContract {
   abstract final bool isNsfw;
   abstract final int rateLimitPerUser;
   abstract final Snowflake? lastMessageId;
-  abstract final int? lastPinTimestamp;
-  abstract final int position;
-  abstract final int? parentId;
+  abstract final String? lastPinTimestamp;
   abstract final CacheContract<dynamic> webhooks;
   abstract final CacheContract<GuildMessageContract> messages;
+  abstract final Snowflake? parentId;
 
   Future<void> send();
-
-  Future<void> delete({ String? reason });
+  Future<void> setRateLimitPerUser(int rateLimitPerUser, { String? reason });
+  Future<void> setNsfw(bool isNsfw, { String? reason });
 }
