cb34327a81434d8888073992abf90ceae3fe4722
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-10-14 22:45:35 +0200

    edit: set vanity to public instance

--- lib/api/server/resources/vanity.dart
+++ lib/api/server/resources/vanity.dart
@@ -2,10 +2,10 @@ class Vanity { // todo set extends to invite ?
   String code;
   String? uses;
 
-  Vanity._(this.code, this.uses);
+  Vanity(this.code, this.uses);
 
   factory Vanity.from(final payload) {
-    return Vanity._(
+    return Vanity(
       payload['code'],
       payload['uses'],
     );
