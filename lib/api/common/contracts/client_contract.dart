a88dd8a5eb8eabb1e63ae9fa9e3dd94cd8180977
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-10-14 22:45:56 +0200

    edit: change to contract guild cache

--- lib/api/common/contracts/client_contract.dart
+++ lib/api/common/contracts/client_contract.dart
@@ -1,11 +1,13 @@
 import 'package:mineral/api/common/contracts/application_contract.dart';
+import 'package:mineral/api/common/contracts/cache_contract.dart';
 import 'package:mineral/api/common/contracts/user_contract.dart';
 import 'package:mineral/api/private/caches/private_channel_cache.dart';
 import 'package:mineral/api/server/caches/guild_cache.dart';
+import 'package:mineral/api/server/contracts/guild_contracts.dart';
 import 'package:mineral/internal/fold/injectable.dart';
 
-abstract interface class ClientContract extends Injectable {
-  abstract final GuildCache guilds;
+abstract interface class ClientContract extends Injectable  {
+  abstract final CacheContract<GuildContract> guilds;
   abstract final UserContract user;
   abstract final int version;
   abstract final String sessionType;
