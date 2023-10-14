57826ad62207b3bed684080a1c8741a7a6a852fe
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-10-14 22:41:42 +0200

    edit: change channel contract to guild channel contract

--- lib/api/server/caches/guild_channels_cache.dart
+++ lib/api/server/caches/guild_channels_cache.dart
@@ -1,19 +1,19 @@
 import 'package:mineral/api/common/collection.dart';
 import 'package:mineral/api/common/contracts/cache_contract.dart';
-import 'package:mineral/api/common/contracts/channel_contract.dart';
 import 'package:mineral/api/common/snowflake.dart';
+import 'package:mineral/api/server/contracts/channels/guild_channel_contracts.dart';
 import 'package:mineral/api/server/contracts/guild_contracts.dart';
 
-final class GuildChannelsCache implements CacheContract<ChannelContract> {
+final class GuildChannelsCache implements CacheContract<GuildChannelContract> {
   final GuildContract _guild;
 
   GuildChannelsCache(this._guild);
 
   @override
-  final Collection<Snowflake, ChannelContract> cache = Collection();
+  final Collection<Snowflake, GuildChannelContract> cache = Collection();
 
   @override
-  Future<ChannelContract> resolve(Snowflake id) async {
+  Future<GuildChannelContract> resolve(Snowflake id) async {
     // todo implement request
     return cache.getOrFail(id);
   }
