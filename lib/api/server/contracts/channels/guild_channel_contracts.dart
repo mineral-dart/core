298aa33a4bbc474d4603eea3a63078f8d844a287
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-10-14 22:42:48 +0200

    feat: introduce guild channel contracts

---
+++ lib/api/server/contracts/channels/guild_channel_contracts.dart
@@ -0,0 +1,16 @@
+import 'package:mineral/api/common/contracts/channel_contract.dart';
+import 'package:mineral/api/common/snowflake.dart';
+import 'package:mineral/api/server/contracts/guild_contracts.dart';
+
+abstract interface class GuildChannelContract implements ChannelContract {
+  abstract final Snowflake guildId;
+  abstract final String? topic;
+  abstract final int? position;
+
+  GuildContract get guild;
+  Future<void> setName(String name, { String? reason });
+  Future<void> delete({ String? reason });
+  Future<void> setParent(Snowflake parentId, { String? reason });
+  Future<void> setPosition(int position, { String? reason });
+  Future<void> setTopic(String topic, { String? reason });
+}
