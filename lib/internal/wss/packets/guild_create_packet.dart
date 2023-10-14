d79b8a1e2545cda227f9f4ad5d00c22d7fae24ca
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-10-14 22:57:49 +0200

    feat(events): add event factory to GuildCreate packet constructor

--- lib/internal/wss/packets/guild_create_packet.dart
+++ lib/internal/wss/packets/guild_create_packet.dart
@@ -1,9 +1,13 @@
-import 'package:mineral/api/common/client/client.dart';
-import 'package:mineral/internal/fold/container.dart';
+import 'package:mineral/internal/factories/event_factory.dart';
 import 'package:mineral/internal/wss/contracts/packet_contract.dart';
 import 'package:mineral/internal/wss/entities/websocket_response.dart';
 
 final class GuildCreatePacket implements PacketContract {
+  @override
+  final EventFactory eventFactory;
+
+  GuildCreatePacket(this.eventFactory);
+
   @override
   Future<void> handle(WebsocketResponse response) async {
     final { "id": id } = response.payload;
