a68941f43c05326e9aee3ba74335b1dbc3a6763d
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-10-14 22:40:23 +0200

    feat: guild object

--- lib/api/server/guild.dart
+++ lib/api/server/guild.dart
@@ -1,61 +1,127 @@
 import 'package:mineral/api/common/resources/image.dart';
 import 'package:mineral/api/common/snowflake.dart';
+import 'package:mineral/api/common/user/user.dart';
 import 'package:mineral/api/server/caches/guild_channels_cache.dart';
 import 'package:mineral/api/server/caches/guild_emojis_cache.dart';
 import 'package:mineral/api/server/caches/guild_members_cache.dart';
 import 'package:mineral/api/server/caches/guild_roles_cache.dart';
-import 'package:mineral/api/server/channels/guild_text_channel.dart';
+import 'package:mineral/api/server/contracts/channels/guild_text_channel_contracts.dart';
 import 'package:mineral/api/server/contracts/guild_contracts.dart';
-import 'package:mineral/api/server/contracts/guild_voice_channel_contracts.dart';
+import 'package:mineral/api/server/contracts/channels/guild_voice_channel_contracts.dart';
 import 'package:mineral/api/server/resources/enums.dart';
 import 'package:mineral/api/server/resources/vanity.dart';
 
 final class Guild implements GuildContract {
-  final String id;
-  String label;
-  String? description;
-  String ownerId;
+  @override
+  final Snowflake id;
 
+  @override
+  final String label;
+
+  @override
+  final String? description;
+
+  @override
+  final String ownerId;
+
+  @override
   late final GuildMemberCache members;
+
+  @override
   late final GuildRolesCache roles;
+
+  @override
   late final GuildChannelsCache channels;
+
+  @override
   late final GuildEmojisCache emojis;
 
-  Image? icon;
-  Image? banner;
-  Image? splash;
+  @override
+  final Image? icon;
 
-  String? afkChannelId;
-  int? afkTimeout;
+  @override
+  final Image? banner;
 
-  String? rulesChannelId;
-  String? publicUpdatesChannelId;
-  String? safetyAlertsChannelId;
-  String? systemChannelId;
-  int? systemChannelFlags;
+  @override
+  final Image? splash;
 
-  bool? widgetEnabled;
-  String? widgetChannelId;
+  final String? afkChannelId;
+  final int? afkTimeout;
 
-  VerificationLevel verificationLevel;
-  NotificationLevel defaultNotificationLevel;
-  ContentFilterLevel explicitContentFilter;
-  NsfwLevel nsfwLevel;
-  MfaLevel mfaLevel;
-  PremiumTier premiumTier;
-  Locale preferredLocale;
-  List<GuildFeature> features;
-  Vanity? vanity;
+  @override
+  final String? rulesChannelId;
 
-  String? applicationId;
-  int? maxPresences;
-  int? maxMembers;
-  int? premiumSubscriptionCount;
-  int? maxVideoChannelUsers;
-  int? maxStageVideoChannelUsers;
-  int? approximateMemberCount;
-  int? approximatePresenceCount;
-  bool premiumProgressBarEnabled;
+  @override
+  final String? publicUpdatesChannelId;
+
+  @override
+  final String? safetyAlertsChannelId;
+
+  @override
+  final String? systemChannelId;
+
+  @override
+  final int? systemChannelFlags;
+
+  @override
+  final bool? widgetEnabled;
+
+  @override
+  final String? widgetChannelId;
+
+  @override
+  final VerificationLevel verificationLevel;
+
+  @override
+  final NotificationLevel defaultNotificationLevel;
+
+  @override
+  final ContentFilterLevel explicitContentFilter;
+
+  @override
+  final NsfwLevel nsfwLevel;
+
+  @override
+  final MfaLevel mfaLevel;
+
+  @override
+  final PremiumTier premiumTier;
+
+  @override
+  final Locale preferredLocale;
+
+  @override
+  final List<GuildFeature> features;
+
+  @override
+  final Vanity? vanity;
+
+  @override
+  final String? applicationId;
+
+  @override
+  final int? maxPresences;
+
+  @override
+  final int? maxMembers;
+
+  @override
+  final int? premiumSubscriptionCount;
+
+  @override
+  final int? maxVideoChannelUsers;
+
+  @override
+  final int? maxStageVideoChannelUsers;
+
+  @override
+  final int? approximateMemberCount;
+
+  @override
+  final int? approximatePresenceCount;
+
+  @override
+  final bool premiumProgressBarEnabled;
 
   Guild._({
     required this.id,
@@ -91,6 +157,8 @@ final class Guild implements GuildContract {
     required this.approximateMemberCount,
     required this.approximatePresenceCount,
     required this.premiumProgressBarEnabled,
+    required this.vanity,
+
   }) {
     members = GuildMemberCache(this);
     channels = GuildChannelsCache(this);
@@ -98,12 +166,14 @@ final class Guild implements GuildContract {
     emojis = GuildEmojisCache(this);
   }
 
-  GuildTextChannel? get systemChannel => channels.cache.get(systemChannelId.toSnowflake()) as GuildTextChannel;
-  GuildTextChannel? get publicUpdatesChannel => channels.cache.get(publicUpdatesChannelId.toSnowflake()) as GuildTextChannel;
-  GuildTextChannel? get safetyAlertsChannel => channels.cache.get(safetyAlertsChannelId.toSnowflake()) as GuildTextChannel;
-  GuildTextChannel? get widgetChannel => channels.cache.get(widgetChannelId.toSnowflake()) as GuildTextChannel;
-  GuildVoiceChannelContract? get afkChannel => channels.cache.get(afkChannelId.toSnowflake()) as GuildVoiceChannelContract;
-  GuildVoiceChannelContract? get rulesChannel => channels.cache.get(rulesChannelId.toSnowflake()) as GuildVoiceChannelContract;
+  GuildTextChannelContract? get systemChannel => channels.cache.get(systemChannelId.toSnowflake()) as GuildTextChannelContract?;
+  GuildTextChannelContract? get publicUpdatesChannel => channels.cache.get(publicUpdatesChannelId.toSnowflake()) as GuildTextChannelContract?;
+  GuildTextChannelContract? get safetyAlertsChannel => channels.cache.get(safetyAlertsChannelId.toSnowflake()) as GuildTextChannelContract?;
+  GuildTextChannelContract? get widgetChannel => channels.cache.get(widgetChannelId.toSnowflake()) as GuildTextChannelContract?;
+  GuildTextChannelContract? get rulesChannel => channels.cache.get(rulesChannelId.toSnowflake()) as GuildTextChannelContract?;
+  GuildVoiceChannelContract? get afkChannel => channels.cache.get(afkChannelId.toSnowflake()) as GuildVoiceChannelContract?;
+
+  User get owner => members.cache.getOrFail(Snowflake(ownerId)).user;
 
   /// [Guild] to Json method is private
   dynamic _toJson() {
@@ -132,7 +202,7 @@ final class Guild implements GuildContract {
   }
 
   // not tested
-  factory Guild.fromWebsocket(final payload) {
+  factory Guild.fromWss(final payload) {
     List<GuildFeature> features = [];
 
     if (payload["features"] != null) {
@@ -143,7 +213,7 @@ final class Guild implements GuildContract {
     }
 
     return Guild._(
-        id: payload["id"],
+        id: payload["id"].toString().toSnowflake(),
         label: payload["name"],
         description: payload["description"],
         ownerId: payload["owner_id"],
@@ -175,7 +245,8 @@ final class Guild implements GuildContract {
         maxStageVideoChannelUsers: payload["max_stage_video_channel_users"],
         approximateMemberCount: payload["approximate_member_count"],
         approximatePresenceCount: payload["approximate_presence_count"],
-        premiumProgressBarEnabled: payload["premium_progress_bar_enabled"]
+        premiumProgressBarEnabled: payload["premium_progress_bar_enabled"],
+        vanity: payload['vanity_url_code'] != null ? Vanity(payload["vanity_url_code"], null) : null,
     );
   }
 }
