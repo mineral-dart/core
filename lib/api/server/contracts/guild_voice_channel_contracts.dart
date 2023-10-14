5bbf4b8d4ff419506cf721d36f80c50a0c136ce3
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2023-10-14 22:44:43 +0200

    feat: introduce guild voice channel contracts

--- lib/api/server/contracts/guild_voice_channel_contracts.dart
+++
@@ -1,8 +0,0 @@
-abstract interface class GuildVoiceChannelContract {
-  abstract final String id;
-  abstract String name;
-  abstract int position;
-  abstract int bitrate;
-  abstract int userLimit;
-  abstract String? parentId;
-}
