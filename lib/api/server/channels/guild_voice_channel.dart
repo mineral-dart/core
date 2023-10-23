fcd66f9c7c450390ef2d92210a2a3e51c2315e1c
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-10-24 00:14:24 +0200

    feat(voice channel): implement method definitions

--- lib/api/server/channels/guild_voice_channel.dart
+++ lib/api/server/channels/guild_voice_channel.dart
@@ -1,8 +1,12 @@
 import 'package:mineral/api/common/client/client.dart';
 import 'package:mineral/api/common/snowflake.dart';
+import 'package:mineral/api/server/builders/guild_voice_channel_builder.dart';
 import 'package:mineral/api/server/contracts/channels/guild_voice_channel_contracts.dart';
 import 'package:mineral/api/server/contracts/guild_contracts.dart';
 import 'package:mineral/internal/fold/container.dart';
+import 'package:mineral/internal/services/http/discord_http_client.dart';
+import 'package:mineral/services/http/entities/either.dart';
+import 'package:mineral/services/http/entities/http_error.dart';
 
 final class GuildVoiceChannel implements GuildVoiceChannelContract {
   @override
@@ -13,9 +17,6 @@ final class GuildVoiceChannel implements GuildVoiceChannelContract {
 
   @override
   final Snowflake guildId;
-
-  @override
-  final String? topic;
 
   @override
   final int? position;
@@ -33,25 +34,47 @@ final class GuildVoiceChannel implements GuildVoiceChannelContract {
   GuildContract get guild => container.use<Client>('client').guilds.cache.getOrFail(guildId);
 
   @override
+  Future<void> update (GuildVoiceChannelBuilder builder, { String? reason }) async {
+    final http = DiscordHttpClient.singleton();
+
+    final request = http.patch('/channels/${id.value}')
+      .payload(builder.build())
+      .build();
+
+    await Either.future(
+        future: request,
+      onError: (HttpError error) => switch(error) {
+        HttpError(message: final message)
+          => throw ArgumentError(message),
+      }
+    );
+  }
+
+  @override
   Future<void> delete({ String? reason }) async {}
 
   @override
-  Future<void> setParent(Snowflake parentId, { String? reason }) async {}
+  Future<void> setParent(Snowflake parentId, { String? reason }) async {
+    GuildVoiceChannelBuilder builder = GuildVoiceChannelBuilder();
+    return update(builder.setParent(parentId), reason: reason);
+  }
 
   @override
-  Future<void> setPosition(int position, { String? reason }) async {}
+  Future<void> setPosition(int position, { String? reason }) async {
+    GuildVoiceChannelBuilder builder = GuildVoiceChannelBuilder();
+    return update(builder.setPosition(position), reason: reason);
+  }
 
   @override
-  Future<void> setTopic(String topic, { String? reason }) async {}
-
-  @override
-  Future<void> setName(String name, { String? reason }) async {}
+  Future<void> setName(String name, { String? reason }) async {
+    GuildVoiceChannelBuilder builder = GuildVoiceChannelBuilder();
+    return update(builder.setName(name), reason: reason);
+  }
 
   GuildVoiceChannel._({
     required this.id,
     required this.name,
     required this.guildId,
-    required this.topic,
     required this.position,
     required this.parentId,
     required this.bitrate,
@@ -63,7 +86,6 @@ final class GuildVoiceChannel implements GuildVoiceChannelContract {
       id: Snowflake(payload['id']),
       name: payload['name'],
       guildId: guild.id,
-      topic: payload['topic'],
       position: payload['position'],
       parentId: payload['parent_id'] != null
           ? Snowflake(payload['parent_id'])
