0312b6706ccc3c3d4148d4d0f8e46bc24e8686ef
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-12-01 20:20:33 +0100

    feat(api): implement private channel structure

---
+++ lib/api/api/private/channels/private_channel.dart
@@ -0,0 +1,14 @@
+import 'package:mineral/api/api/common/channel.dart';
+
+final class PrivateChannel implements Channel {
+  @override
+  final String id;
+
+  @override
+  final String name;
+
+  PrivateChannel({
+    required this.id,
+    required this.name,
+  });
+}
