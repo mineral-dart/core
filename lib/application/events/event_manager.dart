3488938382eb1d2217e09389400c4b1a65b66e8d
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-12-26 21:59:52 +0100

    feat(dispatcher): changed name to packet dispatcher

--- lib/application/events/event_manager.dart
+++ lib/application/events/event_manager.dart
@@ -1,23 +1,23 @@
 import 'package:mineral/application/events/dispatchers/event_dispatcher.dart';
-import 'package:mineral/application/events/dispatchers/payload_dispatcher.dart';
+import 'package:mineral/application/events/dispatchers/packet_dispatcher.dart';
 import 'package:mineral/application/events/types/listenable.dart';
 
 abstract interface class EventManagerContract {
-  Listenable get payloads;
+  Listenable get packets;
   Listenable get events;
   void dispose();
 }
 
 final class EventManager implements EventManagerContract {
   @override
-  final PayloadDispatcher payloads = PayloadDispatcher();
+  final PacketDispatcher packets = PacketDispatcher();
 
   @override
   final EventDispatcher events = EventDispatcher();
 
   @override
   void dispose() {
-    payloads.dispose();
+    packets.dispose();
     events.dispose();
   }
 }
