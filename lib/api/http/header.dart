ff9663fa6c871b503af807362791af239fbbbd05
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-12-26 21:17:17 +0100

    chore(http): changed HeaderImpl to Header

--- lib/api/http/header.dart
+++ lib/api/http/header.dart
@@ -1,19 +1,11 @@
-abstract interface class Header {
-  abstract final String key;
-  abstract final String value;
-}
-
-final class HeaderImpl implements Header {
-  @override
+final class Header {
   final String key;
-
-  @override
   final String value;
 
-  HeaderImpl(this.key, this.value);
+  Header(this.key, this.value);
 
-  HeaderImpl.contentType(String value) : this('Content-Type', value);
-  HeaderImpl.accept(String value) : this('Accept', value);
-  HeaderImpl.authorization(String value) : this('Authorization', value);
-  HeaderImpl.userAgent(String value) : this('User-Agent', value);
+  Header.contentType(String value) : this('Content-Type', value);
+  Header.accept(String value) : this('Accept', value);
+  Header.authorization(String value) : this('Authorization', value);
+  Header.userAgent(String value) : this('User-Agent', value);
 }
