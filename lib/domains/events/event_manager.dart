75bb2323961803abf83ab8cf26322c813279e0e1
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-12-26 22:35:44 +0100

    refactor(event): changed filename to ListenableDispatcher

--- lib/domains/events/event_manager.dart
+++ lib/domains/events/event_manager.dart
@@ -1,6 +1,6 @@
 import 'package:mineral/domains/events/dispatchers/event_dispatcher.dart';
 import 'package:mineral/domains/events/dispatchers/packet_dispatcher.dart';
-import 'package:mineral/domains/events/types/listenable.dart';
+import 'package:mineral/domains/events/types/listenable_dispatcher.dart';
 import 'package:mineral/domains/wss/shard_message.dart';
 
 abstract interface class EventManagerContract {
