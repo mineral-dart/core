549db8fad1944892c198b145d1457ed58c244d1a
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-12-29 11:58:56 +0100

    feat(api): added bot params declaration

--- lib/domains/data/events/ready_event.dart
+++ lib/domains/data/events/ready_event.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
 
+import 'package:mineral/api/common/bot.dart';
 import 'package:mineral/domains/data/types/listenable_event.dart';
 
 abstract class ReadyEvent implements ListenableEvent {
   @override
   String get event => 'ReadyEvent';
 
-  FutureOr<void> handle();
+  FutureOr<void> handle(Bot bot);
 }
