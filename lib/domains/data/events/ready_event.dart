d429381da8993054877c42328e019d69d55184fd
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2023-12-30 12:34:43 +0100

    feat(event): enforce type safety

--- lib/domains/data/events/ready_event.dart
+++ lib/domains/data/events/ready_event.dart
@@ -3,6 +3,8 @@ import 'dart:async';
 import 'package:mineral/api/common/bot.dart';
 import 'package:mineral/domains/data/types/listenable_event.dart';
 
+typedef ReadyEventHandler = FutureOr<void> Function(Bot bot);
+
 abstract class ReadyEvent implements ListenableEvent {
   @override
   String get event => 'ReadyEvent';
--- lib/domains/data/functional_event_registrar.dart
+++ lib/domains/data/functional_event_registrar.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
-
-import 'package:http/http.dart';
-import 'package:mineral/api/common/bot.dart';
+import 'package:mineral/domains/data/events/ready_event.dart';
 import 'package:mineral/domains/data/internal_event.dart';
 import 'package:mineral/domains/data/types/packet_type.dart';
 import 'package:mineral/domains/shared/types/mineral_client_contract.dart';
@@ -9,7 +6,7 @@ import 'package:mineral/domains/shared/types/mineral_client_contract.dart';
 abstract interface class FunctionalEventRegistrarContract {
   void make(String event, Function() handle);
 
-  void ready(FutureOr<void> Function(Bot bot) handle);
+  void ready(ReadyEventHandler handle);
 }
 
 final class FunctionalEventRegistrar implements FunctionalEventRegistrarContract {
@@ -21,7 +18,7 @@ final class FunctionalEventRegistrar implements FunctionalEventRegistrarContract
   void make(String event, Function() handle) => _registerEvent(InternalEvent(event, handle));
 
   @override
-  void ready(FutureOr<void> Function(Bot bot) handle) => _registerEvent(InternalEvent(PacketType.ready.toString(), handle));
+  void ready(ReadyEventHandler handle) => _registerEvent(InternalEvent(PacketType.ready.toString(), handle));
 
   void _registerEvent(InternalEvent event) =>
       _client.kernel.eventManager.events.listen(event);
