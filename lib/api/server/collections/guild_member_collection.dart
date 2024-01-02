ad2ad3be73214fe34c02c5d5338a14efc26281fd
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-02 19:23:21 +0100

    feat(api): added member collection

---
+++ lib/api/server/collections/guild_member_collection.dart
@@ -0,0 +1,20 @@
+import 'package:mineral/api/server/collections/role_collection.dart';
+import 'package:mineral/api/server/guild_member.dart';
+
+final class GuildMemberCollection {
+  final Map<String, GuildMember> _members;
+
+  GuildMemberCollection(this._members);
+
+  Map<String, GuildMember> get list => _members;
+
+  factory GuildMemberCollection.fromJson(
+      {required String guildId,
+      required RoleCollection roles,
+      required List<Map<String, dynamic>> json}) {
+    return GuildMemberCollection(Map<String, GuildMember>.from(json.fold({}, (value, element) {
+      final member = GuildMember.fromJson(roles: roles, member: element, guildId: guildId);
+      return {...value, member.id: member};
+    })));
+  }
+}
