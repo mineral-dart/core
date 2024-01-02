65852185bf03503b0c6ebc2507dccb1c17783d64
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-02 19:24:12 +0100

    feat(api): implemented factories

--- lib/api/server/channels/guild_announcement_channel.dart
+++ lib/api/server/channels/guild_announcement_channel.dart
@@ -15,16 +15,25 @@ final class GuildAnnouncementChannel implements GuildChannel {
   final String guildId;
 
   @override
-  final Guild guild;
+  late final Guild guild;
 
-  final String description;
+  final String? description;
 
   GuildAnnouncementChannel({
     required this.id,
     required this.name,
     required this.position,
     required this.guildId,
-    required this.guild,
     required this.description,
   });
+
+  factory GuildAnnouncementChannel.fromJson(String guildId, Map<String, dynamic> json) {
+    return GuildAnnouncementChannel(
+      id: json['id'],
+      name: json['name'],
+      position: json['position'],
+      guildId: guildId,
+      description: json['description'],
+    );
+  }
 }
