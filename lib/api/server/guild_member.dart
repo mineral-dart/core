4a185645472c28822ecd5de11b23f12d481f36e1
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-02 19:27:20 +0100

    feat(api): enhanced types and implemented factory

--- lib/api/server/guild_member.dart
+++ lib/api/server/guild_member.dart
@@ -1,37 +1,60 @@
-import 'package:mineral/api/server/guild.dart';
+import 'package:mineral/api/common/avatar_decoration.dart';
+import 'package:mineral/api/server/collections/role_collection.dart';
+import 'package:mineral/api/server/role.dart';
+import 'package:mineral/domains/shared/utils.dart';
 
 final class GuildMember {
   final String id;
   final String username;
+  final String? nickname;
+  final String? globalName;
   final String discriminator;
   final String? avatar;
-  final bool? bot;
-  final bool? system;
-  final bool? mfaEnabled;
-  final String? locale;
-  final bool? verified;
-  final String? email;
+  final AvatarDecoration? avatarDecoration;
   final int? flags;
-  final int? premiumType;
+  final DateTime? premiumSince;
   final int? publicFlags;
   final String guildId;
-  final Guild guild;
+  final RoleCollection roles;
+  final bool isBot;
 
-  GuildMember({
+  GuildMember._({
     required this.id,
     required this.username,
+    required this.nickname,
+    required this.globalName,
     required this.discriminator,
     required this.avatar,
-    required this.bot,
-    required this.system,
-    required this.mfaEnabled,
-    required this.locale,
-    required this.verified,
-    required this.email,
+    required this.avatarDecoration,
     required this.flags,
-    required this.premiumType,
+    required this.premiumSince,
     required this.publicFlags,
     required this.guildId,
-    required this.guild,
+    required this.roles,
+    required this.isBot,
   });
+
+  factory GuildMember.fromJson(
+      {required String guildId,
+      required RoleCollection roles,
+      required Map<String, dynamic> member}) {
+    return GuildMember._(
+      id: member['user']['id'],
+      username: member['user']['nick'] ?? member['user']['username'],
+      nickname: member['nick'] ?? member['user']['display_name'],
+      globalName: member['user']['global_name'],
+      discriminator: member['user']['discriminator'],
+      avatar: member['avatar'],
+      avatarDecoration: createOrNull(
+          field: member['user']?['avatar_decoration_data'],
+          fn: () => AvatarDecoration.fromJson(member['user']['avatar_decoration_data'])),
+      flags: member['flags'],
+      premiumSince: createOrNull(
+          field: member['premium_since'], fn: () => DateTime.parse(member['premium_since'])),
+      publicFlags: member['user']['public_flags'],
+      guildId: guildId,
+      roles: RoleCollection.fromJson(roles.list, List<String>.from(member['roles'])),
+      isBot: member['user']['bot'],
+    );
+  }
 }
