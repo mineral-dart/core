1c5ebe1b2eb3771f4fbd8f5ba7c05dad4f2b5ac8
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-02 19:23:53 +0100

    feat(api): added true types and implemented factory

--- lib/api/common/emoji.dart
+++ lib/api/common/emoji.dart
@@ -4,8 +4,7 @@ import 'package:mineral/api/server/role.dart';
 
 final class Emoji extends PartialEmoji {
   final String? globalName;
-  final List<Role> roles;
-  final User? user;
+  final Map<String, Role> roles;
   final bool managed;
   final bool animated;
   final bool available;
@@ -15,12 +14,32 @@ final class Emoji extends PartialEmoji {
     required String name,
     required this.globalName,
     required this.roles,
-    required this.user,
     required this.managed,
     required this.animated,
     required this.available,
-  }) : super(
-          id: id,
-          name: name,
-        );
+  }) : super(id, name);
+
+  factory Emoji.fromJson(
+      {required Map<String, Role> guildRoles, required Map<String, dynamic> json}) {
+    final Map<String, Role> roles = List<String>.from(json['roles']).fold({}, (value, element) {
+      final role = guildRoles[element];
+
+      if (role == null) {
+        // Todo add report case
+        return value;
+      }
+
+      return {...value, role.id: role};
+    });
+
+    return Emoji(
+      id: json['id'],
+      name: json['name'],
+      globalName: json['global_name'],
+      roles: roles,
+      managed: json['managed'],
+      animated: json['animated'],
+      available: json['available'],
+    );
+  }
 }
