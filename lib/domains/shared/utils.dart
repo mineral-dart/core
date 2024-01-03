e488970a8b738183808fe0d7ce4a61894f68397a
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-03 20:44:59 +0100

    feat(api): implemented system channel flag flags

--- lib/domains/shared/utils.dart
+++ lib/domains/shared/utils.dart
@@ -1,1 +1,14 @@
+import 'package:mineral/api/common/types/flag.dart';
+
 T? createOrNull<T>({ required dynamic field, required T? Function() fn }) => field != null ? fn() : null;
+
+List<T> bitfieldToList<T extends Flag> (List<T> values, int bitfield) {
+  final List<T> flags = [];
+
+  for (final element in values) {
+    if ((bitfield & element.value) == element.value) {
+      flags.add(element);
+    }
+  }
+  return flags;
+}
