6ecd29d431d9fec1f0861f5f494ceace3378fe03
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-06 00:31:00 +0100

    feat(memory): implemented memory storage

--- lib/domains/data/packets/message_create_packet.dart
+++ lib/domains/data/packets/message_create_packet.dart
@@ -1,6 +1,7 @@
 import 'dart:convert';
 
 import 'package:mineral/domains/data/internal_event_params.dart';
+import 'package:mineral/domains/data/memory/memory_storage.dart';
 import 'package:mineral/domains/data/types/listenable_packet.dart';
 import 'package:mineral/domains/data/types/packet_type.dart';
 import 'package:mineral/domains/wss/shard_message.dart';
@@ -8,6 +9,9 @@ import 'package:mineral/domains/wss/shard_message.dart';
 final class MessageCreatePacket implements ListenablePacket {
   @override
   PacketType get event => PacketType.messageCreate;
+
+  final MemoryStorageContract storage;
+  const MessageCreatePacket(this.storage);
 
   @override
   void listen(Map<String, dynamic> payload) {
