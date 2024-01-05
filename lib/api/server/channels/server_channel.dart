2aeddba43857957463ba55a0d5d2435b7f53acdf
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-06 00:08:49 +0100

    refactor(collections): move stickers and emoji into server assets

--- lib/api/server/channels/server_channel.dart
+++ lib/api/server/channels/server_channel.dart
@@ -2,7 +2,7 @@ import 'package:mineral/api/common/channel.dart';
 import 'package:mineral/api/server/server.dart';
 
 abstract class ServerChannel extends Channel {
-  late final Server guild;
+  late final Server server;
   final int position;
 
   ServerChannel(String id, String name, this.position) : super(id, name);
--- lib/api/server/server_assets.dart
+++ lib/api/server/server_assets.dart
@@ -1,21 +1,31 @@
 import 'package:mineral/api/common/image_asset.dart';
+import 'package:mineral/api/server/managers/emoji_manager.dart';
+import 'package:mineral/api/server/managers/role_manager.dart';
+import 'package:mineral/api/server/managers/sticker_manager.dart';
 import 'package:mineral/domains/shared/utils.dart';
 
 final class ServerAsset {
+  final EmojiManager emojis;
+  final StickerManager stickers;
   final ImageAsset? icon;
   final ImageAsset? splash;
   final ImageAsset? banner;
   final ImageAsset? discoverySplash;
 
   ServerAsset({
-    this.icon,
-    this.splash,
-    this.banner,
-    this.discoverySplash,
+    required this.emojis,
+    required this.stickers,
+    required this.icon,
+    required this.splash,
+    required this.banner,
+    required this.discoverySplash,
   });
 
-  factory ServerAsset.fromJson(Map<String, dynamic> json) {
+  factory ServerAsset.fromJson(RoleManager roles, Map<String, dynamic> json) {
+
     return ServerAsset(
+      emojis: EmojiManager.fromJson(roles: roles, json: json['emojis']),
+      stickers: StickerManager.fromJson(json['stickers']),
       icon: createOrNull(
           field: json['icon'], fn: () => ImageAsset(['icons', json['id']], json['icon'])),
       splash: createOrNull(
