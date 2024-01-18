c3c9f4fd3a30771e93799ba26dbdccbbeb06ffca
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-18 19:50:55 +0100

    feat(placeholder): implemented placeholder

---
+++ lib/application/placeholder/environment_placeholder.dart
@@ -0,0 +1,37 @@
+import 'package:mineral/application/environment/environment.dart';
+import 'package:mineral/application/placeholder/placeholder.dart';
+import 'package:recase/recase.dart';
+
+final class EnvironmentPlaceholder implements PlaceholderContract {
+  final Map<String, dynamic> _values = {};
+
+  @override
+  String get identifier => 'env';
+
+  @override
+  Map<String, dynamic> get values => _values;
+
+  EnvironmentPlaceholder({required EnvironmentContract environment}) {
+    _injectEntryMap(identifier, environment.list);
+  }
+
+  void _injectEntryMap(String identifier, Map<String, dynamic> values) {
+    final mapEntry = Map<String, dynamic>.from(values.map((key, value) {
+      final currentKey = key.snakeCase.toUpperCase();
+      return MapEntry('$identifier.$currentKey', value);
+    }));
+
+    _values.addAll(mapEntry);
+  }
+
+  @override
+  String apply(String value) => values.entries.fold(value, (acc, element) {
+        final finalValue = switch (element.value) {
+          String() => element.value,
+          int() => element.value.toString(),
+          _ => throw Exception('Invalid type')
+        };
+
+        return acc.replaceAll('{${element.key}}', finalValue);
+      });
+}
---
+++ lib/application/placeholder/placeholder.dart
@@ -0,0 +1,80 @@
+abstract interface class PlaceholderContract {
+  Map<String, dynamic> get values;
+
+  String? get identifier;
+
+  String apply(String value);
+}
+
+class Placeholder implements PlaceholderContract {
+  final Map<String, dynamic> _values = {};
+
+  @override
+  final String? identifier;
+
+  @override
+  Map<String, dynamic> get values => _values;
+
+  Placeholder({this.identifier, Map<String, dynamic>? values}) {
+    if (values != null) {
+      _injectEntryMap(identifier, values);
+    }
+  }
+
+  void _injectEntryMap(String? identifier, Map<String, dynamic> values) {
+    final mapEntry = Map<String, dynamic>.from(values.map((key, value) {
+      final hasIdentifier = key.contains('.');
+      final finalKey = !hasIdentifier
+          ? identifier != null
+              ? '$identifier.$key'
+              : key
+          : key;
+
+      return MapEntry(finalKey, value);
+    }));
+
+    _values.addAll(mapEntry);
+  }
+
+  void merge(PlaceholderContract placeholder) {
+    _injectEntryMap(placeholder.identifier, placeholder.values);
+  }
+
+  @override
+  String apply(String value, {Map<String, dynamic>? values}) {
+    String finalValue = '';
+    if (values != null) {
+      final currentValues = Map<String, dynamic>.from(values.map((key, value) =>
+          identifier != null ? MapEntry('$identifier.$key', value) : MapEntry(key, value)));
+
+      finalValue = replace(value, currentValues);
+    }
+
+    return replace(finalValue, this.values);
+  }
+
+  String replace(String value, Map<String, dynamic> map) {
+    return map.entries.fold(value, (acc, element) {
+      final finalValue = switch (element.value) {
+        String() => element.value,
+        int() => element.value.toString(),
+        _ => throw Exception('Invalid type')
+      };
+
+      return acc.replaceAll('{${element.key}}', finalValue);
+    });
+  }
+
+// @override
+// String apply(String value, { Map<String, dynamic>? values }) => (values ?? this.values).entries.fold(value, (acc, element) {
+//   print(element.key);
+//
+//   final finalValue = switch (element.value) {
+//     String() => element.value,
+//     int() => element.value.toString(),
+//     _ => throw Exception('Invalid type')
+//   };
+//
+//   return acc.replaceAll('{${element.key}}', finalValue);
+// });
+}
