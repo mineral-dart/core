87a7654b3ab53812d43556c27817b55c69468fc5
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-01-20 20:09:03 +0100

    feat(messages): added fromJson method

--- lib/api/private/private_message.dart
+++ lib/api/private/private_message.dart
@@ -1,8 +1,9 @@
-import 'package:mineral/api/common/channel.dart';
 import 'package:mineral/api/common/message.dart';
+import 'package:mineral/api/private/channels/private_channel.dart';
 import 'package:mineral/api/private/user.dart';
+import 'package:mineral/domains/shared/utils.dart';
 
-final class PrivateMessage extends Message<Channel> {
+final class PrivateMessage extends Message<PrivateChannel> {
   final String userId;
 
   final User user;
@@ -11,9 +12,21 @@ final class PrivateMessage extends Message<Channel> {
     required String id,
     required String content,
     required DateTime createdAt,
-    required DateTime updatedAt,
-    required Channel channel,
+    required DateTime? updatedAt,
+    required PrivateChannel channel,
     required this.userId,
     required this.user,
   }): super(id, content, channel, createdAt, updatedAt);
+
+  factory PrivateMessage.fromJson({required Map<String, dynamic> json, required User user}) {
+    return PrivateMessage(
+        id: json['id'],
+        content: json['content'],
+        createdAt: DateTime.parse(json['timestamp']),
+        updatedAt: createOrNull(field: json['edited_timestamp'], fn: () => DateTime.parse(json['edited_timestamp'])),
+        channel: PrivateChannel.fromJson(json),
+        userId: json['author']['id'],
+        user: user
+    );
+  }
 }
