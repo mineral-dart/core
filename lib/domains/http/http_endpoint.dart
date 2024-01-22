9f5f896bc41455380ca21d1356bd05e80f0a1f03
LeadcodeDev <baptiste.parmantier.pro@gmail.com>
2024-01-22 21:01:25 +0100

    feat(http): implemented request executor

---
+++ lib/domains/http/http_endpoint.dart
@@ -0,0 +1,15 @@
+class HttpEndpoint {
+  final String method;
+  final String url;
+
+  HttpEndpoint(this.method, this.url);
+
+  factory HttpEndpoint.getServerChannelList({required String guildId}) =>
+      HttpEndpoint('GET', '/guilds/$guildId/channels');
+
+  factory HttpEndpoint.getServerChannel({required String guildId, required String channelId}) =>
+      HttpEndpoint('GET', '/guilds/$guildId/channels/$channelId');
+
+  @override
+  String toString() => '[$method] $url';
+}
