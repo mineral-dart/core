df08b3ec95e69f7dace1fef7e43c57b70d5a6f33
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-02-10 19:13:13 +0100

    feat(channel): added recipients on private channel

--- lib/api/private/channels/private_channel.dart
+++ lib/api/private/channels/private_channel.dart
@@ -1,15 +1,25 @@
 import 'package:mineral/api/common/channel.dart';
+import 'package:mineral/api/private/user.dart';
 
 final class PrivateChannel extends Channel {
+  final List<User> recipients;
   PrivateChannel({
     required String id,
     required String name,
+    required this.recipients,
   }): super(id, name);
 
   factory PrivateChannel.fromJson(Map<String, dynamic> json) {
+    final List<User> recipients = [];
+
+    for (final recipient in json['recipients']) {
+      recipients.add(User.fromJson(recipient));
+    }
+
     return PrivateChannel(
-      id: json['channel_id'],
-      name: json['author']['username'],
+      id: json['id'],
+      name: recipients.map((e) => e.username).join(', '),
+      recipients: recipients,
     );
   }
 }
