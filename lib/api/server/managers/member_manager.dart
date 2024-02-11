0e35c5274c3ff84e792c80dd52283228bfbd9a80
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-02-11 14:06:20 +0100

    feat(members): added utils method

--- lib/api/server/managers/member_manager.dart
+++ lib/api/server/managers/member_manager.dart
@@ -22,5 +22,13 @@ final class MemberManager {
   Member getOrFail(String id, {String? error}) => _members.values
       .firstWhere((element) => element.id == id, orElse: () => throw error ?? 'Member not found');
 
+  void add(Member member) {
+    _members[member.id] = member;
+  }
+
+  void remove(String id) {
+    _members.remove(id);
+  }
+
   late final int maxInGuild;
 }
