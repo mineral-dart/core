70d99a4770ac41c86570a20162b58a12a1ec0f1d
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-02-25 14:09:09 +0100

    feat(datastore): implement getChannel method

--- lib/domains/data_store/parts/channel_part.dart
+++ lib/domains/data_store/parts/channel_part.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 
+import 'package:mineral/api/common/channel.dart';
 import 'package:mineral/api/common/snowflake.dart';
 import 'package:mineral/domains/data_store/data_store.dart';
 import 'package:mineral/domains/data_store/data_store_part.dart';
@@ -9,6 +10,10 @@ final class ChannelPart implements DataStorePart {
 
   ChannelPart(this._dataStore);
 
-  FutureOr<void> getChannel(Snowflake id) async {
+  Future<Channel?> getChannel<T extends Channel>(Snowflake id) async {
+    final response = await _dataStore.client.get('/channels/$id');
+    final channel = _dataStore.marshaller.serializers.channels.serialize(response.body);
+
+    return channel;
   }
 }
