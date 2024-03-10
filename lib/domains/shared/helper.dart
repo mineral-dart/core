07226e100bc2c1e6bc3ec06345ba74a12afd3e9d
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-03-10 22:19:36 +0100

    feat(common): move utils standalone functions to helper class

---
+++ lib/domains/shared/helper.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+final class Helper {
+  static T? createOrNull<T>({required dynamic field, required T? Function() fn}) {
+    return field != null ? fn() : null;
+  }
+
+  static Future<T?> createOrNullAsync<T>(
+          {required dynamic field, required Future<T?> Function() fn}) async =>
+      field != null ? await fn() : null;
+}
