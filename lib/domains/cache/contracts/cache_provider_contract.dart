cf66e388363ffea0324e1b79736dfad839a42ed9
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-03-10 22:19:53 +0100

    feat(cache): create cache provider contract

---
+++ lib/domains/cache/contracts/cache_provider_contract.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+abstract interface class CacheProviderContract<K> {
+  String get name;
+
+  Future<int> length();
+  Future<Map<K, T>> getAll<T>();
+  Future<T?> get<T>(K? key);
+  Future<T> getOrFail<T>(K key, { Exception Function()? onFail });
+  Future<bool> has(K key);
+  Future<void> put<T>(K key, T object);
+  Future<void> remove(K key);
+  Future<void> removeMany(List<K> key);
+  Future<void> clear();
+
+  Future<void> dispose();
+  Future<bool> getHealth();
+}
