7aae7293270039d21c372f0bfa3572bec07903d7
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-03-22 10:15:45 +0100

    feat(datastore): enhance channel methods

--- lib/domains/data_store/parts/channel_part.dart
+++ lib/domains/data_store/parts/channel_part.dart
@@ -3,6 +3,7 @@ import 'dart:io';
 
 import 'package:mineral/api/common/channel.dart';
 import 'package:mineral/api/common/snowflake.dart';
+import 'package:mineral/application/http/http_client_status.dart';
 import 'package:mineral/application/http/http_request_option.dart';
 import 'package:mineral/domains/data_store/data_store.dart';
 import 'package:mineral/domains/data_store/data_store_part.dart';
@@ -11,24 +12,44 @@ import 'package:mineral/domains/http/discord_header.dart';
 final class ChannelPart implements DataStorePart {
   final DataStore _dataStore;
 
+  HttpClientStatus get status => _dataStore.client.status;
+
   ChannelPart(this._dataStore);
 
   Future<Channel?> getChannel<T extends Channel>(Snowflake id) async {
     final response = await _dataStore.client.get('/channels/$id');
 
     final channel = await switch (response.statusCode) {
-      int() when _dataStore.client.status.isSuccess(response.statusCode) =>
-          _dataStore.marshaller.serializers.channels.serialize(response.body),
-      int() when _dataStore.client.status.isError(response.statusCode) =>
-      throw HttpException(response.body),
+      int() when status.isSuccess(response.statusCode) =>
+        _dataStore.marshaller.serializers.channels.serialize(response.body),
+      int() when status.isError(response.statusCode) => throw HttpException(response.body),
       _ => throw Exception('Unknown status code: ${response.statusCode}'),
     };
 
     _dataStore.marshaller.cache.put(channel?.id, response.body);
-    return channel;
+    return channel as T?;
   }
 
-  Future<Channel?> updateChannel<T extends Channel>(
+  Future<T> createChannel<T extends Channel>(
+      {required Snowflake serverId,
+      required Map<String, dynamic> payload,
+      required String? reason}) async {
+    final response = await _dataStore.client.post('/guilds/$serverId/channels',
+        body: payload,
+        option: HttpRequestOptionImpl(headers: {DiscordHeader.auditLogReason(reason)}));
+
+    final channel = await switch (response.statusCode) {
+      int() when status.isSuccess(response.statusCode) =>
+        _dataStore.marshaller.serializers.channels.serialize(response.body),
+      int() when status.isError(response.statusCode) => throw HttpException(response.body),
+      _ => throw Exception('Unknown status code: ${response.statusCode}'),
+    };
+
+    await _dataStore.marshaller.cache.put(channel?.id, response.body);
+    return channel as T;
+  }
+
+  Future<T?> updateChannel<T extends Channel>(
       {required Snowflake id,
       required Map<String, dynamic> payload,
       required String? reason}) async {
@@ -37,15 +58,14 @@ final class ChannelPart implements DataStorePart {
         option: HttpRequestOptionImpl(headers: {DiscordHeader.auditLogReason(reason)}));
 
     final channel = await switch (response.statusCode) {
-      int() when _dataStore.client.status.isSuccess(response.statusCode) =>
+      int() when status.isSuccess(response.statusCode) =>
         _dataStore.marshaller.serializers.channels.serialize(response.body),
-      int() when _dataStore.client.status.isError(response.statusCode) =>
-        throw HttpException(response.body),
+      int() when status.isError(response.statusCode) => throw HttpException(response.body),
       _ => throw Exception('Unknown status code: ${response.statusCode}'),
     };
 
-    _dataStore.marshaller.cache.put(channel?.id, response.body);
-    return channel;
+    await _dataStore.marshaller.cache.put(channel?.id, response.body);
+    return channel as T?;
   }
 
   Future<void> deleteChannel(Snowflake id, String? reason) async {
@@ -53,10 +73,8 @@ final class ChannelPart implements DataStorePart {
         option: HttpRequestOptionImpl(headers: {DiscordHeader.auditLogReason(reason)}));
 
     return switch (response.statusCode) {
-      int() when _dataStore.client.status.isSuccess(response.statusCode) =>
-        _dataStore.marshaller.cache.remove(id),
-      int() when _dataStore.client.status.isError(response.statusCode) =>
-        throw HttpException(response.body),
+      int() when status.isSuccess(response.statusCode) => _dataStore.marshaller.cache.remove(id),
+      int() when status.isError(response.statusCode) => throw HttpException(response.body),
       _ => throw Exception('Unknown status code: ${response.statusCode}'),
     };
   }
