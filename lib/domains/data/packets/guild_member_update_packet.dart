287c94dc9ddb4717ec90c91983075ff0b2ebec5b
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-03-24 11:13:32 +0100

    feat(event): apply datastore in member update event

--- lib/domains/data/packets/guild_member_update_packet.dart
+++ lib/domains/data/packets/guild_member_update_packet.dart
@@ -1,3 +1,4 @@
+import 'package:mineral/api/common/snowflake.dart';
 import 'package:mineral/application/logger/logger.dart';
 import 'package:mineral/domains/data/types/listenable_packet.dart';
 import 'package:mineral/domains/data/types/packet_type.dart';
@@ -16,19 +17,20 @@ final class GuildMemberUpdatePacket implements ListenablePacket {
 
   @override
   Future<void> listen(ShardMessage message, DispatchEvent dispatch) async {
-    final String serverId = message.payload['guild_id'];
-    final rawServer = await marshaller.cache.get(serverId);
-    final server = await marshaller.serializers.server.serialize(rawServer);
+    final Snowflake serverId = Snowflake(message.payload['guild_id']);
+    final server = await marshaller.dataStore.server.getServer(serverId);
 
-    final member = await marshaller.serializers.member.serialize({
-      ...message.payload,
-      'guild_roles': server.roles.list
-    });
+    final before = await marshaller.dataStore.member
+        .getMember(guildId: serverId, memberId: Snowflake(message.payload['user']['id']));
 
-    server.members.add(member);
-    marshaller.cache.put(server.id, await marshaller.serializers.server.deserialize(server));
-    marshaller.cache.put(member.id, member);
+    final after = await marshaller.serializers.member
+        .serialize({...message.payload, 'guild_roles': server.roles.list});
 
-    dispatch(event: MineralEvent.serverMemberAdd, params: [member, server]);
+    server.members.add(after);
+
+    await marshaller.cache.put(server.id, await marshaller.serializers.server.deserialize(server));
+    await marshaller.cache.put(after.id, await marshaller.serializers.member.deserialize(after));
+
+    dispatch(event: MineralEvent.serverMemberUpdate, params: [before, after]);
   }
 }
