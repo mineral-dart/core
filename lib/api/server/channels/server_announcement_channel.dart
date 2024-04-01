d970235782c5b5319430cb2c55bd46397a55d7bb
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-04-01 21:08:13 +0200

    feat(api): implement server announcement channel methods

--- lib/api/server/channels/server_announcement_channel.dart
+++ lib/api/server/channels/server_announcement_channel.dart
@@ -1,3 +1,4 @@
+import 'package:mineral/api/common/channel_methods.dart';
 import 'package:mineral/api/common/channel_permission_overwrite.dart';
 import 'package:mineral/api/common/channel_properties.dart';
 import 'package:mineral/api/common/snowflake.dart';
@@ -7,6 +8,7 @@ import 'package:mineral/api/server/channels/server_channel.dart';
 
 final class ServerAnnouncementChannel extends ServerChannel {
   final ChannelProperties _properties;
+  final ChannelMethods _methods;
 
   @override
   Snowflake get id => _properties.id;
@@ -29,5 +31,21 @@ final class ServerAnnouncementChannel extends ServerChannel {
 
   ServerCategoryChannel? category;
 
-  ServerAnnouncementChannel(this._properties, {required this.category});
+  ServerAnnouncementChannel(this._properties, {required this.category})
+      : _methods = ChannelMethods(_properties.id);
+
+  Future<void> setName(String name, {String? reason}) => _methods.setName(name, reason);
+
+  Future<void> setDescription(String description, {String? reason}) =>
+      _methods.setDescription(description, reason);
+
+  Future<void> setCategory(String categoryId, {String? reason}) =>
+      _methods.setCategory(categoryId, reason);
+
+  Future<void> setPosition(int position, {String? reason}) =>
+      _methods.setPosition(position, reason);
+
+  Future<void> setNsfw(bool nsfw, {String? reason}) => _methods.setNsfw(nsfw, reason);
+
+  Future<void> delete({String? reason}) => _methods.delete(reason);
 }
