ea41a818f7098713591f3e4c1483df265e5823c3
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-04-03 21:23:08 +0200

    fix(marshaller): add environment into ioc

--- lib/core/container.dart
+++ lib/core/container.dart
@@ -1,5 +1,10 @@
+import 'package:mineral/application/environment/environment.dart' as env_service;
 import 'package:mineral/application/logger/logger.dart' as logger_service;
 
 mixin Logger {
   logger_service.LoggerContract get logger => logger_service.Logger.singleton();
+}
+
+mixin Environment {
+  env_service.EnvContract get logger => env_service.Environment.singleton();
 }
