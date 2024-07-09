daa7d4854d7686a58c54341c6ea4a120143051f2
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-07-09 21:18:42 +0200

    feat(commands): finished contexts

---
+++ lib/infrastructure/interaction/command/guild_command_context.dart
@@ -0,0 +1,36 @@
+import 'package:mineral/api/common/channel.dart';
+import 'package:mineral/api/common/snowflake.dart';
+import 'package:mineral/api/private/user.dart';
+import 'package:mineral/api/server/server.dart';
+import 'package:mineral/infrastructure/interaction/command/command_context.dart';
+
+final class ServerCommandContext implements CommandContext {
+  @override
+  final Snowflake id;
+  @override
+  final Snowflake applicationId;
+  @override
+  final String token;
+  @override
+  final int version;
+
+  final User user;
+  final Channel? channel;
+  final Server server;
+
+
+  ServerCommandContext({
+    required this.id,
+    required this.applicationId,
+    required this.token,
+    required this.version,
+    required this.user,
+    required this.server,
+    this.channel,
+  });
+
+  @override
+  Future<void> reply() {
+    throw UnimplementedError();
+  }
+}
