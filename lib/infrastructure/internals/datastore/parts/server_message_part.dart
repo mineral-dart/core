0a03ce6dcb8a27ad89c38488ae34d60f8660c648
DakioCode <snn.67@hotmail.com>
2024-07-15 20:18:47 +0200

    feat(server-messages) ServerMessage.pin()

--- lib/infrastructure/internals/datastore/parts/server_message_part.dart
+++ lib/infrastructure/internals/datastore/parts/server_message_part.dart
@@ -30,6 +30,10 @@ final class ServerMessagePart implements DataStorePart {
     });
   }
 
+  Future<void> pin({ required Snowflake id, required Snowflake channelId }) async {
+    await _kernel.dataStore.client.put('/channels/$channelId/pins/$id');
+  }
+
   Future<void> delete({ required Snowflake id, required Snowflake channelId }) async {
     await _kernel.dataStore.client.delete('/channels/$channelId/messages/$id');
   }
