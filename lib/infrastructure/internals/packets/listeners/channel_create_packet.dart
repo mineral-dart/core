285e129a4ce4f96b462d8d5e99fdd525b6bf35c8
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-07-21 20:28:18 +0200

    feat(packets): apply newer serialization in channel create packet

--- lib/infrastructure/internals/packets/listeners/channel_create_packet.dart
+++ lib/infrastructure/internals/packets/listeners/channel_create_packet.dart
@@ -29,19 +29,26 @@ final class ChannelCreatePacket implements ListenablePacket {
 
   Future<void> registerServerChannel(ServerChannel channel, DispatchEvent dispatch) async {
     final server = await marshaller.dataStore.server.getServer(channel.guildId);
+    final serverCacheKey = marshaller.cacheKey.server(server.id);
+    final channelCacheKey = marshaller.cacheKey.serverChannel(serverId: server.id, channelId: channel.id);
 
     channel.server = server;
     server.channels.list.putIfAbsent(channel.id, () => channel);
 
     final rawServer = await marshaller.serializers.server.deserialize(server);
-    await marshaller.cache.put(server.id.value, rawServer);
+    await marshaller.cache.put(serverCacheKey, rawServer);
+
+    final rawChannel = await marshaller.serializers.channels.deserialize(channel);
+    await marshaller.cache.put(channelCacheKey, rawChannel);
 
     dispatch(event: Event.serverChannelCreate, params: [channel]);
   }
 
   Future<void> registerPrivateChannel(PrivateChannel channel, DispatchEvent dispatch) async {
+    final cacheKey = marshaller.cacheKey.privateChannel(channel.id);
+
     final rawChannel = await marshaller.serializers.channels.deserialize(channel);
-    await marshaller.cache.put(channel.id.value, rawChannel);
+    await marshaller.cache.put(cacheKey, rawChannel);
 
     dispatch(event: Event.privateChannelCreate, params: [channel]);
   }
