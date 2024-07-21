f0b5b3926543ce9be5acff4ca6fd223e2e92e237
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-07-21 19:50:25 +0200

    feat(marshaller): implement server subscription serialization from cache

--- lib/infrastructure/internals/marshaller/serializers/server_subscription_serializer.dart
+++ lib/infrastructure/internals/marshaller/serializers/server_subscription_serializer.dart
@@ -10,17 +10,17 @@ final class ServerSubscriptionSerializer implements SerializerContract<ServerSub
   ServerSubscriptionSerializer(this._marshaller);
 
   @override
-  ServerSubscription serializeRemote(Map<String, dynamic> json) {
+  ServerSubscription serializeRemote(Map<String, dynamic> json) => _serialize(json);
+
+  @override
+  ServerSubscription serializeCache(Map<String, dynamic> json) => _serialize(json);
+
+  ServerSubscription _serialize(Map<String, dynamic> json) {
     return ServerSubscription(
       tier: findInEnum(PremiumTier.values, json['premium_tier']),
       subscriptionCount: json['premium_subscription_count'],
       hasEnabledProgressBar: json['premium_progress_bar_enabled'],
     );
-  }
-
-  @override
-  Future<ServerSubscription> serializeCache(Map<String, dynamic> json) {
-    throw UnimplementedError();
   }
 
   @override
