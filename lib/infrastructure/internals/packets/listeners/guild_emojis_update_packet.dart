12cd0d1b51e9dd7b33d682b43b16402c368c82f2
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-07-21 20:45:36 +0200

    feat(packets): apply newer serialization in guild emojis update packet

--- lib/infrastructure/internals/packets/listeners/guild_emojis_update_packet.dart
+++ lib/infrastructure/internals/packets/listeners/guild_emojis_update_packet.dart
@@ -1,10 +1,10 @@
 import 'package:mineral/api/server/managers/emoji_manager.dart';
-import 'package:mineral/infrastructure/services/logger/logger.dart';
+import 'package:mineral/domains/events/event.dart';
+import 'package:mineral/infrastructure/internals/marshaller/marshaller.dart';
 import 'package:mineral/infrastructure/internals/packets/listenable_packet.dart';
 import 'package:mineral/infrastructure/internals/packets/packet_type.dart';
-import 'package:mineral/infrastructure/internals/marshaller/marshaller.dart';
-import 'package:mineral/domains/events/event.dart';
 import 'package:mineral/infrastructure/internals/wss/shard_message.dart';
+import 'package:mineral/infrastructure/services/logger/logger.dart';
 
 final class GuildEmojisUpdatePacket implements ListenablePacket {
   @override
@@ -18,9 +18,20 @@ final class GuildEmojisUpdatePacket implements ListenablePacket {
   @override
   Future<void> listen(ShardMessage message, DispatchEvent dispatch) async {
     final server = await marshaller.dataStore.server.getServer(message.payload['guild_id']);
-    final EmojiManager emojiManager = EmojiManager.fromJson(marshaller, payload: message.payload['emojis'], roles: server.roles.list.values.toList());
+    final serverCacheKey = marshaller.cacheKey.server(server.id);
+
+    final EmojiManager emojiManager = EmojiManager.fromJson(marshaller,
+        payload: message.payload['emojis'], roles: server.roles.list.values.toList());
+
+    await emojiManager.list.values.map((emoji) async {
+      final emojiCacheKey = marshaller.cacheKey.serverEmoji(serverId: server.id, emojiId: emoji.id!);
+      return marshaller.cache.put(emojiCacheKey, emoji);
+    }).wait;
 
     server.assets.emojis = emojiManager;
+
+    final rawServer = await marshaller.serializers.server.deserialize(server);
+    await marshaller.cache.put(serverCacheKey, rawServer);
 
     dispatch(event: Event.serverEmojisUpdate, params: [emojiManager, server]);
   }
