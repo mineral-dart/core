e969310ea52876bf2a0228872b3771ff90f557a0
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-07-21 13:48:58 +0200

    feat(marshaller): enhance role cache serialization

--- lib/infrastructure/internals/marshaller/serializers/role_serializer.dart
+++ lib/infrastructure/internals/marshaller/serializers/role_serializer.dart
@@ -2,9 +2,9 @@ import 'package:mineral/api/common/color.dart';
 import 'package:mineral/api/common/permissions.dart';
 import 'package:mineral/api/common/snowflake.dart';
 import 'package:mineral/api/server/role.dart';
+import 'package:mineral/infrastructure/commons/utils.dart';
 import 'package:mineral/infrastructure/internals/marshaller/marshaller.dart';
 import 'package:mineral/infrastructure/internals/marshaller/types/serializer.dart';
-import 'package:mineral/infrastructure/commons/utils.dart';
 
 final class RoleSerializer implements SerializerContract<Role> {
   final MarshallerContract marshaller;
@@ -12,44 +12,28 @@ final class RoleSerializer implements SerializerContract<Role> {
   RoleSerializer(this.marshaller);
 
   @override
-  Future<Role> serializeRemote(Map<String, dynamic> json) async {
-    return Role(
-      id: Snowflake(json['id']),
-      name: json['name'],
-      color: Color.of(json['color'] ?? 0),
-      hoist: json['hoist'] ?? false,
-      position: json['position'] ?? 0,
-      permissions: switch(json['permissions']) {
-        int() => Permissions.fromInt(json['permissions']),
-        String() => Permissions.fromInt(int.parse(json['permissions'])),
-        _ => Permissions.fromInt(0),
-      },
-      managed: json['managed'],
-      mentionable: json['mentionable'],
-      flags: json['flags'],
-      icon: json['icon'],
-      unicodeEmoji: json['unicode_emoji'],
-    );
-  }
+  Future<Role> serializeRemote(Map<String, dynamic> json) async => _serialize(json);
 
   @override
-  Future<Role> serializeCache(Map<String, dynamic> json) async {
+  Future<Role> serializeCache(Map<String, dynamic> json) async => _serialize(json);
+
+  Role _serialize(Map<String, dynamic> payload) {
     return Role(
-      id: Snowflake(json['id']),
-      name: json['name'],
-      color: Color.of(json['color'] ?? 0),
-      hoist: json['hoist'] ?? false,
-      position: json['position'] ?? 0,
-      permissions: switch(json['permissions']) {
-        int() => Permissions.fromInt(json['permissions']),
-        String() => Permissions.fromInt(int.parse(json['permissions'])),
+      id: Snowflake(payload['id']),
+      name: payload['name'],
+      color: Color.of(payload['color'] ?? 0),
+      hoist: payload['hoist'] ?? false,
+      position: payload['position'] ?? 0,
+      permissions: switch(payload['permissions']) {
+        int() => Permissions.fromInt(payload['permissions']),
+        String() => Permissions.fromInt(int.parse(payload['permissions'])),
         _ => Permissions.fromInt(0),
       },
-      managed: json['managed'],
-      mentionable: json['mentionable'],
-      flags: json['flags'],
-      icon: json['icon'],
-      unicodeEmoji: json['unicode_emoji'],
+      managed: payload['managed'],
+      mentionable: payload['mentionable'],
+      flags: payload['flags'],
+      icon: payload['icon'],
+      unicodeEmoji: payload['unicode_emoji'],
     );
   }
 
