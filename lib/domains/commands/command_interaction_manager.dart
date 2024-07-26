b46cb6f1513018fcfa861f8e4bf7f210e3f0bef8
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-07-26 23:22:32 +0200

    feat(commands): enhance some changes

--- lib/domains/commands/command_interaction_manager.dart
+++ lib/domains/commands/command_interaction_manager.dart
@@ -1,10 +1,11 @@
 import 'dart:async';
-import 'dart:convert';
 
 import 'package:mineral/api/common/bot.dart';
-import 'package:mineral/api/common/commands/builder/command_builder.dart';
+import 'package:mineral/api/common/commands/builder/command_declaration_builder.dart';
+import 'package:mineral/api/common/commands/builder/command_definition_builder.dart';
 import 'package:mineral/api/common/commands/command_context_type.dart';
 import 'package:mineral/api/server/server.dart';
+import 'package:mineral/domains/commands/command_builder.dart';
 import 'package:mineral/domains/commands/command_interaction_dispatcher.dart';
 import 'package:mineral/domains/types/interaction_dispatcher_contract.dart';
 import 'package:mineral/infrastructure/internals/marshaller/marshaller.dart';
@@ -15,11 +16,14 @@ abstract class CommandInteractionManagerContract {
   late InteractionDispatcherContract dispatcher;
 
   Future<void> registerGlobal(Bot bot);
+
   Future<void> registerServer(Bot bot, Server server);
+
   void addCommand(CommandBuilder command);
 }
 
-final class CommandInteractionManager implements CommandInteractionManagerContract {
+final class CommandInteractionManager
+    implements CommandInteractionManagerContract {
   @override
   final List<(String, Function handler)> commandsHandler = [];
 
@@ -41,25 +45,58 @@ final class CommandInteractionManager implements CommandInteractionManagerContra
       throw Exception('Command $command already exists');
     }
 
+    final handlers = switch (command) {
+      final CommandDeclarationBuilder command => command.reduceHandlers(),
+      final CommandDefinitionBuilder definition =>
+        definition.command.reduceHandlers(),
+      final _ => throw Exception('Unknown command type')
+    };
+
     commands.add(command);
-    commandsHandler.addAll(command.reduceHandlers());
+    commandsHandler.addAll(handlers);
   }
 
   @override
   Future<void> registerGlobal(Bot bot) async {
-    final List<CommandBuilder> globalCommands = commands.where((command) => command.context == CommandContextType.global).toList();
+    final List<CommandBuilder> globalCommands =
+        _getContext(CommandContextType.global);
+    final payload = _serializeCommand(globalCommands);
 
-    await _marshaller.dataStore.client.put('/applications/${bot.id}/commands', body: [
-      ...globalCommands.map((e) => e.toJson())
-    ]);
+    await _marshaller.dataStore.client
+        .put('/applications/${bot.id}/commands', body: payload);
   }
 
   @override
   Future<void> registerServer(Bot bot, Server server) async {
-    final List<CommandBuilder> guildCommands = commands.where((command) => command.context == CommandContextType.guild).toList();
+    final List<CommandBuilder> guildCommands =
+        _getContext(CommandContextType.guild);
+    final payload = _serializeCommand(guildCommands);
 
-    await _marshaller.dataStore.client.put('/applications/${bot.id}/guilds/${server.id}/commands', body: [
-      ...guildCommands.map((e) => e.toJson())
-    ]);
+    await _marshaller.dataStore.client.put(
+        '/applications/${bot.id}/guilds/${server.id}/commands',
+        body: payload);
+  }
+
+  List<CommandBuilder> _getContext(CommandContextType contextType) {
+    return commands.where((command) {
+      final context = switch (command) {
+        final CommandDeclarationBuilder command => command.context,
+        final CommandDefinitionBuilder definition => definition.command.context,
+        final _ => throw Exception('Unknown command type')
+      };
+
+      return context == contextType;
+    }).toList();
+  }
+
+  List<Map<String, dynamic>> _serializeCommand(List<CommandBuilder> commands) {
+    return commands.map((command) {
+      return switch (command) {
+        final CommandDeclarationBuilder command => command.toJson(),
+        final CommandDefinitionBuilder definition =>
+          definition.command.toJson(),
+        final _ => throw Exception('Unknown command type')
+      };
+    }).toList();
   }
 }
