7f737e6a475529466a6ffbabfd47f7e970131ec7
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-08-11 23:29:44 +0200

    feat(interaction): implement server role select handler

---
+++ lib/domains/events/contracts/server/server_role_select_event.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:mineral/api/server/channels/server_channel.dart';
+import 'package:mineral/api/server/role.dart';
+import 'package:mineral/domains/components/selects/contexts/server_select_context.dart';
+import 'package:mineral/domains/events/event.dart';
+import 'package:mineral/domains/events/types/listenable_event.dart';
+
+typedef ServerRoleSelectEventHandler = FutureOr Function(ServerSelectContext, List<Role>);
+
+abstract class ServerRoleSelectEvent implements ListenableEvent {
+  @override
+  Event get event => Event.serverRoleSelect;
+
+  String? get customId;
+
+  FutureOr<void> handle(ServerSelectContext ctx, List<Role> roles);
+}
