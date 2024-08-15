0bac4d7c26b88da8a3423c2b82a9d942420ac713
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-08-15 18:31:08 +0200

    feat(message): remove futur context in message factory (doesn't helpfull)

--- lib/infrastructure/internals/marshaller/types/message_factory.dart
+++ lib/infrastructure/internals/marshaller/types/message_factory.dart
@@ -3,5 +3,5 @@ import 'package:mineral/infrastructure/internals/marshaller/marshaller.dart';
 
 abstract interface class MessageFactory<T extends Message> {
   Future<T> serialize(MarshallerContract marshaller, Map<String, dynamic> json);
-  Future<Map<String, dynamic>> deserialize(MarshallerContract marshaller, T message);
+  Map<String, dynamic> deserialize(MarshallerContract marshaller, T message);
 }
