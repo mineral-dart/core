0e860dc455f41051aa536ca6665f1b647aeb5fd2
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-08-16 23:20:17 +0200

    feat(serialization): finish message serialization

--- lib/infrastructure/internals/marshaller/serializers/private_message_serializer.dart
+++ lib/infrastructure/internals/marshaller/serializers/private_message_serializer.dart
@@ -11,18 +11,34 @@ final class PrivateMessageSerializer implements SerializerContract<PrivateMessag
   PrivateMessageSerializer(this.marshaller);
 
   @override
-  Future<PrivateMessage> serializeRemote(Map<String, dynamic> json) async {
-    final channel = await marshaller.dataStore.channel.getChannel(Snowflake(json['channel_id']));
+  Future<void> normalize(Map<String, dynamic> json) async {
+    final payload = {
+      'id': json['id'],
+      'content': json['content'],
+      'embeds': json['embeds'],
+      'channel_id': json['channel_id'],
+      'created_at': json['created_at'],
+      'updated_at': json['updated_at'],
+    };
 
-    final messageProperties = MessageProperties.fromJson(channel as PrivateChannel, json);
-    final user = await marshaller.serializers.user.serializeRemote(json['author']);
-
-    return PrivateMessage(messageProperties, userId: json['author']['id'], author: user);
+    final cacheKey = marshaller.cacheKey.privateMessage(messageId: json['id'], channelId: Snowflake(json['channel_id']));
+    await marshaller.cache.put(cacheKey, payload);
   }
 
   @override
-  Future<PrivateMessage> serializeCache(Map<String, dynamic> json) {
-    throw UnimplementedError();
+  Future<PrivateMessage> serialize(Map<String, dynamic> json) async {
+    final properties = MessageProperties<PrivateChannel>(
+      id: Snowflake(json['id']),
+      content: json['content'],
+      embeds: json['embeds'],
+      createdAt: DateTime.parse(json['created_at']),
+      updatedAt: json['updated_at'] != null ? DateTime.parse(json['updated_at']) : null,
+      channelId: Snowflake(json['channel_id']),
+    );
+
+    final user = await marshaller.serializers.user.serialize(json['user']);
+
+    return PrivateMessage(properties, userId: user.id, author: user);
   }
 
   @override
