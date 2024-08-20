9acf6484153a3b0c373cd1904b2afc443df68584
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-08-20 23:33:15 +0200

    feat(threads): introduce thread list sync packet

---
+++ lib/infrastructure/internals/packets/listeners/thread_list_sync_packet.dart
@@ -0,0 +1,46 @@
+import 'package:mineral/domains/events/event.dart';
+import 'package:mineral/infrastructure/internals/marshaller/marshaller.dart';
+import 'package:mineral/infrastructure/internals/packets/listenable_packet.dart';
+import 'package:mineral/infrastructure/internals/packets/packet_type.dart';
+import 'package:mineral/infrastructure/internals/wss/shard_message.dart';
+import 'package:mineral/infrastructure/services/logger/logger.dart';
+
+final class ThreadListSyncPacket implements ListenablePacket {
+  @override
+  PacketType get packetType => PacketType.threadListSync;
+
+  final LoggerContract logger;
+  final MarshallerContract marshaller;
+
+  ThreadListSyncPacket(this.logger, this.marshaller);
+
+  @override
+  Future<void> listen(ShardMessage message, DispatchEvent dispatch) async {
+    final payload = message.payload;
+
+    final server = await marshaller.dataStore.server.getServer(payload['guild_id']);
+    final threadChannels = payload['threads'] as List<Map<String, dynamic>>;
+
+    final threads = await threadChannels.map((element) async {
+      final thread = await marshaller.serializers.thread.serializeRemote(element);
+
+      thread.server = server;
+
+      for (final member in thread.members) {
+        member.member.server = server;
+        member.member.roles.server = server;
+      }
+
+      //todo
+
+      final threadCacheKey = marshaller.cacheKey.threadChannel(serverId: json['id'], threadId: thread.id);
+      final threadRaw = await marshaller.serializers.thread.deserialize(thread);
+
+      await marshaller.cache.put(threadCacheKey, threadRaw);
+
+      return thread;
+    }).wait;
+
+    dispatch(event: Event.serverThreadDelete, params: [threads, server]);
+  }
+}
