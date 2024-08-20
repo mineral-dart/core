a66f43ee2f1df50684af8e9b6de45c104f1ff6e1
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-08-20 23:33:05 +0200

    feat(threads): introduce thread delete packet

---
+++ lib/domains/events/contracts/server/server_thread_update_event.dart
@@ -0,0 +1,15 @@
+import 'dart:async';
+
+import 'package:mineral/api/server/channels/thread_channel.dart';
+import 'package:mineral/api/server/server.dart';
+import 'package:mineral/domains/events/event.dart';
+import 'package:mineral/domains/events/types/listenable_event.dart';
+
+typedef ServerThreadUpdateEventHandler = FutureOr<void> Function(ThreadChannel, ThreadChannel, Server);
+
+abstract class ServerThreadUpdateEvent implements ListenableEvent {
+  @override
+  Event get event => Event.serverThreadUpdate;
+
+  FutureOr<void> handle(ThreadChannel before, ThreadChannel after, Server server);
+}
