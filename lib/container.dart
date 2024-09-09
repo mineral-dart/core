292fa51a50ebcfbe21d1642cebf679ece8430cd6
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2024-09-09 09:04:08 +0200

    style: apply dart format

--- lib/container.dart
+++ lib/container.dart
@@ -1,13 +1,17 @@
 library container;
 
 import 'package:mineral/src/infrastructure/internals/container/ioc_container.dart';
-import 'package:mineral/src/infrastructure/internals/environment/environment.dart' as env_service;
-import 'package:mineral/src/infrastructure/services/logger/logger.dart' as logger_service;
+import 'package:mineral/src/infrastructure/internals/environment/environment.dart'
+    as env_service;
+import 'package:mineral/src/infrastructure/services/logger/logger.dart'
+    as logger_service;
 
-export 'package:mineral/src/infrastructure/internals/container/ioc_container.dart' show ioc;
+export 'package:mineral/src/infrastructure/internals/container/ioc_container.dart'
+    show ioc;
 
 mixin InjectLogger {
-  logger_service.LoggerContract get logger => ioc.resolve<logger_service.LoggerContract>();
+  logger_service.LoggerContract get logger =>
+      ioc.resolve<logger_service.LoggerContract>();
 }
 
 mixin InjectEnvironment {
