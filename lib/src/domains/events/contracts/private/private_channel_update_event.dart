2c03bfcc8d7db9770558cf0daacb03ddb32acf46
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-10-20 13:13:54 +0200

    fix(private channels): added nullable value

--- lib/src/domains/events/contracts/private/private_channel_update_event.dart
+++ lib/src/domains/events/contracts/private/private_channel_update_event.dart
@@ -4,7 +4,7 @@ import 'package:mineral/src/api/private/channels/private_channel.dart';
 import 'package:mineral/src/domains/events/event.dart';
 import 'package:mineral/src/domains/events/types/listenable_event.dart';
 
-typedef PrivateChannelUpdateEventHandler = FutureOr<void> Function(PrivateChannel, PrivateChannel);
+typedef PrivateChannelUpdateEventHandler = FutureOr<void> Function(PrivateChannel?, PrivateChannel);
 
 abstract class PrivateChannelUpdateEvent implements ListenableEvent {
   @override
@@ -13,5 +13,5 @@ abstract class PrivateChannelUpdateEvent implements ListenableEvent {
   @override
   String? customId;
 
-  FutureOr<void> handle(PrivateChannel before, PrivateChannel after);
+  FutureOr<void> handle(PrivateChannel? before, PrivateChannel after);
 }
