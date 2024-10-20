d70d89c7b2670b666c0fdaba75e78bb8c66cddcc
PandaGuerrier <76222146+PandaGuerrier@users.noreply.github.com>
2024-10-20 13:20:49 +0200

    fix(interaction): missing `interaction` in command_context.dart (for global use too)

--- lib/src/domains/commands/command_context.dart
+++ lib/src/domains/commands/command_context.dart
@@ -1,10 +1,12 @@
 import 'package:mineral/src/api/common/snowflake.dart';
+import 'package:mineral/src/infrastructure/internals/interactions/types/interaction_contract.dart';
 
 abstract class CommandContext {
   final Snowflake id;
   final Snowflake applicationId;
   final String token;
   final int version;
+  late final InteractionContract interaction;
 
   CommandContext({
     required this.id,
