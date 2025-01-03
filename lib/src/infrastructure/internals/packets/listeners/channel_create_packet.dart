674d608e68dd9d14c076098608fafd02e235af59
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-01-03 12:21:54 +0100

    feat: simplify message listener and rename environment variables

--- lib/src/infrastructure/internals/packets/listeners/channel_create_packet.dart
+++ lib/src/infrastructure/internals/packets/listeners/channel_create_packet.dart
@@ -10,12 +10,8 @@ import 'package:mineral/src/infrastructure/internals/wss/shard_message.dart';
 final class ChannelCreatePacket implements ListenablePacket {
   @override
   PacketType get packetType => PacketType.channelCreate;
-
   LoggerContract get _logger => ioc.resolve<LoggerContract>();
-
   MarshallerContract get _marshaller => ioc.resolve<MarshallerContract>();
-
-  DataStoreContract get _dataStore => ioc.resolve<DataStoreContract>();
 
   @override
   Future<void> listen(ShardMessage message, DispatchEvent dispatch) async {
@@ -25,26 +21,9 @@ final class ChannelCreatePacket implements ListenablePacket {
         await _marshaller.serializers.channels.serialize(rawChannel);
 
     return switch (channel) {
-      ServerChannel() => registerServerChannel(channel, dispatch),
-      PrivateChannel() => registerPrivateChannel(channel, dispatch),
-      _ => _logger
-          .warn("Unknown channel type: $channel contact Mineral's core team.")
+      ServerChannel() => dispatch(event: Event.serverChannelCreate, params: [channel]),
+      PrivateChannel() => dispatch(event: Event.serverChannelCreate, params: [channel]),
+      _ => _logger.warn("Unknown channel type: $channel contact Mineral's core team.")
     };
-  }
-
-  Future<void> registerServerChannel(
-      ServerChannel channel, DispatchEvent dispatch) async {
-    final server = await _dataStore.server.get(channel.serverId.value, false);
-    final serverCacheKey = _marshaller.cacheKey.server(server.id.value);
-
-    final rawServer = await _marshaller.serializers.server.deserialize(server);
-    await _marshaller.cache.put(serverCacheKey, rawServer);
-
-    dispatch(event: Event.serverChannelCreate, params: [channel]);
-  }
-
-  Future<void> registerPrivateChannel(
-      PrivateChannel channel, DispatchEvent dispatch) async {
-    dispatch(event: Event.privateChannelCreate, params: [channel]);
   }
 }
