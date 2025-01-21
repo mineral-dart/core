8a83abf52f613c8f0f69a63ed3b4e4c2b8c16a4c
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-01-21 18:38:48 +0100

    feat: implement interactive menu components

---
+++ lib/src/domains/components/interactives/components/interactive_select_menu.dart
@@ -0,0 +1,8 @@
+import 'dart:async';
+
+import 'package:mineral/api.dart';
+import 'package:mineral/src/domains/components/interactives/components/interactive_component.dart';
+
+abstract interface class InteractiveSelectMenu<T extends dynamic> implements InteractiveComponent<SelectMenuBuilderContract> {
+  FutureOr<void> handle(SelectContext ctx, T values);
+}
