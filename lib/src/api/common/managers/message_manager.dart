6c2649a9d3f9c0c67d03ea939fd375d7f792123d
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-06-12 15:39:50 +0200

    feat: add MessageManager class with channel ID and data retrieval methods

--- lib/src/api/common/managers/message_manager.dart
+++ lib/src/api/common/managers/message_manager.dart
@@ -1,6 +1,20 @@
+import 'package:mineral/container.dart';
+import 'package:mineral/contracts.dart';
 import 'package:mineral/src/api/common/message.dart';
 import 'package:mineral/src/api/common/snowflake.dart';
 
 final class MessageManager<T extends BaseMessage> {
+  final Snowflake _channelId;
+
+  MessageManager(this._channelId);
+
+  final DataStoreContract _datastore = ioc.resolve<DataStoreContract>();
+
   final Map<Snowflake, T> list = {};
+
+  Future<T?> get(Snowflake id) =>
+      _datastore.message.get<T>(_channelId.value, id.value, false);
+
+  Future<T> getOrFail(Snowflake id) =>
+      _datastore.message.get<T>(_channelId.value, id.value, false) as Future<T>;
 }
