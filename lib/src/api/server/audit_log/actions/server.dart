f8f51a4e1fe04842f5835b62ece77913af8f989c
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-06-19 11:41:54 +0200

    feat: add unknown audit log action handler and enhance server update handling

--- lib/src/api/server/audit_log/actions/server.dart
+++ lib/src/api/server/audit_log/actions/server.dart
@@ -2,19 +2,13 @@ import 'package:mineral/api.dart';
 import 'package:mineral/src/api/server/audit_log/audit_log.dart';
 
 final class ServerUpdateAuditLogAction extends AuditLog {
+  final Server server;
   final List<Change> changes;
 
   ServerUpdateAuditLogAction(
       {required Snowflake serverId,
       required Snowflake userId,
+      required this.server,
       required this.changes})
       : super(AuditLogType.guildUpdate, serverId, userId);
-
-  factory ServerUpdateAuditLogAction.fromJson(Map<String, dynamic> json) {
-    return ServerUpdateAuditLogAction(
-        serverId: Snowflake.parse(json['guild_id']),
-        userId: Snowflake.parse(json['user_id']),
-        changes:
-            List<Change>.from(json['changes'].map((e) => Change.fromJson(e))));
-  }
 }
--- lib/src/infrastructure/internals/packets/listeners/guild_audit_log_entry_create_packet.dart
+++ lib/src/infrastructure/internals/packets/listeners/guild_audit_log_entry_create_packet.dart
@@ -1,16 +1,49 @@
+import 'package:mineral/container.dart';
+import 'package:mineral/contracts.dart';
 import 'package:mineral/events.dart';
 import 'package:mineral/src/api/server/audit_log/audit_log.dart';
+import 'package:mineral/src/api/server/audit_log/audit_log_action.dart';
 import 'package:mineral/src/infrastructure/internals/packets/listenable_packet.dart';
+import 'package:mineral/src/infrastructure/internals/packets/listeners/audit_logs/_default.dart';
+import 'package:mineral/src/infrastructure/internals/packets/listeners/audit_logs/channel.dart';
+import 'package:mineral/src/infrastructure/internals/packets/listeners/audit_logs/emoji.dart';
+import 'package:mineral/src/infrastructure/internals/packets/listeners/audit_logs/role.dart';
+import 'package:mineral/src/infrastructure/internals/packets/listeners/audit_logs/server.dart';
 import 'package:mineral/src/infrastructure/internals/packets/packet_type.dart';
 import 'package:mineral/src/infrastructure/internals/wss/shard_message.dart';
 
 final class GuildAuditLogEntryCreatePacket implements ListenablePacket {
+  LoggerContract get logger => ioc.resolve<LoggerContract>();
+
   @override
   PacketType get packetType => PacketType.guildAuditLogEntryCreate;
 
   @override
   Future<void> listen(ShardMessage message, DispatchEvent dispatch) async {
-    final auditLog = AuditLog.fromJson(message.payload);
+    final auditLogType = AuditLogType.values.firstWhere(
+        (element) => element.value == message.payload['action_type'],
+        orElse: () => AuditLogType.unknown);
+
+    final auditLog = await switch (auditLogType) {
+      AuditLogType.emojiCreate => emojiCreateAuditLogHandler(message.payload),
+      AuditLogType.emojiUpdate => emojiUpdateAuditLogHandler(message.payload),
+      AuditLogType.emojiDelete => emojiDeleteAuditLogHandler(message.payload),
+      AuditLogType.roleCreate => roleCreateAuditLogHandler(message.payload),
+      AuditLogType.roleUpdate => roleUpdateAuditLogHandler(message.payload),
+      AuditLogType.roleDelete => roleDeleteAuditLogHandler(message.payload),
+      AuditLogType.guildUpdate => serverUpdateAuditLogHandler(message.payload),
+      AuditLogType.channelCreate =>
+        channelCreateAuditLogHandler(message.payload),
+      AuditLogType.channelUpdate =>
+        channelUpdateAuditLogHandler(message.payload),
+      AuditLogType.channelDelete =>
+        channelDeleteAuditLogHandler(message.payload),
+      _ => unknownAuditLogHandler(message.payload),
+    };
+
+    if (auditLog case final UnknownAuditLogAction action) {
+      logger.warn('Audit log action not found ${action.type}');
+    }
 
     dispatch(
       event: Event.serverAuditLog,
