9531b1649604464ee6faccfc5160ff678b5d1df3
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-06-19 11:41:30 +0200

    feat: add emoji audit log actions

--- lib/src/api/server/audit_log/actions/emoji.dart
+++ lib/src/api/server/audit_log/actions/emoji.dart
@@ -5,15 +5,10 @@ final class EmojiCreateAuditLog extends AuditLog {
   final String emojiName;
 
   EmojiCreateAuditLog(
-      {required Snowflake serverId, required Snowflake userId, required this.emojiName})
+      {required Snowflake serverId,
+      required Snowflake userId,
+      required this.emojiName})
       : super(AuditLogType.emojiCreate, serverId, userId);
-
-  factory EmojiCreateAuditLog.fromJson(Map<String, dynamic> json) {
-    return EmojiCreateAuditLog(
-        serverId: json['guild_id'],
-        emojiName: json['changes'][0]['new_value'],
-        userId: json['user_id']);
-  }
 }
 
 final class EmojiUpdateAuditLog extends AuditLog {
@@ -25,28 +20,15 @@ final class EmojiUpdateAuditLog extends AuditLog {
       required Snowflake userId,
       required this.beforeEmojiName,
       required this.afterEmojiName})
-      : super(AuditLogType.emojiCreate, serverId, userId);
-
-  factory EmojiUpdateAuditLog.fromJson(Map<String, dynamic> json) {
-    return EmojiUpdateAuditLog(
-        serverId: json['guild_id'],
-        beforeEmojiName: json['changes'][0]['old_value'],
-        afterEmojiName: json['changes'][0]['new_value'],
-        userId: json['user_id']);
-  }
+      : super(AuditLogType.emojiUpdate, serverId, userId);
 }
 
 final class EmojiDeleteAuditLog extends AuditLog {
   final String emojiName;
 
   EmojiDeleteAuditLog(
-      {required Snowflake serverId, required Snowflake userId, required this.emojiName})
+      {required Snowflake serverId,
+      required Snowflake userId,
+      required this.emojiName})
       : super(AuditLogType.emojiDelete, serverId, userId);
-
-  factory EmojiDeleteAuditLog.fromJson(Map<String, dynamic> json) {
-    return EmojiDeleteAuditLog(
-        serverId: json['guild_id'],
-        emojiName: json['changes'][0]['old_value'],
-        userId: json['user_id']);
-  }
 }
