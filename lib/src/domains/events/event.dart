46c973d345d894eeac8d5d4e84381b0ca5ceb58a
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-07-11 10:37:02 +0200

    refactor: update event parameter structure to use nested lists for improved clarity and consistency across event definitions

--- lib/src/domains/events/event.dart
+++ lib/src/domains/events/event.dart
@@ -14,11 +14,11 @@ import 'package:mineral/src/domains/events/contracts/private/private_channel_pin
 import 'package:mineral/src/domains/events/contracts/private/private_channel_update_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_dialog_submit_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_message_create_event.dart';
+import 'package:mineral/src/domains/events/contracts/private/private_message_reaction_add_event.dart';
+import 'package:mineral/src/domains/events/contracts/private/private_message_reaction_remove_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_poll_vote_remove_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_text_select_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_user_select_event.dart';
-import 'package:mineral/src/domains/events/contracts/private/private_message_reaction_add_event.dart';
-import 'package:mineral/src/domains/events/contracts/private/private_message_reaction_remove_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_audit_log_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_ban_add_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_ban_remove_event.dart';
@@ -63,109 +63,232 @@ import 'contracts/server/server_poll_vote_add_event.dart';
 interface class EventType {}
 
 enum Event implements EnhancedEnum, EventType {
-  ready(ReadyEvent, ['Bot bot']),
-  inviteCreate(InviteCreateEvent, ['Invite invite']),
-  inviteDelete(InviteDeleteEvent, ['String code, Channel channel']),
-  typing(TypingEvent, ['Typing typing']),
-  serverAuditLog(ServerAuditLogEvent, ['Server server']),
-  serverCreate(ServerCreateEvent, ['Server server']),
-  serverUpdate(ServerUpdateEvent, ['Server before', 'Server after']),
-  serverDelete(ServerDeleteEvent, ['Server? server']),
-  serverMessageCreate(ServerMessageCreateEvent, ['ServerMessage message']),
-  serverChannelCreate(ServerChannelCreateEvent, ['ServerChannel channel']),
-  serverChannelUpdate(ServerChannelUpdateEvent,
-      ['ServerChannel before', 'ServerChannel after']),
-  serverChannelDelete(ServerChannelDeleteEvent, ['ServerChannel? channel']),
-  serverChannelPinsUpdate(
-      ServerChannelPinsUpdateEvent, ['ServerChannel channel']),
-  privateChannelPinsUpdate(
-      PrivateChannelPinsUpdateEvent, ['PrivateChannel channel']),
-  serverMemberAdd(ServerMemberAddEvent, ['Server server', 'Member member']),
-  serverMemberRemove(ServerMemberRemoveEvent, ['Server server', 'User user']),
-  serverBanAdd(ServerBanAddEvent, ['Server server', 'User user']),
-  serverBanRemove(ServerBanRemoveEvent, ['Server server', 'User user']),
-  serverMemberUpdate(ServerMemberUpdateEvent,
-      ['Server server', 'ServerMember? before', 'Member after']),
-  serverPresenceUpdate(ServerPresenceUpdateEvent,
-      ['Member member', 'Server server', 'Presence presence']),
-  serverEmojisUpdate(ServerEmojisUpdateEvent,
-      ['Map<Snowflake, Emoji> emojis', 'Server server']),
-  serverStickersUpdate(ServerStickersUpdateEvent,
-      ['Server server', 'Map<Snowflake, Sticker> stickers']),
-  serverRoleCreate(ServerRolesCreateEvent, ['Server server', 'Role role']),
-  serverRoleUpdate(
-      ServerRolesUpdateEvent, ['Server server', 'Role before', 'Role after']),
-  serverRoleDelete(ServerRolesDeleteEvent, ['Server server', 'Role? role']),
-  serverButtonClick(ServerButtonClickEvent, ['ServerButtonContext ctx']),
-  serverDialogSubmit(ServerDialogSubmitEvent, ['ServerDialogContext ctx']),
-  serverChannelSelect(ServerChannelSelectEvent,
-      ['ServerSelectContext ctx', 'List<ServerChannel> channels']),
-  serverRoleSelect(
-      ServerRoleSelectEvent, ['ServerSelectContext ctx', 'List<Role> roles']),
-  serverMemberSelect(ServerMemberSelectEvent,
-      ['ServerSelectContext ctx', 'List<Member> members']),
-  serverTextSelect(ServerTextSelectEvent,
-      ['ServerSelectContext ctx', 'List<String> values']),
-  serverThreadCreate(
-      ServerThreadCreateEvent, ['Server server', 'ThreadChannel channel']),
-  serverThreadUpdate(ServerThreadUpdateEvent,
-      ['Server server', 'ThreadChannel before', 'ThreadChannel after']),
-  serverThreadDelete(
-      ServerThreadDeleteEvent, ['ThreadChannel thread', 'Server server']),
-  serverThreadMemberUpdate(ServerThreadMemberUpdateEvent,
-      ['ThreadChannel thread', 'Server server', 'Member member']),
-  serverThreadMemberAdd(ServerThreadMemberAddEvent,
-      ['ThreadChannel thread', 'Server server', 'Member member']),
-  serverThreadMemberRemove(ServerThreadMemberRemoveEvent,
-      ['ThreadChannel thread', 'Server server', 'Member member']),
-  serverThreadListSync(ServerThreadListSyncEvent,
-      ['List<ThreadChannel> threads', 'Server server']),
-  serverMemberChunk(ServerMemberChunkEvent,
-      ['Server server', 'Map<Snowflake, Member> members']),
-  serverMessageReactionAdd(
-      ServerMessageReactionAddEvent, ['MessageReaction reaction']),
-  serverMessageReactionRemove(
-      ServerMessageReactionRemoveEvent, ['MessageReaction reaction']),
-
-  serverPollVoteAdd(
-      ServerPollVoteAddEvent, ['PollAnswerVote<ServerMessage> message', 'User user']),
-  serverPollVoteRemove(
-      ServerPollVoteRemoveEvent, ['PollAnswerVote<ServerMessage> message', 'User user']),
+  ready(ReadyEvent, [
+    ['Bot', 'bot']
+  ]),
+  inviteCreate(InviteCreateEvent, [
+    ['Invite', 'invite']
+  ]),
+  inviteDelete(InviteDeleteEvent, [
+    ['String', 'code'],
+    ['Channel', 'channel']
+  ]),
+  typing(TypingEvent, [
+    ['Typing', 'typing']
+  ]),
+  serverAuditLog(ServerAuditLogEvent, [
+    ['Server', 'server']
+  ]),
+  serverCreate(ServerCreateEvent, [
+    ['Server', 'server']
+  ]),
+  serverUpdate(ServerUpdateEvent, [
+    ['Server', 'before'],
+    ['Server', 'after']
+  ]),
+  serverDelete(ServerDeleteEvent, [
+    ['Server', 'server']
+  ]),
+  serverMessageCreate(ServerMessageCreateEvent, [
+    ['ServerMessage', 'message']
+  ]),
+  serverChannelCreate(ServerChannelCreateEvent, [
+    ['ServerChannel', 'channel']
+  ]),
+  serverChannelUpdate(ServerChannelUpdateEvent, [
+    ['ServerChannel', 'before'],
+    ['ServerChannel', 'after']
+  ]),
+  serverChannelDelete(ServerChannelDeleteEvent, [
+    ['ServerChannel', 'channel']
+  ]),
+  serverChannelPinsUpdate(ServerChannelPinsUpdateEvent, [
+    ['ServerChannel', 'channel']
+  ]),
+  privateChannelPinsUpdate(PrivateChannelPinsUpdateEvent, [
+    ['PrivateChannel', 'channel']
+  ]),
+  serverMemberAdd(ServerMemberAddEvent, [
+    ['Server', 'server'],
+    ['Member', 'member']
+  ]),
+  serverMemberRemove(ServerMemberRemoveEvent, [
+    ['Server', 'server'],
+    ['User', 'user']
+  ]),
+  serverBanAdd(ServerBanAddEvent, [
+    ['Server', 'server'],
+    ['User', 'user']
+  ]),
+  serverBanRemove(ServerBanRemoveEvent, [
+    ['Server', 'server'],
+    ['User', 'user']
+  ]),
+  serverMemberUpdate(ServerMemberUpdateEvent, [
+    ['Server', 'server'],
+    ['ServerMember', 'before'],
+    ['Member', 'after']
+  ]),
+  serverPresenceUpdate(ServerPresenceUpdateEvent, [
+    ['Member', 'member'],
+    ['Server', 'server'],
+    ['Presence', 'presence']
+  ]),
+  serverEmojisUpdate(ServerEmojisUpdateEvent, [
+    ['Map<Snowflake, Emoji>', 'emojis'],
+    ['Server', 'server']
+  ]),
+  serverStickersUpdate(ServerStickersUpdateEvent, [
+    ['Server', 'server'],
+    ['Map<Snowflake, Sticker>', 'stickers']
+  ]),
+  serverRoleCreate(ServerRolesCreateEvent, [
+    ['Server', 'server'],
+    ['Role', 'role']
+  ]),
+  serverRoleUpdate(ServerRolesUpdateEvent, [
+    ['Server', 'server'],
+    ['Role', 'before'],
+    ['Role', 'after']
+  ]),
+  serverRoleDelete(ServerRolesDeleteEvent, [
+    ['Server', 'server'],
+    ['Role', 'role']
+  ]),
+  serverButtonClick(ServerButtonClickEvent, [
+    ['ServerButtonContext', 'ctx']
+  ]),
+  serverDialogSubmit(ServerDialogSubmitEvent, [
+    ['ServerDialogContext', 'ctx']
+  ]),
+  serverChannelSelect(ServerChannelSelectEvent, [
+    ['ServerSelectContext', 'ctx'],
+    ['List<ServerChannel>', 'channels']
+  ]),
+  serverRoleSelect(ServerRoleSelectEvent, [
+    ['ServerSelectContext', 'ctx'],
+    ['List<Role>', 'roles']
+  ]),
+  serverMemberSelect(ServerMemberSelectEvent, [
+    ['ServerSelectContext', 'ctx'],
+    ['List<Member>', 'members']
+  ]),
+  serverTextSelect(ServerTextSelectEvent, [
+    ['ServerSelectContext', 'ctx'],
+    ['List<String>', 'values']
+  ]),
+  serverThreadCreate(ServerThreadCreateEvent, [
+    ['Server', 'server'],
+    ['ThreadChannel', 'channel']
+  ]),
+  serverThreadUpdate(ServerThreadUpdateEvent, [
+    ['Server', 'server'],
+    ['ThreadChannel', 'before'],
+    ['ThreadChannel', 'after']
+  ]),
+  serverThreadDelete(ServerThreadDeleteEvent, [
+    ['ThreadChannel', 'thread'],
+    ['Server', 'server']
+  ]),
+  serverThreadMemberUpdate(ServerThreadMemberUpdateEvent, [
+    ['ThreadChannel', 'thread'],
+    ['Server', 'server'],
+    ['Member', 'member']
+  ]),
+  serverThreadMemberAdd(ServerThreadMemberAddEvent, [
+    ['ThreadChannel', 'thread'],
+    ['Server', 'server'],
+    ['Member', 'member']
+  ]),
+  serverThreadMemberRemove(ServerThreadMemberRemoveEvent, [
+    ['ThreadChannel', 'thread'],
+    ['Server', 'server'],
+    ['Member', 'member']
+  ]),
+  serverThreadListSync(ServerThreadListSyncEvent, [
+    ['List<ThreadChannel>', 'threads'],
+    ['Server', 'server']
+  ]),
+  serverMemberChunk(ServerMemberChunkEvent, [
+    ['Server', 'server'],
+    ['Map<Snowflake, Member>', 'members']
+  ]),
+  serverMessageReactionAdd(ServerMessageReactionAddEvent, [
+    ['MessageReaction', 'reaction']
+  ]),
+  serverMessageReactionRemove(ServerMessageReactionRemoveEvent, [
+    ['MessageReaction', 'reaction']
+  ]),
+  serverPollVoteAdd(ServerPollVoteAddEvent, [
+    ['PollAnswerVote<ServerMessage>', 'message'],
+    ['User', 'user']
+  ]),
+  serverPollVoteRemove(ServerPollVoteRemoveEvent, [
+    ['PollAnswerVote<ServerMessage>', 'message'],
+    ['User', 'user']
+  ]),
 
   // private
-  privateMessageCreate(PrivateMessageCreateEvent, ['PrivateMessage message']),
-  privateChannelCreate(PrivateChannelCreateEvent, ['PrivateChannel channel']),
-  privateChannelUpdate(PrivateChannelUpdateEvent,
-      ['PrivateChannel before', 'PrivateChannel after']),
-
-  privatePollVoteAdd(
-      PrivatePollVoteAddEvent, ['PollAnswerVote<PrivateMessage> message', 'User user']),
-
-  privatePollVoteRemove(
-      PrivatePollVoteRemoveEvent, ['PollAnswerVote<PrivateMessage> message', 'User user']),
-
-  privateChannelDelete(PrivateChannelDeleteEvent, ['PrivateChannel? channel']),
-  privateButtonClick(PrivateButtonClickEvent, ['PrivateButtonContext ctx']),
-  privateDialogSubmit(PrivateDialogSubmitEvent, ['PrivateDialogContext ctx']),
-  privateUserSelect(
-      PrivateUserSelectEvent, ['PrivateSelectContext ctx', 'List<User> users']),
-  privateTextSelect(PrivateTextSelectEvent,
-      ['PrivateSelectContext ctx', 'List<String> values']),
-  privateMessageReactionAdd(
-      PrivateMessageReactionAddEvent, ['MessageReaction reaction']),
-  privateMessageReactionRemove(
-      PrivateMessageReactionRemoveEvent, ['MessageReaction reaction']),
-
-  voiceStateUpdate(
-      VoiceStateUpdateEvent, ['VoiceState before', 'VoiceState after']),
-  voiceJoin(VoiceJoinEvent, ['VoiceState state']),
-  voiceLeave(VoiceLeaveEvent, ['VoiceState state']),
-  voiceMove(VoiceMoveEvent, ['VoiceState? before', 'VoiceState after']);
+  privateMessageCreate(PrivateMessageCreateEvent, [
+    ['PrivateMessage', 'message']
+  ]),
+  privateChannelCreate(PrivateChannelCreateEvent, [
+    ['PrivateChannel', 'channel']
+  ]),
+  privateChannelUpdate(PrivateChannelUpdateEvent, [
+    ['PrivateChannel', 'before'],
+    ['PrivateChannel', 'after']
+  ]),
+  privatePollVoteAdd(PrivatePollVoteAddEvent, [
+    ['PollAnswerVote<PrivateMessage>', 'message'],
+    ['User', 'user']
+  ]),
+  privatePollVoteRemove(PrivatePollVoteRemoveEvent, [
+    ['PollAnswerVote<PrivateMessage>', 'message'],
+    ['User', 'user']
+  ]),
+  privateChannelDelete(PrivateChannelDeleteEvent, [
+    ['PrivateChannel', 'channel']
+  ]),
+  privateButtonClick(PrivateButtonClickEvent, [
+    ['PrivateButtonContext', 'ctx']
+  ]),
+  privateDialogSubmit(PrivateDialogSubmitEvent, [
+    ['PrivateDialogContext', 'ctx']
+  ]),
+  privateUserSelect(PrivateUserSelectEvent, [
+    ['PrivateSelectContext', 'ctx'],
+    ['List<User>', 'users']
+  ]),
+  privateTextSelect(PrivateTextSelectEvent, [
+    ['PrivateSelectContext', 'ctx'],
+    ['List<String>', 'values']
+  ]),
+  privateMessageReactionAdd(PrivateMessageReactionAddEvent, [
+    ['MessageReaction', 'reaction']
+  ]),
+  privateMessageReactionRemove(PrivateMessageReactionRemoveEvent, [
+    ['MessageReaction', 'reaction']
+  ]),
+  voiceStateUpdate(VoiceStateUpdateEvent, [
+    ['VoiceState', 'before'],
+    ['VoiceState', 'after']
+  ]),
+  voiceJoin(VoiceJoinEvent, [
+    ['VoiceState', 'state']
+  ]),
+  voiceLeave(VoiceLeaveEvent, [
+    ['VoiceState', 'state']
+  ]),
+  voiceMove(VoiceMoveEvent, [
+    ['VoiceState', 'before'],
+    ['VoiceState', 'after']
+  ]);
 
   @override
   final Type value;
 
-  final List<String> parameters;
+  final List<List<String>> parameters;
 
   const Event(this.value, this.parameters);
 }
