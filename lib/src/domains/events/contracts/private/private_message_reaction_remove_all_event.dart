cf98ca422ada1a743abd776bb9de79bf0a2013d5
pandaguerrier <juleslofficial@gmail.com>
2025-09-11 10:09:20 +0200

    feat: remove un used import

--- lib/src/domains/events/contracts/private/private_message_reaction_remove_all_event.dart
+++ lib/src/domains/events/contracts/private/private_message_reaction_remove_all_event.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:mineral/api.dart';
-import 'package:mineral/src/api/common/message_reaction.dart';
 import 'package:mineral/src/domains/events/event.dart';
 import 'package:mineral/src/domains/events/types/listenable_event.dart';
 
