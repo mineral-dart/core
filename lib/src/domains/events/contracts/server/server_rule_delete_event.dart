74ee1ec699a29708ae16ac7352251bc89442bafe
pandaguerrier <juleslofficial@gmail.com>
2025-09-19 13:07:06 +0200

    fix: remove unusued imports

--- lib/src/domains/events/contracts/server/server_rule_delete_event.dart
+++ lib/src/domains/events/contracts/server/server_rule_delete_event.dart
@@ -1,7 +1,5 @@
 import 'dart:async';
 
-import 'package:mineral/api.dart';
-import 'package:mineral/src/api/common/polls/poll_answer_vote.dart';
 import 'package:mineral/src/api/server/moderation/auto_moderation_rule.dart';
 import 'package:mineral/src/domains/events/event.dart';
 import 'package:mineral/src/domains/events/types/listenable_event.dart';
