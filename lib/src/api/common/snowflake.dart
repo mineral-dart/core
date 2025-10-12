84ff0092bf89eeffdd2ae23484f3d56d35cf477f
DCodeProg <danael.legrand@outlook.fr>
2025-10-12 20:42:40 +0200

    feat: add getters to retrieve snowflake properties

--- lib/src/api/common/snowflake.dart
+++ lib/src/api/common/snowflake.dart
@@ -16,4 +16,26 @@ extension type Snowflake(String value) {
   }
 
   bool equals(String value) => this.value == value;
+
+  DateTime get createdAt {
+    const discordEpochMs = 1420070400000;
+    final intSnowflake = int.parse(value);
+    final timestampMs = (intSnowflake >> 22) + discordEpochMs;
+    return DateTime.fromMillisecondsSinceEpoch(timestampMs, isUtc: true);
+  }
+
+  int get internalWorkerId {
+    final intSnowflake = int.parse(value);
+    return (intSnowflake & 0x3E0000) >> 17;
+  }
+
+  int get internalProcessId {
+    final intSnowflake = int.parse(value);
+    return (intSnowflake & 0x1F000) >> 12;
+  }
+
+  int get increment {
+    final intSnowflake = int.parse(value);
+    return intSnowflake & 0xFFF;
+  }
 }
