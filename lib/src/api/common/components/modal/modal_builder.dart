f772f9e7ce8d782bb2415c7741902a202a766061
DCodeProg <danael.legrand@outlook.fr>
2025-10-14 01:24:40 +0200

    feat: add a ModalBuilder class that handle modal creation

---
+++ lib/src/api/common/components/modal/modal_builder.dart
@@ -0,0 +1,43 @@
+import '../../../../../api.dart';
+import '../shared/text_display.dart';
+import 'modal_label.dart';
+
+final class ModalBuilder {
+  final List<Component> _components = [];
+
+  final String _customId;
+  String? _title;
+
+  ModalBuilder(this._customId);
+
+  ModalBuilder setTitle(String title) {
+    _title = title;
+    return this;
+  }
+
+  void text(String text) {
+    _components.add(TextDisplay(text));
+  }
+
+  void label({
+    required String label,
+    required Component component,
+    String? description,
+  }) {
+    _components.add(
+      ModalLabel(
+        label: label,
+        component: component,
+        description: description,
+      ),
+    );
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      'custom_id': _customId,
+      'title': _title,
+      'components': _components.map((e) => e.toJson()).toList(),
+    };
+  }
+}
