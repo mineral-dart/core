40ebd114335ed2489b93a9976ec3b32a85cd4ab9
pandaguerrier <juleslofficial@gmail.com>
2025-10-15 17:26:59 +0200

    feat: integrate the regex in command helper.

--- lib/src/api/common/commands/builder/command_declaration_builder.dart
+++ lib/src/api/common/commands/builder/command_declaration_builder.dart
@@ -6,7 +6,6 @@ import 'package:mineral/src/api/common/commands/command_helper.dart';
 import 'package:mineral/src/api/common/commands/command_option.dart';
 import 'package:mineral/src/api/common/commands/command_type.dart';
 import 'package:mineral/src/domains/commands/command_builder.dart';
-import 'package:mineral/src/infrastructure/io/exceptions/command_name_exception.dart';
 import 'package:mineral/src/infrastructure/io/exceptions/missing_method_exception.dart';
 import 'package:mineral/src/infrastructure/io/exceptions/missing_property_exception.dart';
 
@@ -24,7 +23,7 @@ final class CommandDeclarationBuilder implements CommandBuilder {
   Function? _handle;
 
   CommandDeclarationBuilder setName(String name, {Translation? translation}) {
-    _verifyName(name);
+    _helper.verifyName(name);
 
     this.name = name.toLowerCase();
 
@@ -32,22 +31,11 @@ final class CommandDeclarationBuilder implements CommandBuilder {
       _nameLocalizations = _helper.extractTranslations('name', translation);
 
       for (final nameTranslation in _nameLocalizations!.values) {
-        _verifyName(nameTranslation);
+        _helper.verifyName(nameTranslation);
       }
     }
 
     return this;
-  }
-
-  void _verifyName(String name) {
-    final regex = RegExp(
-      r"^[-_'A-Za-z0-9\u0900-\u097F\u0E00-\u0E7F]{1,32}$",
-      unicode: true,
-    );
-
-    if (!regex.hasMatch(name)) {
-      throw CommandNameException('Invalid command name for $name.');
-    }
   }
 
   CommandDeclarationBuilder setContext(CommandContextType context) {
--- lib/src/api/common/commands/command_helper.dart
+++ lib/src/api/common/commands/command_helper.dart
@@ -1,7 +1,13 @@
 import 'package:mineral/src/api/common/commands/builder/translation.dart';
 import 'package:mineral/src/api/common/lang.dart';
+import 'package:mineral/src/infrastructure/io/exceptions/command_name_exception.dart';
 
 final class CommandHelper {
+  final _regex = RegExp(
+    r"^[-_'A-Za-z0-9\u0900-\u097F\u0E00-\u0E7F]{1,32}$",
+    unicode: true,
+  );
+
   Map<String, String>? extractTranslations(
       String key, Translation translations) {
     if (translations.translations[key] case final Map<Lang, String> elements) {
@@ -11,5 +17,11 @@ final class CommandHelper {
     }
 
     return null;
+  }
+
+  void verifyName(String name) {
+    if (!_regex.hasMatch(name)) {
+      throw CommandNameException('Invalid command name for $name.');
+    }
   }
 }
