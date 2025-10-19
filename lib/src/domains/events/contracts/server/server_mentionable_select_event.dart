5db2bafb5977e653a51a3d0be7debbce7dc30c16
DCodeProg <danael.legrand@outlook.fr>
2025-10-19 17:15:14 +0200

    feat: add PrivateMentionableSelectEvent and ServerMentionableSelectEvent implementations

---
+++ lib/src/domains/events/contracts/server/server_mentionable_select_event.dart
@@ -0,0 +1,18 @@
+import 'dart:async';
+
+import 'package:mineral/src/domains/components/selects/contexts/server_select_context.dart';
+import 'package:mineral/src/domains/events/event.dart';
+import 'package:mineral/src/domains/events/types/listenable_event.dart';
+
+typedef ServerMentionableSelectEventHandler = FutureOr<void> Function(
+    ServerSelectContext ctx, List<dynamic> mentionables);
+
+abstract class ServerMentionableSelectEvent implements ListenableEvent {
+  @override
+  Event get event => Event.serverMentionableSelect;
+
+  @override
+  String? customId;
+
+  FutureOr<void> handle(ServerSelectContext ctx, List<dynamic> mentionables);
+}
--- lib/src/domains/events/event.dart
+++ lib/src/domains/events/event.dart
@@ -12,11 +12,12 @@ import 'package:mineral/src/domains/events/contracts/private/private_channel_cre
 import 'package:mineral/src/domains/events/contracts/private/private_channel_delete_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_channel_pins_update_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_channel_update_event.dart';
-import 'package:mineral/src/domains/events/contracts/private/private_modal_submit_event.dart';
+import 'package:mineral/src/domains/events/contracts/private/private_mentionable_select_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_message_create_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_message_reaction_add_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_message_reaction_remove_all_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_message_reaction_remove_event.dart';
+import 'package:mineral/src/domains/events/contracts/private/private_modal_submit_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_poll_vote_remove_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_text_select_event.dart';
 import 'package:mineral/src/domains/events/contracts/private/private_user_select_event.dart';
@@ -31,17 +32,18 @@ import 'package:mineral/src/domains/events/contracts/server/server_channel_selec
 import 'package:mineral/src/domains/events/contracts/server/server_channel_update_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_create_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_delete_event.dart';
-import 'package:mineral/src/domains/events/contracts/server/server_modal_submit_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_emojis_update_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_member_add_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_member_chunk_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_member_remove_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_member_select_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_member_update_event.dart';
+import 'package:mineral/src/domains/events/contracts/server/server_mentionable_select_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_message_create_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_message_reaction_add_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_message_reaction_remove_all_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_message_reaction_remove_event.dart';
+import 'package:mineral/src/domains/events/contracts/server/server_modal_submit_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_poll_vote_remove_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_presence_update_event.dart';
 import 'package:mineral/src/domains/events/contracts/server/server_role_select_event.dart';
@@ -179,6 +181,10 @@ enum Event implements EnhancedEnum, EventType {
     ['ServerSelectContext', 'ctx'],
     ['List<Member>', 'members']
   ]),
+  serverMentionableSelect(ServerMentionableSelectEvent, [
+    ['ServerSelectContext', 'ctx'],
+    ['List<dynamic>', 'mentionables']
+  ]),
   serverTextSelect(ServerTextSelectEvent, [
     ['ServerSelectContext', 'ctx'],
     ['List<String>', 'values']
@@ -283,6 +289,10 @@ enum Event implements EnhancedEnum, EventType {
   privateUserSelect(PrivateUserSelectEvent, [
     ['PrivateSelectContext', 'ctx'],
     ['List<User>', 'users']
+  ]),
+  privateMentionableSelect(PrivateMentionableSelectEvent, [
+    ['PrivateSelectContext', 'ctx'],
+    ['List<dynamic>', 'mentionables']
   ]),
   privateTextSelect(PrivateTextSelectEvent, [
     ['PrivateSelectContext', 'ctx'],
