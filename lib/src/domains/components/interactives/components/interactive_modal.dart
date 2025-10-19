febc3e7c759e1ded34ff3eb65c0de9c21c9efaca
DCodeProg <danael.legrand@outlook.fr>
2025-10-19 17:00:23 +0200

    refactor: update InteractiveModal handle values type

--- lib/src/domains/components/interactives/components/interactive_modal.dart
+++ lib/src/domains/components/interactives/components/interactive_modal.dart
@@ -3,6 +3,6 @@ import 'dart:async';
 import 'package:mineral/api.dart';
 import 'package:mineral/src/domains/components/interactives/components/interactive_component.dart';
 
-abstract interface class InteractiveModal<T extends Map<String, String>> implements InteractiveComponent<ModalBuilder> {
-  FutureOr<void> handle(ModalContext ctx, T options);
+abstract interface class InteractiveModal<T extends dynamic> implements InteractiveComponent<ModalBuilder> {
+  FutureOr<void> handle(ModalContext ctx, T values);
 }
