0dcce76663ba46fa61789cd3ae5edc22d026d26c
DCodeProg <danael.legrand@outlook.fr>
2025-10-27 13:50:19 +0100

    style: improve code formatting and readability in channel_builder.dart

--- lib/src/api/common/builder/channel_builder.dart
+++ lib/src/api/common/builder/channel_builder.dart
@@ -3,11 +3,8 @@ import 'package:mineral/src/domains/common/utils/utils.dart';
 
 abstract class ChannelBuilderContract {
   void setName(String value);
-
   void setPosition(int value);
-
   void setPermissionOverwrite(List<ChannelPermissionOverwrite> value);
-
   void addPermissionOverwrite(ChannelPermissionOverwrite value);
 
   Map<String, dynamic> build();
@@ -15,71 +12,50 @@ abstract class ChannelBuilderContract {
 
 abstract interface class TextChannelBuilder extends ChannelBuilderContract {
   void setTopic(String value);
-
   void setRateLimitPerUser(Duration value);
-
   void setParentId(String value);
-
   void setNsfw(bool value);
-
   void setDefaultAutoArchiveDuration(Duration value);
-
   void setDefaultThreadRateLimitPerUser(Duration value);
 }
 
 abstract interface class ForumChannelBuilder extends ChannelBuilderContract {
   void setTopic(String value);
-
   void setRateLimitPerUser(Duration value);
-
   void setParentId(String value);
-
   void setNsfw(bool value);
-
   void setDefaultAutoArchiveDuration(Duration value);
-
   void setDefaultReactionEmoji(PartialEmoji value);
-
   void setDefaultSortOrder(int value);
-
   void setDefaultForumLayout(int value);
-
   void setDefaultThreadRateLimitPerUser(Duration value);
 }
 
-abstract interface class AnnouncementChannelBuilder extends ChannelBuilderContract {
+abstract interface class AnnouncementChannelBuilder
+    extends ChannelBuilderContract {
   void setTopic(String value);
-
   void setParentId(String value);
-
   void setNsfw(bool value);
-
   void setDefaultAutoArchiveDuration(Duration value);
-
   void setDefaultThreadRateLimitPerUser(Duration value);
 }
 
 abstract interface class VoiceChannelBuilder extends ChannelBuilderContract {
   void setBitrate(int value);
-
   void setUserLimit(int value);
-
   void setParentId(String value);
-
   void setNsfw(bool value);
-
   void setRtcRegion(String value);
-
   void setVideoQualityMode(VideoQuality value);
 }
 
 abstract interface class ThreadChannelBuilder extends ChannelBuilderContract {
   void setDefaultAutoArchiveDuration(Duration value);
-
   void setDefaultThreadRateLimitPerUser(Duration value);
 }
 
-abstract interface class CategoryChannelBuilder extends ChannelBuilderContract {}
+abstract interface class CategoryChannelBuilder
+    extends ChannelBuilderContract {}
 
 final class ChannelBuilder
     implements
@@ -89,7 +65,6 @@ final class ChannelBuilder
         ForumChannelBuilder,
         CategoryChannelBuilder,
         ThreadChannelBuilder {
-
   final ChannelType? _type;
   String? _name;
   String? _topic;
@@ -129,7 +104,8 @@ final class ChannelBuilder
   void setRateLimitPerUser(Duration value) => _rateLimitPerUser = value;
 
   @override
-  void addPermissionOverwrite(ChannelPermissionOverwrite value) => _permissionOverwrites.add(value);
+  void addPermissionOverwrite(ChannelPermissionOverwrite value) =>
+      _permissionOverwrites.add(value);
 
   @override
   void setPermissionOverwrite(List<ChannelPermissionOverwrite> value) =>
@@ -148,10 +124,12 @@ final class ChannelBuilder
   void setVideoQualityMode(VideoQuality value) => _videoQualityMode = value;
 
   @override
-  void setDefaultAutoArchiveDuration(Duration value) => _defaultAutoArchiveDuration = value;
+  void setDefaultAutoArchiveDuration(Duration value) =>
+      _defaultAutoArchiveDuration = value;
 
   @override
-  void setDefaultReactionEmoji(PartialEmoji value) => _defaultReactionEmoji = value;
+  void setDefaultReactionEmoji(PartialEmoji value) =>
+      _defaultReactionEmoji = value;
 
   @override
   void setDefaultSortOrder(int value) => _defaultSortOrder = value;
@@ -160,7 +138,8 @@ final class ChannelBuilder
   void setDefaultForumLayout(int value) => _defaultForumLayout = value;
 
   @override
-  void setDefaultThreadRateLimitPerUser(Duration value) => _defaultThreadRateLimitPerUser = value;
+  void setDefaultThreadRateLimitPerUser(Duration value) =>
+      _defaultThreadRateLimitPerUser = value;
 
   /// Build text channel.
   /// ```dart
@@ -177,7 +156,8 @@ final class ChannelBuilder
   ///   ..setName('announcements')
   ///   ..setTopic('Announcement channel');
   /// ```
-  static AnnouncementChannelBuilder announcement() => ChannelBuilder(ChannelType.guildAnnouncement);
+  static AnnouncementChannelBuilder announcement() =>
+      ChannelBuilder(ChannelType.guildAnnouncement);
 
   /// Build voice channel.
   /// ```dart
@@ -202,7 +182,8 @@ final class ChannelBuilder
   /// final builder = ChannelBuilder.category()
   ///   ..setName('category');
   ///  ```
-  static CategoryChannelBuilder category() => ChannelBuilder(ChannelType.guildCategory);
+  static CategoryChannelBuilder category() =>
+      ChannelBuilder(ChannelType.guildCategory);
 
   /// Build category channel.
   /// ```dart
@@ -220,23 +201,28 @@ final class ChannelBuilder
       if (_position != null) 'position': _position,
       if (_bitrate != null) 'bitrate': _bitrate,
       if (_userLimit != null) 'user_limit': _userLimit,
-      if (_rateLimitPerUser != null) 'rate_limit_per_user': _rateLimitPerUser!.inSeconds,
+      if (_rateLimitPerUser != null)
+        'rate_limit_per_user': _rateLimitPerUser!.inSeconds,
       if (_permissionOverwrites.isNotEmpty)
         'permission_overwrites': _permissionOverwrites
-            .map((element) => {
-                  'id': element.id,
-                  'type': element.type.value,
-                  'allow': listToBitfield(element.allow),
-                  'deny': listToBitfield(element.deny),
-                })
+            .map(
+              (element) => {
+                'id': element.id,
+                'type': element.type.value,
+                'allow': listToBitfield(element.allow),
+                'deny': listToBitfield(element.deny),
+              },
+            )
             .toList(),
       if (_parentId != null) 'parent_id': _parentId,
       if (_nsfw != null) 'nsfw': _nsfw,
       if (_rtcRegion != null) 'rtc_region': _rtcRegion,
-      if (_videoQualityMode != null) 'video_quality_mode': _videoQualityMode?.value,
+      if (_videoQualityMode != null)
+        'video_quality_mode': _videoQualityMode?.value,
       if (_defaultAutoArchiveDuration != null)
         'default_auto_archive_duration': _defaultAutoArchiveDuration!.inMinutes,
-      if (_defaultForumLayout != null) 'default_forum_layout': _defaultForumLayout,
+      if (_defaultForumLayout != null)
+        'default_forum_layout': _defaultForumLayout,
       if (_defaultReactionEmoji != null)
         'default_reaction_emoji': {
           'emoji_id': _defaultReactionEmoji?.id,
@@ -244,7 +230,8 @@ final class ChannelBuilder
         },
       if (_defaultSortOrder != null) 'default_sort_order': _defaultSortOrder,
       if (_defaultThreadRateLimitPerUser != null)
-        'default_thread_rate_limit_per_user': _defaultThreadRateLimitPerUser!.inSeconds,
+        'default_thread_rate_limit_per_user':
+            _defaultThreadRateLimitPerUser!.inSeconds,
     };
   }
 }
