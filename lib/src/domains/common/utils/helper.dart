1f0442f269588affa65b29afa65cdb8793bcd342
DCodeProg <danael.legrand@outlook.fr>
2025-10-27 13:44:22 +0100

    style: improve code formatting and readability in helper.dart

--- lib/src/domains/common/utils/helper.dart
+++ lib/src/domains/common/utils/helper.dart
@@ -4,12 +4,17 @@ typedef Constructable<T> = T Function();
 typedef ConstructableWithArgs<T, Args> = T Function(Args);
 
 final class Helper {
-  static T? createOrNull<T>(
-      {required dynamic field, required T? Function() fn}) {
+  static T? createOrNull<T>({
+    required dynamic field,
+    required T? Function() fn,
+  }) {
     return field != null ? fn() : null;
   }
 
-  static Future<T?> createOrNullAsync<T>(
-          {required dynamic field, required Future<T?> Function() fn}) async =>
-      field != null ? await fn() : null;
+  static Future<T?> createOrNullAsync<T>({
+    required dynamic field,
+    required Future<T?> Function() fn,
+  }) async {
+    return field != null ? await fn() : null;
+  }
 }
