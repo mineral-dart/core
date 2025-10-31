8f3d5d9ec69f6f387967f62ebda90c63dfa8e39b
DCodeProg <danael.legrand@outlook.fr>
2025-10-31 11:04:20 +0100

    style: improve code formatting and readability in InviteDeleteEventHandler typedef

--- lib/src/domains/events/contracts/common/invite_delete_event.dart
+++ lib/src/domains/events/contracts/common/invite_delete_event.dart
@@ -5,7 +5,9 @@ import 'package:mineral/src/domains/events/event.dart';
 import 'package:mineral/src/domains/events/types/listenable_event.dart';
 
 typedef InviteDeleteEventHandler = FutureOr<void> Function(
-    String code, Channel channel);
+  String code,
+  Channel channel,
+);
 
 abstract class InviteDeleteEvent implements ListenableEvent {
   @override
