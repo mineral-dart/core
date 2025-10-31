f35f12a4b13e3b6e8fd436f804ebb1c054ec72a4
DCodeProg <danael.legrand@outlook.fr>
2025-10-31 12:14:16 +0100

    style: improve code formatting and readability in ChannelFactoryContract methods

--- lib/src/infrastructure/internals/marshaller/types/channel_factory.dart
+++ lib/src/infrastructure/internals/marshaller/types/channel_factory.dart
@@ -7,9 +7,15 @@ import 'package:mineral/src/domains/services/marshaller/marshaller.dart';
 abstract interface class ChannelFactoryContract<T extends Channel> {
   ChannelType get type;
   FutureOr<Map<String, dynamic>> normalize(
-      MarshallerContract marshaller, Map<String, dynamic> json);
+    MarshallerContract marshaller,
+    Map<String, dynamic> json,
+  );
   FutureOr<T> serialize(
-      MarshallerContract marshaller, Map<String, dynamic> json);
+    MarshallerContract marshaller,
+    Map<String, dynamic> json,
+  );
   FutureOr<Map<String, dynamic>> deserialize(
-      MarshallerContract marshaller, T channel);
+    MarshallerContract marshaller,
+    T channel,
+  );
 }
