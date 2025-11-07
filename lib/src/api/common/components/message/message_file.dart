1c8750253a39756552a64baf3ff97393076ea254
pandaguerrier <juleslofficial@gmail.com>
2025-11-07 12:57:05 +0100

    feat: renamed to MessageFile

--- lib/src/api/common/components/message/message_file.dart
+++ lib/src/api/common/components/message/message_file.dart
@@ -4,42 +4,27 @@ import 'dart:typed_data';
 import 'package:http/http.dart' as http;
 import 'package:mineral/api.dart';
 
-final class Attachment implements Component {
+final class MessageFile implements Component {
   ComponentType get type => ComponentType.file;
-
-  static final Map<String, Uint8List> _cachedAttachments = {};
 
   Uint8List? bytes;
   final String _path;
   final bool? _spoiler;
 
-  Attachment(this._path, {bool? spoiler}) : _spoiler = spoiler;
+  MessageFile._(this._path, {bool? spoiler}) : _spoiler = spoiler;
 
-  factory Attachment.path(String path, {bool? spoiler, bool cache = false}) {
-    if (cache && _cachedAttachments.containsKey(path)) {
-      return Attachment(path, spoiler: spoiler)
-        ..bytes = _cachedAttachments[path]!;
-    }
-
+  factory MessageFile.path(String path, {bool? spoiler}) {
     final file = File(path);
     final bytes = file.readAsBytesSync();
 
-    return Attachment(path, spoiler: spoiler)..bytes = bytes;
+    return MessageFile._(path, spoiler: spoiler)..bytes = bytes;
   }
 
-  static Future<Attachment> network(String url,
-      {bool? spoiler, bool cache = false}) async {
-    if (cache && _cachedAttachments.containsKey(url)) {
-      return Attachment(url, spoiler: spoiler)
-        ..bytes = _cachedAttachments[url]!;
-    }
-
+  static Future<MessageFile> network(String url, {bool? spoiler}) async {
     final uri = Uri.parse(url);
     final response = await http.get(uri);
 
-    _cachedAttachments[url] = response.bodyBytes;
-
-    return Attachment(uri.path, spoiler: spoiler)..bytes = response.bodyBytes;
+    return MessageFile._(uri.path, spoiler: spoiler)..bytes = response.bodyBytes;
   }
 
   @override
