2df439159c6b70e925e68c35164d8363820d2d5b
pandaguerrier <juleslofficial@gmail.com>
2025-11-07 12:56:55 +0100

    feat: rework this func

--- lib/src/domains/common/utils/attachment.dart
+++ lib/src/domains/common/utils/attachment.dart
@@ -1,10 +1,7 @@
 import 'package:http/http.dart' as http;
 import 'package:mineral/api.dart';
 
-typedef AttachmentResult = (
-  List<Map<String, dynamic>>,
-  List<http.MultipartFile>
-);
+typedef AttachmentResult = (List<Map<String, dynamic>>, List<http.MultipartFile>);
 
 AttachmentResult makeAttachmentFromBuilder(MessageBuilder builder) {
   final components = builder.build();
@@ -14,18 +11,13 @@ AttachmentResult makeAttachmentFromBuilder(MessageBuilder builder) {
     final comp = components[i];
 
     if (comp['type'] == ComponentType.file.value) {
-      final filePath = comp['file']['url'];
-      final filename = filePath.split('/').last;
+      final file = _prepareFile(comp, files.length);
 
-      final multipartFile = http.MultipartFile.fromBytes(
-        'files[${files.length}]',
-        comp['file']['bytes'],
-        filename: filename,
-      );
-
-      files.add(multipartFile);
-      comp['file']['url'] = 'attachment://$filename';
+      files.add(file);
+      comp['file']['url'] = 'attachment://${file.filename}';
       comp['file']['bytes'] = null;
+      comp['id'] = files.length;
+
       continue;
     }
 
@@ -33,21 +25,10 @@ AttachmentResult makeAttachmentFromBuilder(MessageBuilder builder) {
       final items = comp['items'] as List<dynamic>?;
       if (items != null) {
         for (final item in items) {
-          final media = item['media'] as Map<String, dynamic>?;
-          final url = media?['url'] as String?;
+          final file = _prepareAsset(item, files.length);
 
-          if (url != null && url.startsWith('attachment://') && item['bytes'] != null) {
-            final filename = url.replaceFirst('attachment://', '');
-            final bytes = item['bytes'];
-
-            if (bytes != null) {
-              final multipartFile = http.MultipartFile.fromBytes(
-                'files[${files.length}]',
-                bytes,
-                filename: filename,
-              );
-              files.add(multipartFile);
-            }
+          if (file != null) {
+            files.add(file);
           }
         }
       }
@@ -55,4 +36,39 @@ AttachmentResult makeAttachmentFromBuilder(MessageBuilder builder) {
   }
 
   return (components, files);
+}
+
+http.MultipartFile? _prepareAsset(dynamic payload, int filesLength) {
+  final media = payload['media'] as Map<String, dynamic>?;
+  final url = media?['url'] as String?;
+
+  if (url != null && url.startsWith('attachment://') && payload['bytes'] != null) {
+    final filename = url.replaceFirst('attachment://', '');
+
+    final multipartFile = http.MultipartFile.fromBytes(
+      'files[$filesLength]',
+      payload['bytes'],
+      filename: filename,
+    );
+
+    payload['url'] = 'attachment://$filename';
+    payload['bytes'] = null;
+
+    return multipartFile;
+  }
+
+  return null;
+}
+
+http.MultipartFile _prepareFile(dynamic payload, int filesLength) {
+  final filePath = payload['file']['url'];
+  final filename = filePath.split('/').last;
+
+  final multipartFile = http.MultipartFile.fromBytes(
+    'files[$filesLength]',
+    payload['file']['bytes'],
+    filename: filename,
+  );
+
+  return multipartFile;
 }
