256867857f9641fa9a3c93a7878a7b13a72617d6
pandaguerrier <juleslofficial@gmail.com>
2025-11-07 12:56:25 +0100

    feat: created general asset for img uses

---
+++ lib/src/api/common/components/asset.dart
@@ -0,0 +1,53 @@
+import 'dart:io';
+import 'dart:typed_data';
+
+final class Asset {
+  Uint8List? _bytes;
+  final String url;
+  final bool? spoiler;
+  final String? proxyUrl;
+  final int? height;
+  final int? width;
+  final String? contentType;
+  final String? description;
+
+  Asset(this.url, {this.spoiler, this.proxyUrl, this.height, this.width, this.contentType, this.description});
+
+  factory Asset.file(File file, String name, {bool? spoiler, String? proxyUrl, int? height, int? width, String? contentType, String? description}) {
+    if (!file.existsSync()) {
+      throw ArgumentError('File ${file.path} does not exist');
+    }
+
+    if (name.isEmpty) {
+      throw ArgumentError("Name can't be empty.");
+    }
+
+    final bytes = file.readAsBytesSync();
+
+    return Asset(
+      'attachment://$name',
+      spoiler: spoiler,
+      proxyUrl: proxyUrl,
+      height: height,
+      width: width,
+      contentType: contentType,
+      description: description,
+    ).._bytes = bytes;
+  }
+
+  Map<String, dynamic> toJson() {
+    return {
+      if (description != null) 'description': description,
+      'url': url,
+      if (_bytes != null) 'bytes': _bytes!,
+      if (spoiler != null) 'spoiler': spoiler,
+      'media': {
+        'url': url,
+        if (proxyUrl != null) 'proxy_url': proxyUrl,
+        if (height != null) 'height': height,
+        if (width != null) 'width': width,
+        if (contentType != null) 'content_type': contentType,
+      },
+    };
+  }
+}
