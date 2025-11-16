c41d5c9671b3f06efa465aea90021dd037f9c31b
Baptiste Parmantier <baptiste.parmantier.pro@gmail.com>
2025-11-16 18:17:52 +0100

    feat: rerun ready event to prevent the non-existence of bot instance (#312)
    * feat: rerun ready event to prevent the non-existence of bot instance
    
    * feat: remove duplicated getter

--- lib/src/domains/container/ioc_container.dart
+++ lib/src/domains/container/ioc_container.dart
@@ -1,5 +1,4 @@
 import 'dart:collection';
-
 import 'package:mineral/src/domains/common/utils/helper.dart';
 
 final class IocContainer {
--- lib/src/infrastructure/internals/packets/packet_dispatcher.dart
+++ lib/src/infrastructure/internals/packets/packet_dispatcher.dart
@@ -1,7 +1,10 @@
+import 'package:mineral/container.dart';
 import 'package:mineral/contracts.dart';
 import 'package:mineral/src/domains/common/kernel.dart';
 import 'package:mineral/src/domains/services/packets/packet_dispatcher.dart';
 import 'package:mineral/src/infrastructure/internals/packets/listenable_packet.dart';
+import 'package:mineral/src/infrastructure/internals/packets/listeners/ready_packet.dart';
+import 'package:mineral/src/infrastructure/internals/packets/packet_type.dart';
 import 'package:mineral/src/infrastructure/internals/wss/shard_message.dart';
 import 'package:rxdart/rxdart.dart';
 
@@ -17,6 +20,9 @@ final class PacketDispatcher implements PacketDispatcherContract {
     _packets.stream
         .where((event) => event.type == packet.name)
         .listen((ShardMessage message) {
+      if (message.type == PacketType.ready.name) {
+        ioc.bind(() => ReadyPacketMessage(message));
+      }
       Function.apply(
           listener, [message, _kernel.eventListener.dispatcher.dispatch]);
     });
