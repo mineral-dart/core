cde8c8f3bf16589dbcc6e26edff3c60d5d26899a
DCodeProg <danael.legrand@outlook.fr>
2025-11-24 16:01:50 +0100

    docs: add example main file

---
+++ example/main.dart
@@ -0,0 +1,31 @@
+import 'package:mineral/api.dart';
+
+void main(_, dynamic port) async {
+  final client = ClientBuilder()
+      .setHmrDevPort(port)
+      .setIntent(Intent.allNonPrivileged)
+      .build();
+
+  // Simple command - just 4 lines!
+  client.commands.declare((cmd) => cmd
+    ..setName('hello')
+    ..setDescription('Say hello')
+    ..setHandle(
+      (ctx) {
+        final message = MessageBuilder.text('👋 Hello from Mineral!');
+
+        final mineralLinkButton = Button.link(
+          'https://mineral-dart.dev/',
+          emoji: PartialEmoji.fromUnicode('📘'),
+          label: 'Check our documentation',
+        );
+        message.addButton(mineralLinkButton);
+
+        return ctx.interaction.reply(
+          builder: message,
+        );
+      },
+    ));
+
+  await client.init();
+}
